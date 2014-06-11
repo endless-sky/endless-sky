891b55f3428b471ca3d7641f5dff1cbb548924e1
mzahniser@gmail.com <mzahniser@gmail.com@d00857da-6ca9-0267-48c4-95db20019e50>
2014-06-11 01:29:42 +0000

    Added some helpful extra location functions to Sprite and Screen (e.g. TopLeft()).

--- trunk/Screen.h
+++ trunk/Screen.h
@@ -13,16 +13,30 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
+#include "Point.h"
+
 
 
 // Class that simply holds the screen dimensions. This is really just a wrapper
-// around some global variables.
+// around some global variables, which means that no one but the drawing thread
+// is allowed to use it.
 class Screen {
 public:
 	static void Set(int width, int height);
 	
 	static int Width();
 	static int Height();
+	
+	// Get the positions of the edges and corners of the viewport.
+	static int Left();
+	static int Top();
+	static int Right();
+	static int Bottom();
+	
+	static Point TopLeft();
+	static Point TopRight();
+	static Point BottomLeft();
+	static Point BottomRight();
 	
 	
 private:
