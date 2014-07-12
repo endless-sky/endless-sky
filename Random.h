1281b051befa1d7e57ce27a0d79724a3df121b7f
mzahniser@gmail.com <mzahniser@gmail.com@d00857da-6ca9-0267-48c4-95db20019e50>
2014-07-12 21:03:08 +0000

    Added new functions to Random to set the seed and to get a binomial distribution.

--- Random.h
+++ Random.h
@@ -19,12 +19,21 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 class Random {
 public:
+	// Seed the generator (e.g. to make it produce exactly the same random
+	// numbers it produced previously).
+	static void Seed(uint64_t seed);
+	
 	static uint32_t Int();
 	static uint32_t Int(uint32_t modulus);
 	
+	// Slower functions for getting random numbers from a given distribution.
+	// Do not use these functions in time-critical code.
+	
 	// Return the expected number of failures before k successes, when the 
 	// probability of success is p. The mean value will be k / (1 - p).
-	static uint32_t Polya(uint32_t k, double p);
+	static uint32_t Polya(uint32_t k, double p = .5);
+	// Get a number from a binomial distribution (i.e. integer bell curve).
+	static uint32_t Binomial(uint32_t t, double p = .5);
 };
 
 
