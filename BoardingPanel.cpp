d9a0990974c45474bb586fc4fc72167081b322f1
mzahniser@gmail.com <mzahniser@gmail.com@d00857da-6ca9-0267-48c4-95db20019e50>
2014-09-06 21:45:17 +0000

    Modified the ship capturing logic to also make sure the newly captured ship has at least one jump of fuel, if you can spare any.

--- BoardingPanel.cpp
+++ BoardingPanel.cpp
@@ -273,6 +273,8 @@ bool BoardingPanel::KeyDown(SDL_Keycode key, Uint16 mod)
 			{
 				messages.push_back("You have succeeded in capturing this ship.");
 				victim->WasCaptured(player.Ships().front());
+				if(!victim->JumpsRemaining() && you->CanRefuel(*victim))
+					you->TransferFuel(victim->Attributes().Get("jump fuel"), &*victim);
 				player.AddShip(victim);
 				you->AddEscort(victim);
 				isCapturing = false;
