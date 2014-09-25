ee3c9a5d225dc0fa3dd3612911f1193c368533bd
mzahniser@gmail.com <mzahniser@gmail.com@d00857da-6ca9-0267-48c4-95db20019e50>
2014-09-25 02:18:34 +0000

    Started working on intro missions, and also fixed the handling of "defer" and of the special ship type flags.

--- Mission.h
+++ Mission.h
@@ -85,7 +85,7 @@ public:
 	// information or show new UI panels. PlayerInfo::MissionCallback() will be
 	// used as the callback for any UI panel that returns a value. If it is not
 	// possible for this change to happen, this function returns false.
-	enum Trigger {COMPLETE, OFFER, ACCEPT, DECLINE, FAIL};
+	enum Trigger {COMPLETE, OFFER, ACCEPT, DECLINE, FAIL, DEFER};
 	bool Do(Trigger trigger, PlayerInfo &player, UI *ui = nullptr);
 	
 	// Get a list of NPCs associated with this mission. Every time the player
