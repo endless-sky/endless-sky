d4f644f612792b00152ae36141a53b0e24e0a789
mzahniser@gmail.com <mzahniser@gmail.com@d00857da-6ca9-0267-48c4-95db20019e50>
2014-10-27 22:29:16 +0000

    Fixed a typo in my previous change.

--- AI.cpp
+++ AI.cpp
@@ -710,7 +710,7 @@ int AI::AutoFire(const Ship &ship, const list<std::shared_ptr<Ship>> &ships)
 	const Government *gov = ship.GetGovernment();
 	for(const Armament::Weapon &weapon : ship.Weapons())
 	{
-		if(!weapon.IsReady() || (!target && weapon.IsHoming()))
+		if(!weapon.IsReady() || (!ship.GetTargetShip() && weapon.IsHoming()))
 		{
 			bit <<= 1;
 			continue;
