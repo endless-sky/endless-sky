7f0505b9f92f99368bf2f21a558343de9ba0a9be
Michael Zahniser <mzahniser@gmail.com>
2015-03-17 18:17:50 -0400

    Slight code simplification in Engine.

--- Engine.cpp
+++ Engine.cpp
@@ -69,9 +69,7 @@ Engine::Engine(PlayerInfo &player)
 		if(!object.GetSprite().IsEmpty())
 		{
 			Point position = object.Position();
-			Point unit(0., -1.);
-			if(position)
-				unit = position.Unit();
+			Point unit = object.Unit();
 			position -= center;
 			
 			int type = object.IsStar() ? Radar::SPECIAL :
