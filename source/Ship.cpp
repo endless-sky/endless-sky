b0c5d78762bf12c9567b47b30659f5cf27ac93af
Michael Zahniser <mzahniser@gmail.com>
2015-11-05 20:54:56 -0500

    Added messages when you get plundered, saying what was stolen.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -1104,8 +1104,24 @@ shared_ptr<Ship> Ship::Board(bool autoPlunder)
 	{
 		// Take any outfits that fit.
 		for(auto &it : victim->outfits)
+		{
+			int stolen = 0;
 			while(it.second && cargo.Transfer(it.first, -1))
+			{
+				++stolen;
 				--it.second;
+			}
+			if(stolen && victim->IsYours())
+			{
+				ostringstream out;
+				if(stolen == 1)
+					out << "A " << it.first->Name() << " was";
+				else
+					out << stolen << " " << it.first->Name() << "s were";
+				out << " stolen from " << victim->Name() << ".";
+				Messages::Add(out.str());
+			}
+		}
 		// Take any commodities that fit.
 		victim->cargo.TransferAll(&cargo);
 		// Stop targeting this ship.
