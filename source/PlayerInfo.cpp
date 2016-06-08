7c04872a7546b4e73ea87a57e8e5d36c8cdaeea6
Michael Zahniser <mzahniser@gmail.com>
2016-06-08 18:38:01 -0400

    Outfits that are "in stock" because you have sold them on a planet are now stored in the saved game so they will not disappear if you reload it. (But, they still disappear once you leave the planet.)

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -138,6 +138,12 @@ void PlayerInfo::Load(const string &path)
 				if(grand.Size() >= 2)
 					costBasis[grand.Token(0)] += grand.Value(1);
 		}
+		else if(child.Token(0) == "stock")
+		{
+			for(const DataNode &grand : child)
+				if(grand.Size() >= 2)
+					soldOutfits[GameData::Outfits().Get(grand.Token(0))] += grand.Value(1);
+		}
 		else if(child.Token(0) == "mission")
 		{
 			missions.push_back(Mission());
@@ -1742,6 +1748,18 @@ void PlayerInfo::Save(const string &path) const
 		out.EndChild();
 	}
 	accounts.Save(out);
+	
+	if(!soldOutfits.empty())
+	{
+		out.Write("stock");
+		out.BeginChild();
+		{
+			for(const auto &it : soldOutfits)
+				if(it.second)
+					out.Write(it.first->Name(), it.second);
+		}
+		out.EndChild();
+	}
 	
 	// Save all missions (accepted or available).
 	for(const Mission &mission : missions)
