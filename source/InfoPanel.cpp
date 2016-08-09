cdc801d70976576a4ffca16ed3a1b12106624e40
MarcelineVQ <marcythevq@gmail.com>
2016-08-09 16:55:38 -0700

    add viewing a ship in the infopanel in space will target that ship
    this is for general convenience and specifically to make selecting wounded ships simpler if you want to board a ship and get out of a system fast

--- source/InfoPanel.cpp
+++ source/InfoPanel.cpp
@@ -455,6 +455,8 @@ void InfoPanel::UpdateInfo()
 	
 	const Ship &ship = **shipIt;
 	info.Update(ship);
+	if(player.Flagship() && ship.GetSystem() == player.GetSystem())
+		player.Flagship()->SetTargetShip(*shipIt);
 	
 	outfits.clear();
 	for(const auto &it : ship.Outfits())
