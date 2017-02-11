e6a5ad51a6c1fc6b0584f0bf5adbb7e0642a5535
Nate Graham <pointedstick@zoho.com>
2017-02-11 15:57:58 -0700

    On second thought, let's also standardize the preceeding word: "space" instead of "capacity"

--- source/ShipInfoDisplay.cpp
+++ source/ShipInfoDisplay.cpp
@@ -250,8 +250,8 @@ void ShipInfoDisplay::UpdateAttributes(const Ship &ship, const Depreciation &dep
 	map<string, double> chassis;
 	static const string names[] = {
 		"outfit space free:", "outfit space",
-		"    weapon capacity free:", "weapon capacity",
-		"    engine capacity free:", "engine capacity",
+		"    weapon space free:", "weapon capacity",
+		"    engine space free:", "engine capacity",
 		"gun ports free:", "gun ports",
 		"turret mounts free:", "turret mounts"
 	};
