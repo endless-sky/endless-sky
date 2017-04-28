1c6c85545ee5432474f1d0cad98674dd822f69ed
EndrosG <endros@gmx.net>
2017-04-28 22:42:47 +0200

    Substitute also conditions in mission actions
    1. Substitute conditions in mission actions, outside conversations.
    2. Introduce "<paymentNum>" as pure number

--- source/MissionAction.cpp
+++ source/MissionAction.cpp
@@ -322,8 +322,11 @@ MissionAction MissionAction::Instantiate(map<string, string> &subs, int jumps, i
 	// Fill in the payment amount if this is the "complete" action (which comes
 	// before all the others in the list).
 	if(trigger == "complete" || result.payment)
+	{
+		subs["<paymentNum>"] = to_string(result.payment);
 		subs["<payment>"] = Format::Number(result.payment)
 			+ (result.payment == 1 ? " credit" : " credits");
+	}
 	
 	if(!dialogText.empty())
 		result.dialogText = Format::Replace(dialogText, subs);
@@ -335,7 +338,7 @@ MissionAction MissionAction::Instantiate(map<string, string> &subs, int jumps, i
 	
 	result.fail = fail;
 	
-	result.conditions = conditions;
+	result.conditions = conditions.Substitute(subs);
 	
 	return result;
 }
