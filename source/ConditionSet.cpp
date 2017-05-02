c4a1cd41d01a2216bc2625825dfc0f6e102cd59a
EndrosG <endros@gmx.net>
2017-05-02 13:54:24 +0200

    missing #include "Format.h" in ConditionSet.cpp
    thx @elyssaen to poing that out - had it in my local version. :-/

--- source/ConditionSet.cpp
+++ source/ConditionSet.cpp
@@ -14,6 +14,7 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 #include "DataNode.h"
 #include "DataWriter.h"
+#include "Format.h"
 #include "Random.h"
 
 #include <cmath>
