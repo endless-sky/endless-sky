endless-sky/endless-sky

commit 11774348c22c72acf2608960ccb470fa16d05e4a

Little fixes, hails, volume, fighters and drones (#1417)
* change hails to only occur for living players

* fix volume bar offset due to recent interface changes

* fix carryable ships in other system were being dumped into space when you landed

* fix carryable ships no longer mass select one parent

this was causing issues with carryables being left behind because their potential parent would leave the system before being asked to wait for them

* fix boarding panel oddities from interface update

* revert UnloadBays condition

this will eventually be an issue when players get annoyed that they have to keep waiting for fighters/drones to reload, but the parenting fix is the important part

source/AI.cpp
@@ -412,9 +412,8 @@ void AI::Step(const list<shared_ptr<Ship>> &ships, const PlayerInfo &player)
 		
 		// Handle fighters:
 		const string &category = it->Attributes().Category();
-		bool isDrone = (category == "Drone");
 		bool isFighter = (category == "Fighter");
-		if(isDrone || isFighter)
+		if(it->CanBeCarried())
 		{
 			bool hasSpace = (parent && parent->BaysFree(isFighter) && !parent->GetGovernment()->IsEnemy(gov));
 			if(!hasSpace || parent->IsDestroyed() || parent->GetSystem() != it->GetSystem())
@@ -424,7 +423,7 @@ void AI::Step(const list<shared_ptr<Ship>> &ships, const PlayerInfo &player)
 				it->SetParent(parent);
 				for(const auto &other : ships)
 					if(other->GetGovernment() == gov && !other->IsDisabled()
-							&& other->GetSystem() == it->GetSystem() && !other->CanBeCarried())
+							&& other->GetSystem() == it->GetSystem() && !other->CanBeCarried() && other->CanCarry(*it.get()))
 					{
 						parent = other;
 						it->SetParent(other);

source/Engine.cpp
@@ -1294,7 +1294,7 @@ void Engine::CalculateStep()
 	}
 	
 	// Occasionally have some ship hail you.
-	if(!Random::Int(600) && !ships.empty())
+	if(!Random::Int(600) && !player.IsDead() && !ships.empty())
 	{
 		shared_ptr<Ship> source;
 		unsigned i = Random::Int(ships.size());

commit af9e646bedeac86e3e164065291087590b5f6d6c

Carried Ships find parents
Commit 11774348c22 broke the assignment of fighters to a member of their government if they did not have a free bay to belong to.
This commit randomizes the assigned parent to one of the fellow members of the same government in the same system that is also not a fighter.

source/AI.cpp
@@ -523,15 +523,28 @@ void AI::Step(const PlayerInfo &player)
 				// Handle orphaned fighters and drones.
 				parent.reset();
 				it->SetParent(parent);
+				vector<shared_ptr<Ship>> parentChoices;
+				parentChoices.reserve(ships.size() * .1);
 				for(const auto &other : ships)
-					if(other->GetGovernment() == gov && !other->IsDisabled()
-							&& other->GetSystem() == it->GetSystem() && !other->CanBeCarried() && other->CanCarry(*it.get()))
+				{
+					if(other->GetGovernment() == gov && other->GetSystem() == it->GetSystem() && !other->CanBeCarried())
 					{
-						parent = other;
-						it->SetParent(other);
-						if(other->BaysFree(isFighter))
-							break;
+						if(!other->IsDisabled() && other->CanCarry(*it.get()))
+						{
+							parent = other;
+							it->SetParent(other);
+							if(other->BaysFree(isFighter))
+								break;
+						}
+						else
+							parentChoices.emplace_back(other);
 					}
+				}
+				if(!parent && parentChoices.size())
+				{
+					parent = parentChoices[Random::Int(parentChoices.size())];
+					it->SetParent(parent);
+				}
 			}
 			else if(parent && !(it->IsYours() ? thisIsLaunching : parent->Commands().Has(Command::DEPLOY)))
 			{