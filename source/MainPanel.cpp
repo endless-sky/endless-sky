f520c0bef514d880503a9b92f3266e6d30976dd0
Michael Zahniser <mzahniser@gmail.com>
2017-07-05 11:59:21 -0400

    Typo fix.

--- source/MainPanel.cpp
+++ source/MainPanel.cpp
@@ -90,7 +90,7 @@ void MainPanel::Step()
 			isActive = !DoHelp("dead");
 		if(isActive && flagship->IsDisabled())
 			isActive = !DoHelp("disabled");
-		bool canRefuel = player.GetSystem()->HasFuelFor(flagship);
+		bool canRefuel = player.GetSystem()->HasFuelFor(*flagship);
 		if(isActive && !flagship->IsHyperspacing() && !flagship->JumpsRemaining() && !canRefuel)
 			isActive = !DoHelp("stranded");
 	}
