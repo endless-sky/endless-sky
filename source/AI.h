5b40b87af7bad93faa27a18e6fa041b48f266cd3
tehhowch <tehhowch@users.noreply.github.com>
2017-08-02 21:11:34 -0500

    Refactor AI::Step's "Find someone to assist me" section (#2861)
    * Refactor "Find someone to assist me"
     - Move into into separate function
     - Use std::vector to store possible choices
     - Randomly choose a possible helper
     - Reduce frequency of calls to avg. 2 per second
    
    Comparing shared_ptr is an automatic stored pointer comparison, e.g. implicit .get()
    
    * Add CanHelp/HasHelper functions
    
     - Enable a ship to remember who it asked to help between AI::Step calls, if any, to avoid re-evaluating find-helper logic.
     - Separate logical decision-making (should help) from physical decision making (can help).
     - Prefer asking faster ships for help.

--- source/AI.h
+++ source/AI.h
@@ -64,6 +64,9 @@ template <class Type>
 	
 	
 private:
+	void AskForHelp(Ship &ship, bool &isStranded, const Ship *flagship);
+	static bool CanHelp(const Ship &ship, const Ship &helper, const bool needsFuel);
+	bool HasHelper(const Ship &ship, const bool needsFuel);
 	// Pick a new target for the given ship.
 	std::shared_ptr<Ship> FindTarget(const Ship &ship) const;
 	
@@ -171,6 +174,7 @@ private:
 	std::map<std::weak_ptr<const Ship>, std::map<std::weak_ptr<const Ship>, int, Comp>, Comp> actions;
 	std::map<const Government *, std::map<std::weak_ptr<const Ship>, int, Comp>> governmentActions;
 	std::map<std::weak_ptr<const Ship>, int, Comp> playerActions;
+	std::map<const Ship *, std::weak_ptr<Ship>> helperList;
 	std::map<const Ship *, int> swarmCount;
 	std::map<const Ship *, Angle> miningAngle;
 	std::map<const Ship *, int> miningTime;
