eb051fd6a6a0939661e99fbd014a39753705ff34
tehhowch <tehhowchmh@gmail.com>
2017-09-25 03:42:10 -0500

    Provide visual feedback of ready-to-complete missions with gifts
     - Update the MapPanel::IsSatisfied function to use a new Mission::IsSatisfied function that checks whether or not the mission's requirements for completion are met.
     - Restore the "are required gifts in present ships cargo?" check, with guards that prevent counting cargo that is out-of-system (e.g. would not be present if the player landed immediately).

--- source/Mission.h
+++ source/Mission.h
@@ -96,6 +96,7 @@ public:
 	bool CanOffer(const PlayerInfo &player) const;
 	bool HasSpace(const PlayerInfo &player) const;
 	bool CanComplete(const PlayerInfo &player) const;
+	bool IsSatisfied(const PlayerInfo &player) const;
 	bool HasFailed(const PlayerInfo &player) const;
 	// Mark a mission failed (e.g. due to a "fail" action in another mission).
 	void Fail();
