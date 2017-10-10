8fab0c3ff4ef6ee8855f6001c810695700ddd8eb
Endless Sky <mzahniser@gmail.com>
2017-10-09 23:04:49 -0400

    Formatting fix.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -125,8 +125,8 @@ void Ship::Load(const DataNode &node)
 				armament.AddGunPort(hardpoint, outfit);
 			else
 				armament.AddTurret(hardpoint, outfit);
-			// print a warning for the first hardpoint after 32, i.e. only 1 warning per ship
-			if (armament.Get().size() == 33)
+			// Print a warning for the first hardpoint after 32, i.e. only 1 warning per ship.
+			if(armament.Get().size() == 33)
 				child.PrintTrace("Warning: ship has more than 32 weapon hardpoints. Some weapons may not fire:");
 		}
 		else if(child.Token(0) == "licenses")
