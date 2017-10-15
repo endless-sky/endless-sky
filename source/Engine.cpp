c9eab971df23d7b9cb5cab3ab0c57689300cf102
Endless Sky <mzahniser@gmail.com>
2017-10-15 15:31:16 -0400

    "Can land on: Planet" makes more sense than "Can land: planet."

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -484,8 +484,8 @@ void Engine::Step(bool isActive)
 	{
 		const StellarObject *object = flagship->GetTargetStellar();
 		string navigationMode = flagship->Commands().Has(Command::LAND) ? "Landing on:" :
-			object->GetPlanet() && object->GetPlanet()->CanLand(*flagship) ? "Can land:" :
-			"Cannot land:";
+			object->GetPlanet() && object->GetPlanet()->CanLand(*flagship) ? "Can land on:" :
+			"Cannot land on:";
 		info.SetString("navigation mode", navigationMode);
 		const string &name = object->Name();
 		info.SetString("destination", name);
