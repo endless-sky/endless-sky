a54f8d31706ca4ace20a4c5702bd77ec30ded08d
Flávio J. Saraiva <flaviojs2005@gmail.com>
2017-10-31 15:02:51 +0000

    Player mission npcs can react to events.

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -352,6 +352,9 @@ void Engine::Step(bool isActive)
 		else if(jumpCount > 0)
 			--jumpCount;
 	}
+	// Player mission npcs can react to events.
+	for(const ShipEvent &event : events)
+		player.HandleEvent(event, nullptr);
 	ai.UpdateEvents(events);
 	ai.UpdateKeys(player, clickCommands, isActive && wasActive);
 	wasActive = isActive;
