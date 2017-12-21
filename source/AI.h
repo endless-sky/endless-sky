546bc6f3ddb7a1d106ab15aa5c0fd05958121431
KiLEdEnNis <devan@zeelandnet.nl>
2017-12-22 00:58:59 +0100

    forgot to Ctrl S this file

--- source/AI.h
+++ source/AI.h
@@ -98,7 +98,7 @@ private:
 	bool DoHarvesting(Ship &ship, Command &command);
 	void DoCloak(Ship &ship, Command &command);
 	void DoScatter(Ship &ship, Command &command);
-	const bool ShouldActFrugally(const Ship &ship) const;
+	bool ShouldActFrugally(const Ship &ship) const;
 	
 	static Point StoppingPoint(const Ship &ship, const Point &targetVelocity, bool &shouldReverse);
 	// Get a vector giving the direction this ship should aim in in order to do
