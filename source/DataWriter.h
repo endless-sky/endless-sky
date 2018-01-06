a6547a3184e4515f7b9a7fcc75e11195771c0f34
tehhowch <tehhowchmh@gmail.com>
2018-01-06 00:10:53 -0600

    Extend DataNode with static methods for string parsing

--- source/DataWriter.h
+++ source/DataWriter.h
@@ -46,7 +46,7 @@ public:
 	
 	// Begin a new line that is a "child" of the previous line.
 	void BeginChild();
-	// Finish writing a block of child nodes and decrese the indentation.
+	// Finish writing a block of child nodes and decrease the indentation.
 	void EndChild();
 	
 	// Write a comment. It will be at the current indentation level, and will
