e264e8c081609fdaced5bde78457eb80064a1ea9
Hadron1776 <jababon1996@gmail.com>
2018-02-14 11:54:47 -0700

    Invert attribute comparison

--- source/Planet.cpp
+++ source/Planet.cpp
@@ -446,7 +446,7 @@ bool Planet::IsAccessible(const Ship *ship) const
 		auto end = attributes.lower_bound(PREFIX_END[i]);
 		 // If the attribute is required yet not present, or prohibited yet present, landing is not allowed.
 		for( ; it != end; ++it)
-			if(!i != !ship->Attributes().Get(it->substr(PREFIX[i].length())))
+			if(!i == !ship->Attributes().Get(it->substr(PREFIX[i].length())))
 				return false;
 	}
 	
