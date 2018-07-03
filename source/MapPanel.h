2e7f593dbc146c5fb60d8bf466c6c8bb226bb71c
tehhowch <tehhowch@users.noreply.github.com>
2018-07-03 15:14:42 -0500

    Display a map tooltip for escorts in a given system (#3587)
    * Basic Hover() support
    
    * Basic tooltip display
    
    * Tooltip displays active and parked escort counts
    
     - Tooltip shifts to remain on-screen if near screen edges
     - Special tooltip message for hovering over a system with just the flagship in it
    
    * Rework tooltip trigger to activate after 1s of dwell time
    
     - hoverSystem is guaranteed to be in escortSystems
     - Set the hover criteria on mouse move (i.e. the hover event)
     - Track dwelling time every step (i.e. the mouse doesn't have to keep moving)
     - Eliminate unused code / checks.

--- source/MapPanel.h
+++ source/MapPanel.h
@@ -18,9 +18,11 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #include "Color.h"
 #include "DistanceMap.h"
 #include "Point.h"
+#include "WrappedText.h"
 
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 class Angle;
@@ -65,6 +67,7 @@ protected:
 	// Only override the ones you need; the default action is to return false.
 	virtual bool KeyDown(SDL_Keycode key, Uint16 mod, const Command &command) override;
 	virtual bool Click(int x, int y, int clicks) override;
+	virtual bool Hover(int x, int y) override;
 	virtual bool Drag(double dx, double dy) override;
 	virtual bool Scroll(double dx, double dy) override;
 	
@@ -111,7 +114,7 @@ protected:
 	// for use in determining which governments are in the legend.
 	std::map<const Government *, double> closeGovernments;
 	// Systems in which your escorts are located.
-	std::map<const System *, bool> escortSystems;
+	std::map<const System *, std::pair<int, int>> escortSystems;
 	// Center the view on the given system (may actually be slightly offset
 	// to account for panels on the screen).
 	void CenterOnSystem(const System *system);
@@ -119,6 +122,12 @@ protected:
 	// Cache the map layout, so it doesn't have to be re-calculated every frame.
 	// The cache must be updated when the coloring mode changes.
 	void UpdateCache();
+	
+	// For tooltips:
+	int hoverCount = 0;
+	const System *hoverSystem = nullptr;
+	std::string tooltip;
+	WrappedText hoverText;
 	
 	
 private:
@@ -131,6 +140,7 @@ private:
 	void DrawSystems();
 	void DrawNames();
 	void DrawMissions();
+	void DrawTooltips();
 	void DrawPointer(const System *system, Angle &angle, const Color &color, bool bigger = false);
 	static void DrawPointer(Point position, Angle &angle, const Color &color, bool drawBack = true, bool bigger = false);
 	
