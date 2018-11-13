78439657f603eb5d9ef29f697fef47d652bbfdd5
tehhowch <tehhowchmh@gmail.com>
2018-11-12 22:56:52 -0600

    Add OSX build environment
     - Homebrew installations of the developer dependencies (per the devel readme).
       This will eventually result in a broken build when a core dependency publishes a new point release, at which point the XCode project will need to be updated.

--- test_parse.sh
+++ test_parse.sh
@@ -2,20 +2,31 @@
 if [ -z "$1" ]; then
   exit 1
 fi
-# Mark the input argument as executable
-chmod +x "$1"
-
-FILEDIR="$HOME/.local/share/endless-sky/"
+# OS Check
+if [[ $(uname) == 'Darwin' ]]; then
+  FILEDIR="$HOME/Library/Application Support/endless-sky"
+else
+  FILEDIR="$HOME/.local/share/endless-sky/"
+fi
 ERR_FILE="$FILEDIR/errors.txt"
+
 # Remove any existing error files first.
 if [ -f "$ERR_FILE" ]; then
   echo "Removing existing error file"
   rm "$ERR_FILE"
 fi
+
 # Parse the game data files (and print to the screen the results).
 "$1" -p
-# Assert there is no "errors.txt" file
-if [ -f "$ERR_FILE" ]; then
+EXIT_CODE=$?
+
+# If the game executed, then assert there is no 'errors.txt' file.
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Error executing file/command '$1'"
+  exit $EXIT_CODE
+elif [ -f "$ERR_FILE" ]; then
   echo "Assertion failed: parsing files created file 'errors.txt' in $FILEDIR"
   exit 1
+else
+  echo "Parse test completed successfully."
 fi
