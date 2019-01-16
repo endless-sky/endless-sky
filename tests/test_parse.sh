53fe39a7da07ae444a3e20352272657a3d65ede1
tehhowch <tehhowch@users.noreply.github.com>
2019-01-16 09:12:24 -0600

    Update test scripts
     - Move test files into 'tests' directory
    
    ps1:
     - Supply actual path for binary for Start-Process
     - Update OS check for non-PowerShell Core and PowerShell v < 6.0
     - Avoid closing host processes
    
    sh:
     - Use APPDATA on Windows (tested on MinGW/MSYS)

--- tests/test_parse.sh
+++ tests/test_parse.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [ -z "$1" ]; then
+  echo "You must supply a path to the binary as an argument, e.g."
+  echo "~$ ./test_parse.sh ./endless-sky"
   exit 1
 fi
 # OS Check
 if [[ $(uname) == 'Darwin' ]]; then
   FILEDIR="$HOME/Library/Application Support/endless-sky"
+elif [[ $OSTYPE == 'msys' ]] || [[ $OS == 'Windows_NT' ]] || [[ ! -z ${APPDATA:-} ]]; then
+  FILEDIR="$APPDATA/endless-sky"
 else
   FILEDIR="$HOME/.local/share/endless-sky"
 fi
@@ -16,17 +20,18 @@ if [ -f "$ERR_FILE" ]; then
   rm "$ERR_FILE"
 fi
 
-# Parse the game data files.
+# Parse the game data files, redirecting all stderr.
 "$1" -p 2>"$ERR_FILE"
 EXIT_CODE=$?
 
-# If the game executed, then assert there is no 'errors.txt' file.
+# If the game executed, then assert there is no 'errors.txt' file,
+# or that it is an empty file.
 if [ $EXIT_CODE -ne 0 ]; then
   echo "Error executing file/command '$1'"
   exit $EXIT_CODE
 elif [ -f "$ERR_FILE" ] && [ -s "$ERR_FILE" ]; then
-  echo "Assertion failed: content written to $ERR_FILE"
   cat "$ERR_FILE"
+  echo && echo "Assertion failed: content written to $ERR_FILE" && echo
   exit 1
 else
   echo "Parse test completed successfully."
