eeeaac68196c2499cb9efd3879138e1ef5b0bb0c
Amazinite <jsteck2000@gmail.com>
2019-09-08 14:31:17 -0400

    Added piercing protection

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -2633,7 +2633,7 @@ int Ship::TakeDamage(const Projectile &projectile, bool isBlast)
 	bool wasDisabled = IsDisabled();
 	bool wasDestroyed = IsDestroyed();
 	
-	double shieldFraction = 1. - weapon.Piercing();
+	double shieldFraction = 1. - (weapon.Piercing() / (1 + attributes.get("piercing protection")));
 	shieldFraction *= 1. / (1. + disruption * .01);
 	if(shields <= 0.)
 		shieldFraction = 0.;
