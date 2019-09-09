0403ba39d673c1854499f93cc1cba1f3dfc3fed7
stechio <stechio@users.noreply.github.com>
2019-09-09 18:49:38 +0200

    Highlighting of installed outfits implemented (half-tone as normal, full-tone as selected)

--- source/OutfitterPanel.cpp
+++ source/OutfitterPanel.cpp
@@ -782,7 +782,7 @@ void OutfitterPanel::DrawOutfit(const Outfit &outfit, const Point &center, bool 
 	const Sprite *back = SpriteSet::Get(
 		isSelected ? "ui/outfitter selected" : "ui/outfitter unselected");
 	SpriteShader::Draw(back, center);
-	SpriteShader::Draw(thumbnail, center, 1, 0, 0, GetItemOpacity(isSelected, isEnabled));
+	SpriteShader::Draw(thumbnail, center, 1, 0, 0, GetItemOpacity(isSelected | isOwned, isEnabled || (isSelected && isOwned)));
 	
 	// Draw the outfit name.
 	const string &name = outfit.Name();
