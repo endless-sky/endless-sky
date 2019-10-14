d8258f2aec3ddb3250e3dfd038655b344f1223a6
Amazinite <jsteck2000@gmail.com>
2019-10-14 15:00:32 -0400

    Include the maintenance costs of in cargo outfits

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -696,6 +696,12 @@ int64_t PlayerInfo::Maintenance() const
 		if(!ship->IsDestroyed())
 		{
 			maintenance += ship->Attributes().Get("maintenance costs");
+			auto outfit = ship->Cargo().Outfits().begin();
+			for(unsigned int i = 0; i < ship->Cargo().Outfits().size(); ++i)
+			{
+				maintenance += outfit->first->Get("maintenance costs") * outfit->second;
+				++outfit;
+			}
 			if(!ship->IsParked())
 				maintenance += ship->Attributes().Get("operating costs");
 		}
