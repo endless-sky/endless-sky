085a8a16e114e909c4ac7a0c827f2b41289d3aa0
Amazinite <jsteck2000@gmail.com>
2020-01-20 10:29:55 -0500

    Suggested change from @tehhowch

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2372,7 +2372,11 @@ void PlayerInfo::StepMissions(UI *ui)
 			// getting spammed by on visit dialogs if they are stacking jobs
 			// from the same destination.
 			if(visitText.empty())
-				visitText = Format::Replace(mission.GetAction(Mission::VISIT).DialogText(), substitutions);
+			{
+				const auto &text = mission.GetAction(Mission::VISIT).DialogText();
+				if(!text.empty())
+					visitText = Format::Replace(text, substitutions);
+			}
 			++missionVisits;
 		}
 	}
