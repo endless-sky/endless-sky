02e49c95d6fedbf079f726e64c41e274a2c479f0
NomadicVolcano <NomadicVolcano@gmail.com>
2020-01-26 14:06:57 -0500

    move the completion check below the waypoints and stopover check in Mission::CanComplete for efficiency

--- source/Mission.cpp
+++ source/Mission.cpp
@@ -636,10 +636,11 @@ bool Mission::CanComplete(const PlayerInfo &player) const
 // bright or dim text colors.
 bool Mission::IsSatisfied(const PlayerInfo &player) const
 {
-	if(!toComplete.Test(player.Conditions()))
+	if(!waypoints.empty() || !stopovers.empty())
 		return false;
 	
-	if(!waypoints.empty() || !stopovers.empty())
+	// Test the completion conditions for this mission.
+	if(!toComplete.Test(player.Conditions()))
 		return false;
 	
 	// Determine if any fines or outfits that must be transferred, can.
