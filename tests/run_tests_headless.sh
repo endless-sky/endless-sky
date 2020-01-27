6d51cd859d4cdf24234cdd85d6516031288300f4
Peter van der Meer <peter.vd.meer@gmail.com>
2020-01-27 22:10:53 +0100

    Add environment setting to use openGL software rendering
    Works on my environment (switches to some VMWare/Mesa OpenGL implementation)
    
    Also added a change to properly return the exit-value of the subscript

--- tests/run_tests_headless.sh
+++ tests/run_tests_headless.sh
@@ -8,6 +8,11 @@ Xvfb :99 -screen 0 1280x1024x24 &
 XVFB_PID=$!
 echo "XVFB PID: ${XVFB_PID}"
 export DISPLAY=:99
+# Force openGL software mode
+export LIBGL_ALWAYS_SOFTWARE=1
+
+echo "OpenGL settings"
+glxinfo | grep OpenGL
 
 # Enable for debugging (and add some secret password file to make it more secure):
 #
@@ -16,8 +21,13 @@ export DISPLAY=:99
 # echo "X11VNC PID: ${X11VNC_PID}"
 
 ./run_tests.sh
+RETURN_VALUE=$?
+
+
 
 kill -s SIGTERM ${XVFB_PID}
 
 # Enable for debugging:
 # kill -s SIGTERM ${X11VNC_PID}
+
+exit $RETURN_VALUE
