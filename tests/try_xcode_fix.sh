dc0eee89067f15b05fc3a17439b56051ccd2e374
Peter van der Meer <peter.vd.meer@gmail.com>
2020-02-08 20:30:36 +0100

    Add script that checks if the Xcode project is complete.
    Script can be extended to automatically try to perform a fix (and print the fix for the user to apply).

---
+++ tests/try_xcode_fix.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Determine path of the current script
+HERE=$(cd `dirname $0` && pwd)
+
+# Go to toplevel ES directory
+cd ${HERE}
+cd ..
+
+ESTOP=$(pwd)
+
+# Checkout helper script for adding Xcode/project files if they are missing
+#git clone https://github.com/zackslash/Xcode-Proj-Adder.git
+
+NUM_ADDED=0
+for FILE in $(ls -1 source)
+do
+	# Check if the file is already in the XCode project
+	cat EndlessSky.xcodeproj/project.pbxproj | grep --silent "$FILE"
+	if [ $? -ne 0 ] && [ "$FILE" != "WinApp.rc" ]
+	then
+		echo "File $FILE is missing from XCode-project"
+		NUM_ADDED=$(( NUM_ADDED + 1 ))
+		#./Xcode-Proj-Adder/bin/XcodeProjAdder \
+		#	-XCP ${ESTOP}/EndlessSky.xcodeproj/project.pbxproj \
+		#	-SSCV "../source/${FILE}"
+	fi
+done
+
+if [ ${NUM_ADDED} -gt 0 ]
+then
+	echo "You should add ${NUM_ADDED} files"
+	echo "The Xcode project file now contains:"
+	echo ""
+	cat EndlessSky.xcodeproj/project.pbxproj
+	echo ""
+fi
