7304e8a5b1369cfa967d8950c1089c363addd012
tehhowch <tehhowch@users.noreply.github.com>
2020-02-29 11:10:35 -0600

    feat: Improve XCode validation check
     - generate a patch file for CI runs
     - attempt to update paths when files are moved
     - enforce unicode file encodings

--- tests/check_xcode.sh
+++ tests/check_xcode.sh
@@ -8,72 +8,98 @@
 # mod-pbxproj should already have been installed (through PIP) before
 # running this script.
 
+has_file() {
+	fgrep -q "path = source/${1};" ${XPROJECT}/project.pbxproj
+}
 
-# Determine path of the current script
+has_encoded_file() {
+	fgrep "path = source/${1};" ${XPROJECT}/project.pbxproj | fgrep -q "fileEncoding = 4;"
+}
+
+add_new() {
+	local OPTS='--tree <group> --parent source'
+	echo -n "'${1}': "
+	python3 -m pbxproj file "${XPROJECT}" "source/${1}" ${OPTS} --target EndlessSky ${2}
+	# Make sure the file was added with Unicode encoding (fileEncoding = 4;)
+	if ! has_encoded_file ${1}; then
+		local GOOD_STR="isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c"
+		local BAD_STR="isa = PBXFileReference; lastKnownFileType = sourcecode.c"
+		local BACKUP=${XPROJECT}/project.pbxproj.bak
+		sed -i.bak s/"${BAD_STR}"/"${GOOD_STR}"/ ${XPROJECT}/project.pbxproj
+		if [ $? -ne 0 ]; then
+			echo "Using sed to enforce unicode encoding failed! Restoring backup XCode project"
+			cp "${BACKUP}" ${XPROJECT}/project.pbxproj
+		fi
+		rm -f "${BACKUP}"
+	fi
+}
+
+move_existing() {
+	# The simplest method to "move" a file in XCode is to update the referred file path. These lines look like
+	# A96862DB1AE6FD0A004FE1FE /* BankPanel.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = BankPanel.cpp; path = source/BankPanel.cpp; sourceTree = "<group>"; };
+	local basename=$(basename ${1})
+	local BACKUP=${XPROJECT}/project.pbxproj.bak
+	# Since the normal sed delimiter / is used in file paths, we will use _ as the delimiter:
+	sed -i.bak "\_path = source/${basename};_ s_/${basename}_/${1}_" ${XPROJECT}/project.pbxproj
+	if [ $? -ne 0 ]; then
+		echo "Using sed to move '${basename}' to '${1}' failed! Restoring backup XCode project"
+		cp "${BACKUP}" ${XPROJECT}/project.pbxproj
+	fi
+	rm -f "${BACKUP}"
+}
+
+# Determine path of the current script, and go to the ES project root.
 HERE=$(cd `dirname $0` && pwd)
-
-# Go to toplevel ES directory
-cd ${HERE}
-cd ..
+cd ${HERE}/..
 
 ESTOP=$(pwd)
 XPROJECT=${ESTOP}/EndlessSky.xcodeproj
 
-echo "script-dir: ${HERE}"
-echo "es-top-dir: ${ESTOP}"
-
-NUM_ADDED=0
+ADDED=()
 RESULT=0
 for FILE in $(find source -type f | sed s,^source/,, | sort)
 do
 	# Check if the file is already in the XCode project
-	grep "$FILE" ${XPROJECT}/project.pbxproj > /dev/null
-	if [ $? -ne 0 ] && [ "$FILE" != "WinApp.rc" ]
-	then
-		echo "File $FILE is missing from XCode-project, trying to add the file."
-		NO_BUILD=""
-		if [ "${FILE: -2}" == ".h" ]
-		then
-			# ES Xcode projects don't use build sections for header files.
-			echo "Not adding build section for header file"
-			NO_BUILD="--no-create-build-files"
-		fi
-		python3 -m pbxproj file ${XPROJECT} "source/${FILE}" --tree "<group>" --parent "source" --target "EndlessSky" ${NO_BUILD}
-		echo "Project to add file to XCode project ran with result $?"
-		# Check if the requested file was added
-		grep "$FILE" ${XPROJECT}/project.pbxproj > /dev/null
-		if [ $? -ne 0 ]
-		then
-			BASENAME=$(basename "${FILE}")
-			OTHERNAME=$(grep "${BASENAME}" ${XPROJECT}/project.pbxproj)
-			if [ $? -eq 0 ]
-			then
-				echo "Error: file ${FILE} not added to XCode project"
-				echo "The file does appear to be present under a different name/path:"
-				echo "${OTHERNAME}"
-				echo "Are you trying to move files? (There is no support to give suggestions for moved files.)"
-			else
-				echo "Error: file ${FILE} not added to XCode project"
-				echo "Git status:"
-				git status
-				echo ""
-				echo "Diff (between project and checked-in version)"
-				git diff ${XPROJECT}/project.pbxproj
+	if [[ "${FILE}" != "WinApp.rc" ]] && ! has_file ${FILE}; then
+		# If this file is present, but under a different path, we should update the paths.
+		BASENAME=$(basename "${FILE}")
+		if has_file ${BASENAME}; then
+			echo "'${FILE}': updating path."
+			move_existing ${FILE}
+		else
+			NO_BUILD=""
+			if [ "${FILE: -2}" == ".h" ]; then
+				# ES's Xcode project doesn't use build sections for header files.
+				NO_BUILD="--no-create-build-files"
 			fi
+			add_new ${FILE} ${NO_BUILD}
+		fi
+		
+		# If the addition or rename was unsuccessful, flag it.
+		if has_file ${FILE}; then
+			ADDED+=(${FILE})
+		else
+			echo -e "\033[0;31mFailed to add '${FILE}'\033[0m"
 			RESULT=1
 		fi
-		NUM_ADDED=$(( NUM_ADDED + 1 ))
 	fi
 done
 
-if [ ${NUM_ADDED} -gt 0 ]
-then
-	echo "You should add ${NUM_ADDED} files"
-	echo "Example of the diff to apply:"
-	echo ""
-	git diff ${XPROJECT}/project.pbxproj
-	echo ""
+if [ ${#ADDED[@]} -gt 0 ]; then
+	if [ -n "${GITHUB_ACTION}" ]; then
+		echo "Generating patch file artifact for use with 'git apply'"
+		git diff -p ${XPROJECT}/project.pbxproj > "${ESTOP}/xcode-project.patch"
+	else
+		# This script was executed locally, so the user can simply stage and commit the changes if desired.
+		echo "Updated XCode project"
+	fi
+	# If some files needed an update, but could not be automatically updated, let the user know.
+	if [ ${RESULT} -ne 0 ]; then
+		echo "\033[0;31m(Some files require manual manipulation.)\033[0m"
+	fi
 	# Also set the result to non-zero here (since the project is incomplete)
 	RESULT=1
+elif [ ${RESULT} -eq 0 ]; then
+	echo "No files added to XCode project"
 fi
 exit ${RESULT}
