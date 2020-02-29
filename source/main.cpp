ebce762f656a55f46d44b1bba090fa63b5a8b2fc
tehhowch <tehhowch@users.noreply.github.com>
2020-02-29 16:00:08 -0600

    style: Remove unexpected trailing whitespace
    The only trailing whitespace should be "continuation tabs"

--- source/main.cpp
+++ source/main.cpp
@@ -54,7 +54,7 @@ void InitConsole();
 
 
 
-// Entry point for the EndlessSky executable 
+// Entry point for the EndlessSky executable
 int main(int argc, char *argv[])
 {
 	// Handle command-line arguments
@@ -120,7 +120,7 @@ int main(int argc, char *argv[])
 	Audio::Init(GameData::Sources());
 	
 	// This is the main loop where all the action begins.
-	try { 
+	try {
 		GameLoop(player, conversation, debugMode);
 	}
 	catch(const runtime_error &error)
@@ -128,9 +128,9 @@ int main(int argc, char *argv[])
 		GameWindow::ExitWithError(error.what());
 		return 1;
 	}
-		
+	
 	// Remember the window state.
-	Preferences::Set("maximized", GameWindow::IsMaximized());		
+	Preferences::Set("maximized", GameWindow::IsMaximized());
 	Preferences::Set("fullscreen", GameWindow::IsFullscreen());
 	Screen::SetRaw(GameWindow::Width(), GameWindow::Height());
 	Preferences::Save();
@@ -143,7 +143,7 @@ int main(int argc, char *argv[])
 
 void GameLoop(PlayerInfo &player, Conversation &conversation, bool &debugMode)
 {
-	// gamePanels is used for the main panel where you fly your spaceship. 
+	// gamePanels is used for the main panel where you fly your spaceship.
 	// All other game content related dialogs are placed on top of the gamePanels.
 	// If there are both menuPanels and gamePanels, then the menuPanels take
 	// priority over the gamePanels. The gamePanels will not be shown until
@@ -167,7 +167,7 @@ void GameLoop(PlayerInfo &player, Conversation &conversation, bool &debugMode)
 			
 	bool showCursor = true;
 	int cursorTime = 0;
-	int frameRate = 60; 
+	int frameRate = 60;
 	FrameTimer timer(frameRate);
 	bool isPaused = false;
 	bool isFastForward = false;
@@ -201,7 +201,7 @@ void GameLoop(PlayerInfo &player, Conversation &conversation, bool &debugMode)
 			else if(event.type == SDL_KEYDOWN && menuPanels.IsEmpty()
 					&& Command(event.key.keysym.sym).Has(Command::MENU)
 					&& !gamePanels.IsEmpty() && gamePanels.Top()->IsInterruptible())
-			{   
+			{
 				// User pressed the Menu key.
 				menuPanels.Push(shared_ptr<Panel>(
 					new MenuPanel(player, gamePanels)));
