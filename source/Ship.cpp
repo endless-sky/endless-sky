5a5f9e5f5e062eac516df4f017f7d6efb5435952
Amazinite <jsteck2000@gmail.com>
2020-03-23 05:47:17 -0400

    feat: Negative repair costs no longer block repairing (#4932)
    Installing outfits with a negative repair cost is not yet possible though.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -72,9 +72,9 @@ namespace {
 			return;
 		
 		// Energy and fuel costs are the energy or fuel required per unit repaired.
-		if(energyCost)
+		if(energyCost > 0.)
 			available = min(available, energy / energyCost);
-		if(fuelCost)
+		if(fuelCost > 0.)
 			available = min(available, fuel / fuelCost);
 		
 		double transfer = min(available, maximum - stat);
