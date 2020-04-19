0add946a3da7768c57d896fb7320eab0244eba8a
Michał Janiszewski <janisozaur@users.noreply.github.com>
2020-04-20 00:38:25 +0200

    Apply suggestions from code review

--- source/Shader.cpp
+++ source/Shader.cpp
@@ -43,10 +43,11 @@ Shader::Shader(const char *vertex, const char *fragment)
 	
 	GLint status;
 	glGetProgramiv(program, GL_LINK_STATUS, &status);
-	if(status == GL_FALSE) {
+	if(status == GL_FALSE)
+	{
 		GLint maxLength = 0;
 		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
-		std::vector<GLchar> infoLog(maxLength);
+		vector<GLchar> infoLog(maxLength);
 		glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
 		string error(infoLog.data());
 		Files::LogError(error);
