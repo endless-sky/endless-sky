f3a10b08a2ad2359200b8152b39d51d8b2f59c1f
Amazinite <jsteck2000@gmail.com>
2020-07-27 15:09:26 -0400

    feat(preferences): Allow the "fancy" sobel filters in ShopPanels to be replaced with "fast" sprite images (#5179)

--- source/Preferences.cpp
+++ source/Preferences.cpp
@@ -57,6 +57,7 @@ void Preferences::Load()
 	settings["Draw starfield"] = true;
 	settings["Hide unexplored map regions"] = true;
 	settings["Turrets focus fire"] = true;
+	settings["Ship outlines in shops"] = true;
 	
 	DataFile prefs(Files::Config() + "preferences.txt");
 	for(const DataNode &node : prefs)
--- source/PreferencesPanel.cpp
+++ source/PreferencesPanel.cpp
@@ -52,6 +52,7 @@ namespace {
 	const string REACTIVATE_HELP = "Reactivate first-time help";
 	const string SCROLL_SPEED = "Scroll speed";
 	const string FIGHTER_REPAIR = "Repair fighters in";
+	const string SHIP_OUTLINES = "Ship outlines in shops";
 }
 
 
@@ -440,6 +441,7 @@ void PreferencesPanel::DrawSettings()
 		"Draw background haze",
 		"Draw starfield",
 		"Show hyperspace flash",
+		SHIP_OUTLINES,
 		"",
 		"Other",
 		"Clickable radar display",
@@ -501,6 +503,11 @@ void PreferencesPanel::DrawSettings()
 		{
 			isOn = true;
 			text = Preferences::Has(FIGHTER_REPAIR) ? "parallel" : "series";
+		}
+		else if(setting == SHIP_OUTLINES)
+		{
+			isOn = true;
+			text = Preferences::Has(SHIP_OUTLINES) ? "fancy" : "fast";
 		}
 		else if(setting == REACTIVATE_HELP)
 		{
