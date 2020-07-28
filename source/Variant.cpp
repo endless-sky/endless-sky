6cc4e81b80d3b772ce3eadab641de531dab62650
Amazinite <jsteck2000@gmail.com>
2020-07-28 13:01:34 -0400

    Mine

--- source/Variant.cpp
+++ source/Variant.cpp
@@ -1,5 +1,5 @@
 /* Variant.cpp
-Copyright (c) 2019 by Michael Zahniser
+Copyright (c) 2019 by Jonathan Steck
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
