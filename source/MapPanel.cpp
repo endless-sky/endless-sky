7362760c7761ac5a815948d4a6460c114ba4e826
tehhowch <tehhowch@users.noreply.github.com>
2020-08-23 11:40:24 -0500

    refactor(2 of 2): rather than read it twice
    And add comment

--- source/MapPanel.cpp
+++ source/MapPanel.cpp
@@ -546,8 +546,10 @@ void MapPanel::Select(const System *system)
 	const System *source = isJumping ? flagship->GetTargetSystem() : playerSystem;
 	
 	auto mod = SDL_GetModState();
-	bool ctrl = (SDL_GetModState() & KMOD_CTRL);
-	if (ctrl) 
+	// TODO: Whoever called Select should tell us what to do with this system vis-a-vis the travel plan, rather than
+	// possibly manipulating it both here and there. Or, we entirely separate Select from travel plan modifications.
+	bool shift = (mod & KMOD_SHIFT) && !plan.empty();
+	if(mod & KMOD_CTRL)
 		return;
 	else if(system == source && !shift)
 	{
