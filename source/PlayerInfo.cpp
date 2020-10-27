eae8de4b719ccacfbac4e2cc5242034b8309ac63
bobrobbow <67859015+bobrobbow@users.noreply.github.com>
2020-10-27 15:53:39 +0000

    fix(mechanics): Skip destroyed escorts when adding to the player's selected ships (#5468)

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -1994,7 +1994,7 @@ bool PlayerInfo::SelectShips(const Rectangle &box, bool hasShift)
 	
 	bool matched = false;
 	for(const shared_ptr<Ship> &ship : ships)
-		if(!ship->IsParked() && ship->GetSystem() == system && ship.get() != Flagship()
+		if(!ship->IsDestroyed() && !ship->IsParked() && ship->GetSystem() == system && ship.get() != Flagship()
 				&& box.Contains(ship->Position()))
 		{
 			matched = true;
