acd6679c230cf4305db743866a45e09271fcf268
tehhowch <tehhowch@users.noreply.github.com>
2020-11-03 21:38:24 -0600

    build: disable old-style-cast on calls from included deps

--- source/Music.cpp
+++ source/Music.cpp
@@ -254,7 +254,10 @@ void Music::Decode()
 		
 					// Clip and scale the sample to 16 bits.
 					sample += (1L << (MAD_F_FRACBITS - 16));
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
 					sample = max(-MAD_F_ONE, min(MAD_F_ONE - 1, sample));
+#pragma GCC diagnostic pop
 					next.push_back(sample >> (MAD_F_FRACBITS + 1 - 16));
 				}
 				// Now, the "next" buffer can be used by others. In theory, the
