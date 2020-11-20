b523f26ee603dfeca03c940d065b84cbfb7540ba
tehhowch <tehhowch@users.noreply.github.com>
2020-11-19 21:36:45 -0600

    test: disable WinCRT memory debugger

--- tests/include/es-test.hpp
+++ tests/include/es-test.hpp
@@ -7,7 +7,8 @@
 // We require SEH for Windows builds, so we test with SEH support too.
 #define CATCH_CONFIG_WINDOWS_SEH
 // Check for memory leaks from test code.
-#define CATCH_CONFIG_WINDOWS_CRTDBG
+// TODO: enable after patching upstream issue
+// #define CATCH_CONFIG_WINDOWS_CRTDBG
 #endif
 
 #include "catch.hpp"
