2919ebb1a00d3fd0fc32937c69afe199b698557a
FranchuFranchu <fff999abc999@gmail.com>
2020-11-22 10:38:12 -0300

    Fixed some check fails and updated XCode project

--- source/MenuPanel.cpp
+++ source/MenuPanel.cpp
@@ -33,6 +33,7 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #include "Sprite.h"
 #include "SpriteShader.h"
 #include "StarField.h"
+#include "StartConditionsPanel.h"
 #include "System.h"
 #include "UI.h"
 
