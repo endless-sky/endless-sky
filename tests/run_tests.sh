5cdf761632ff2bc8265d350e1e3ff476c1999056
Peter van der Meer <peter.vd.meer@gmail.com>
2020-12-08 20:44:44 +0100

    Fix counting of tests, print temporary directory only on not ok
    - The cut command uses by default the tab character as separator.
    - Fixed typo in TESTS_OK variable.
    - Print the number of the test running.
    - Print the temporary directory only if the test fails.
    - Add some empty lines before summary and before failing tests.
    - Count number of passing and failing tests and print in summary.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-NUM_FAILED=0
-
 # Determine path of the current script
 HERE=$(cd `dirname $0` && pwd)
 RESOURCES=$(echo "${HERE}/.." | sed "s,/tests/..,,")
@@ -38,19 +36,19 @@ then
 	echo "not ok 1 Could not retrieve testcases"
 	exit 1
 fi
-TESTS_OK=$(echo "${TESTS}" | grep -e "^ACTIVE" | cut -d$'\t' -f2)
-TESTS_NOK=$(echo "${TESTS}" | grep -e "^KNOWN FAILURE" -e "^MISSING FEATURE" | cut -d$'\t' -f2)
-TESTS_TOTAL=$(( 0 + $(echo "${TEST_OK}" | wc -l) ))
+TESTS_OK=$(echo -n "${TESTS}" | grep -e "^ACTIVE" | cut -f2)
+TESTS_NOK=$(echo "${TESTS}" | grep -e "^KNOWN FAILURE" -e "^MISSING FEATURE" | cut -f2)
+NUM_TOTAL=$(( 0 + $(echo "${TESTS_OK}" | wc -l) ))
 
 #TODO: Allow running known-failures by default as well (to check if they accidentally got solved)
-if [ ${TESTS_TOTAL} -eq 0 ]
+if [ ${NUM_TOTAL} -eq 0 ]
 then
 	echo "1..1"
 	echo "not ok 1 Could not find any testcases"
 	exit 1
 fi
 
-echo "1..${TESTS_TOTAL}"
+echo "1..${NUM_TOTAL}"
 
 # Set separator to newline (in case tests have spaces in their name)
 IFS_OLD=${IFS}
@@ -58,6 +56,9 @@ IFS="
 "
 
 # Run all the tests
+RUNNING_TEST=1
+NUM_FAILED=0
+NUM_OK=0
 for TEST in ${TESTS_OK}
 do
 	# Setup environment for the test
@@ -76,13 +77,15 @@ do
 
 	TEST_NAME=$(echo ${TEST} | sed "s/\"//g")
 	TEST_RESULT="ok"
-	echo "# Running test ${TEST} with ${ES_CONFIG_PATH}"
 	# Use pipefail and use sed to remove ALSA messages that appear due to missing soundcards in the CI environment
 	set -o pipefail
 	"$ES_EXEC_PATH" --resources "${RESOURCES}" --test "${TEST_NAME}" --config "${ES_CONFIG_PATH}" 2>&1 |\
 		sed -e "/^ALSA lib.*$/d" -e "/^AL lib.*$/d" | sed "s/^/# /"
 	if [ $? -ne 0 ]
 	then
+		echo ""
+		echo "# Test ${TEST} not ok"
+		echo "# temporary directory: ${ES_CONFIG_PATH}"
 		if [ -f "${ES_CONFIG_PATH}/errors.txt" ]
 		then
 			echo "# errors.txt:"
@@ -90,16 +93,20 @@ do
 		fi
 		TEST_RESULT="not ok"
 		NUM_FAILED=$((NUM_FAILED + 1))
+	else
+		NUM_OK=$((NUM_OK + 1))
 	fi
-	echo "${TEST_RESULT} ${TEST}"
+	echo "${TEST_RESULT} ${RUNNING_TEST} ${TEST}"
+	RUNNING_TEST=$(( ${RUNNING_TEST} + 1 ))
 done
 
 IFS=${IFS_OLD}
-
+echo ""
+echo "# tests ${NUM_TOTAL}"
+echo "# pass ${NUM_OK}"
 if [ ${NUM_FAILED} -ne 0 ]
 then
-	echo "# ${NUM_FAILED} tests failed"
+	echo "# failed ${NUM_FAILED}"
 	exit 1
 fi
-echo "# All executed tests passed"
 exit 0
