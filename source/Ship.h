470b0b647b646361615e4489132d869fea654428
Amazinite <jsteck2000@gmail.com>
2020-12-13 15:22:12 -0500

    Update Ship.h

--- source/Ship.h
+++ source/Ship.h
@@ -313,9 +313,6 @@ public:
 	double MaxVelocity() const;
 	double MaxReverseVelocity() const;
 	
-	// Create effects and take damage from the given hazard.
-	void TakeHazardDamage(std::vector<Visual> &visuals, const Hazard *hazard, double strength);
-	
 	// This ship just got hit by the given projectile. Take damage according to
 	// what sort of weapon the projectile it. The return value is a ShipEvent
 	// type, which may be a combination of PROVOKED, DISABLED, and DESTROYED.
@@ -323,7 +320,9 @@ public:
 	// not necessarily its primary target.
 	// Blast damage is dependent on the distance to the damage source.
 	int TakeDamage(const Projectile &projectile, bool isBlast = false);
-	int TakeDamage(const Weapon &weapon, double damageScaling, const Point &damagePosition, bool isBlast);
+	// This ship just got hit by the given hazard. Take damage according to what
+	// sort of weapon the hazard has, and create any hit effects as sparks.
+	void TakeHazardDamage(std::vector<Visual> &visuals, const Hazard *hazard, double strength);
 	// Apply a force to this ship, accelerating it. This might be from a weapon
 	// impact, or from firing a weapon, for example.
 	void ApplyForce(const Point &force);
@@ -424,6 +423,8 @@ private:
 	// Place a "spark" effect, like ionization or disruption.
 	void CreateSparks(std::vector<Visual> &visuals, const std::string &name, double amount);
 	void CreateSparks(std::vector<Visual> &visuals, const Effect *effect, double amount);
+	// A helper method for taking damage from either a projectile or a hazard.
+	int TakeDamage(const Weapon &weapon, double damageScaling, double distance, const Point &damagePosition, bool isBlast);
 	
 	
 private:
