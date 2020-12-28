16bceac3a2e3a7a4fe07152f78686c60730f3214
OOTA, Masato <ooyooxei+gh@gmail.com>
2020-12-28 21:19:53 +0900

    Change the expression of the author's name.

--- source/text/layout.hpp
+++ source/text/layout.hpp
@@ -1,5 +1,5 @@
 /* layout.hpp
-Copyright (c) 2020 by Masato Oota
+Copyright (c) 2020 by OOTA, Masato
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
