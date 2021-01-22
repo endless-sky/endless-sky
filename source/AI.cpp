b3d713f0b8d23e7a60b37ca4845d9fb62bc5c4b6
tatami4 <andrius.puksta@gmail.com>
2021-01-22 18:12:16 +0200

    add messages for escorts in player's system

--- source/AI.cpp
+++ source/AI.cpp
@@ -353,7 +353,7 @@ void AI::UpdateKeys(PlayerInfo &player, Command &activeCommands)
 	
 	if(!flagship || flagship->IsDestroyed())
 		return;
-
+	
 	if(activeCommands.Has(Command::STOP))
 		Messages::Add("Coming to a stop.");
 	
@@ -398,7 +398,7 @@ void AI::UpdateKeys(PlayerInfo &player, Command &activeCommands)
 		newOrders.target = player.FlagshipPtr();
 		IssueOrders(player, newOrders, "gathering around your flagship.");
 	}
-
+	
 	// Get rid of any invalid orders. Carried ships will retain orders in case they are deployed.
 	for(auto it = orders.begin(); it != orders.end(); )
 	{
@@ -3082,7 +3082,7 @@ double AI::RendezvousTime(const Point &p, const Point &v, double vp)
 	double discriminant = b * b - 4 * a * c;
 	if(discriminant < 0.)
 		return numeric_limits<double>::quiet_NaN();
-
+	
 	discriminant = sqrt(discriminant);
 
 	// The solutions are b +- discriminant.
@@ -3779,7 +3779,7 @@ void AI::IssueOrders(const PlayerInfo &player, const Orders &newOrders, const st
 	// If this is a move command, make sure the fleet is bunched together
 	// enough that each ship takes up no more than about 30,000 square pixels.
 	double maxSquadOffset = sqrt(10000. * squadCount);
-
+	
 	// A target is valid if we have no target, or when the target is in the
 	// same system as the flagship.
 	bool isValidTarget = !newTarget || (newTarget && player.Flagship() &&
@@ -3801,7 +3801,7 @@ void AI::IssueOrders(const PlayerInfo &player, const Orders &newOrders, const st
 			
 			gaveOrder = true;
 			hasMismatch |= !orders.count(ship);
-
+			
 			Orders &existing = orders[ship];
 			// HOLD_ACTIVE cannot be given as manual order, but we make sure here
 			// that any HOLD_ACTIVE order also matches when an HOLD_POSITION
