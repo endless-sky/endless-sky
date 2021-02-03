f02511f23f91b379c5aef3e926f0f35731ae35fa
Peter van der Meer <peter.vd.meer@gmail.com>
2021-02-04 00:02:18 +0100

    Add tests for usage of ConditionsProvider in ConditionsStore.

--- tests/src/test_conditionsStore.cpp
+++ tests/src/test_conditionsStore.cpp
@@ -12,6 +12,50 @@
 namespace { // test namespace
 
 // #region mock data
+
+class MockConditionsProvider : public ConditionsProvider
+{
+public:
+	virtual int64_t GetCondition(const std::string &name) const override;
+	virtual bool HasCondition(const std::string &name) const override;
+	virtual bool SetCondition(const std::string &name, int64_t value) override;
+	virtual bool EraseCondition(const std::string &name) override;
+
+
+public:
+	bool readOnly = false;
+	std::map<std::string, int64_t> values;
+};
+
+int64_t MockConditionsProvider::GetCondition(const std::string &name) const
+{
+	auto it = values.find(name);
+	if(it != values.end())
+		return it->second;
+	return 0;
+}
+
+bool MockConditionsProvider::HasCondition(const std::string &name) const
+{
+	auto it = values.find(name);
+	return it != values.end();
+}
+
+bool MockConditionsProvider::SetCondition(const std::string &name, int64_t value)
+{
+	if(readOnly)
+		return false;
+	values[name] = value;
+	return true;
+}
+
+bool MockConditionsProvider::EraseCondition(const std::string &name)
+{
+	if(readOnly)
+		return false;
+	values.erase(name);
+	return true;
+}
 // #endregion mock data
 
 
@@ -55,7 +99,7 @@ SCENARIO( "Creating a ConditionsStore" , "[ConditionsStore][Creation]" ) {
 	}
 }
 
-SCENARIO( "Setting and erasing conditions", "[ConditionSet][ConditionSetting]" ) {
+SCENARIO( "Setting and erasing conditions", "[ConditionStore][ConditionSetting]" ) {
 	GIVEN( "an empty starting store" ) {
 		auto store = ConditionsStore();
 		THEN( "stored values can be retrieved" ) {
@@ -88,7 +132,7 @@ SCENARIO( "Setting and erasing conditions", "[ConditionSet][ConditionSetting]" )
 	}
 }
 
-SCENARIO( "Adding and removing on condition values", "[ConditionSet][ConditionArithmetic]" ) {
+SCENARIO( "Adding and removing on condition values", "[ConditionStore][ConditionArithmetic]" ) {
 	GIVEN( "an starting store with 1 condition" ) {
 		auto store = ConditionsStore{{"myFirstVar", 10}};
 		THEN( "adding to an existing variable gives the new value" ) {
@@ -112,6 +156,87 @@ SCENARIO( "Adding and removing on condition values", "[ConditionSet][ConditionAr
 		}
 	}
 }
+
+SCENARIO( "Providing derived conditions", "[ConditionStore][DerivedConditions]" ) {
+	GIVEN( "an starting store with 1 condition" ) {
+		auto mockProv = MockConditionsProvider();
+		auto store = ConditionsStore{{"myFirstVar", 10}};
+		store.SetProviderNamed("namedRedirect1", &mockProv);
+		store.SetProviderPrefixed("prefixA: ", &mockProv);
+
+		THEN( "adding to an existing variable gives the new value" ) {
+			REQUIRE( store["myFirstVar"] == 10 );
+			REQUIRE( store.AddCondition("myFirstVar", 10) == true );
+			REQUIRE( store["myFirstVar"] == 20 );
+			REQUIRE( store.AddCondition("myFirstVar", -15) == true );
+			REQUIRE( store["myFirstVar"] == 5 );
+			REQUIRE( store.AddCondition("myFirstVar", -15) == true );
+			REQUIRE( store["myFirstVar"] == -10 );
+		}
+		THEN( "adding to an non-existing variable sets the new value" ) {
+			REQUIRE( store.GetPrimaryConditions().size() == 1 );
+			REQUIRE( store.AddCondition("mySecondVar", -30) == true );
+			REQUIRE( store["mySecondVar"] == -30 );
+			REQUIRE( store.GetPrimaryConditions().size() == 2 );
+			REQUIRE( store.HasCondition("mySecondVar") == true );
+			REQUIRE( store.AddCondition("mySecondVar", 60) == true );
+			REQUIRE( store["mySecondVar"] == 30 );
+			REQUIRE( store.GetPrimaryConditions().size() == 2 );
+		}
+		THEN( "derived named conditions should be set properly" ) {
+			REQUIRE( store.GetPrimaryConditions().size() == 1 );
+			REQUIRE( store.AddCondition("namedRedirect1", -30) == true );
+			REQUIRE( store.GetPrimaryConditions().size() == 1 );
+			REQUIRE( mockProv.values["namedRedirect1"] == -30 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			mockProv.readOnly = true;
+			REQUIRE( store.AddCondition("namedRedirect1", -20) == false );
+			REQUIRE( mockProv.values["namedRedirect1"] == -30 );
+			REQUIRE( mockProv.values.size() == 1 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			REQUIRE( store.EraseCondition("namedRedirect1") == false );
+			REQUIRE( mockProv.values["namedRedirect1"] == -30 );
+			REQUIRE( mockProv.values.size() == 1 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			REQUIRE( store.HasCondition("namedRedirect1") == true );
+			REQUIRE( store.HasCondition("namedRedirect") == false );
+		}
+		THEN( "derived prefixed conditions should be set properly" ) {
+			REQUIRE( store.GetPrimaryConditions().size() == 1 );
+			REQUIRE( store.AddCondition("prefixA: test", -30) == true );
+			REQUIRE( store.GetPrimaryConditions().size() == 1 );
+			REQUIRE( mockProv.values["prefixA: test"] == -30 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			mockProv.readOnly = true;
+			REQUIRE( store.AddCondition("prefixA: test", -20) == false );
+			REQUIRE( mockProv.values["prefixA: test"] == -30 );
+			REQUIRE( mockProv.values.size() == 1 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			REQUIRE( store.EraseCondition("prefixA: test") == false );
+			REQUIRE( mockProv.values["prefixA: test"] == -30 );
+			REQUIRE( mockProv.values.size() == 1 );
+			REQUIRE( store["myFirstVar"] == 10 );
+
+			REQUIRE( store.HasCondition("prefixA: test") == true );
+			REQUIRE( store.HasCondition("prefixA: t") == false );
+			REQUIRE( store.HasCondition("prefixA: ") == false );
+			REQUIRE( store.HasCondition("prefixA:") == false );
+
+			mockProv.values["prefixA: "] = 22;
+			mockProv.values["prefixA:"] = 21;
+			REQUIRE( store.HasCondition("prefixA: test") == true );
+			REQUIRE( store.HasCondition("prefixA: t") == false );
+			REQUIRE( store.HasCondition("prefixA: ") == true );
+			REQUIRE( store.HasCondition("prefixA:") == false );
+		}
+	}
+}
+
 // #endregion unit tests
 
 
