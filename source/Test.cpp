73647e3440b2a0963d10f9e7556819a2c1b95f7d
Peter van der Meer <peter.vd.meer@gmail.com>
2021-02-20 21:42:38 +0100

    Add navigate step to testframework

--- source/Test.cpp
+++ source/Test.cpp
@@ -158,9 +158,24 @@ void Test::LoadSequence(const DataNode &node)
 				}
 				break;
 			case TestStep::Type::NAVIGATE:
-				child.PrintTrace("Error: Not yet implemented step type navigation");
-				status = Status::BROKEN;
-				return;
+				if(child.Token(0) == "travel" && child.Size() >= 2)
+				{
+					// Using get instead of find since the test might be loaded
+					// before the actual system is loaded.
+					const System *next = GameData::Systems().Get(child.Token(1));
+					if(next)
+						step.travelPlan.push_back(next);
+				}
+				// Using get instead of find since the test might be loaded before
+				// the actual planet is loaded.
+				else if(child.Token(0) == "travel destination" && child.Size() >= 2)
+					step.travelDestination = GameData::Planets().Get(child.Token(1));
+				else
+				{
+					child.PrintTrace("Error: Invalid or incomplete keywords for navigation");
+					status = Status::BROKEN;
+				}
+				break;
 			case TestStep::Type::WATCHDOG:
 				step.watchdog = child.Size() >= 2 ? child.Value(1) : 0;
 				break;
@@ -335,7 +350,12 @@ void Test::Step(Context &context, UI &menuPanels, UI &gamePanels, PlayerInfo &pl
 				++(context.stepToRun);
 				break;
 			case TestStep::Type::NAVIGATE:
-				Fail(context, player, "Navigate not implemented");
+				player.TravelPlan().clear();
+				player.TravelPlan() = stepToRun.travelPlan;
+				if(stepToRun.travelDestination)
+					player.SetTravelDestination(stepToRun.travelDestination);
+				else
+					player.SetTravelDestination(nullptr);
 				break;
 			case TestStep::Type::WATCHDOG:
 				context.watchdog = stepToRun.watchdog;
