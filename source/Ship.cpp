f41623d9347f7ac4e2276ba800c48f6709b5bca9
10010101001 <44052322+10010101001@users.noreply.github.com>
2021-04-27 17:01:16 +1000

    Update Ship.cpp

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -1543,7 +1543,7 @@ void Ship::Move(vector<Visual> &visuals, list<shared_ptr<Flotsam>> &flotsam)
 				else
 				{
 					for(const StellarObject &object : currentSystem->Objects())
-						if(object.HasSprite() && object.HasValidPlanet()
+						if(object.HasSprite() && object.HasValidPlanet())
 								&& object.GetPlanet()->HasSpaceport())
 						{
 							target = object.Position();
