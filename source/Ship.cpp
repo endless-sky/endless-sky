44c5b0b85692c597c8e225e66519fcc0ab88f39d
tehhowch <tehhowch@users.noreply.github.com>
2021-05-09 10:49:45 -0500

    fix(mechanics): set a default value for ship drag
     - avoid possibility of divide-by-0 bugs in ShipInfoDisplay, AI, Ship
     - Print a warning when a ship has negative or 0 drag
     - Use the `VariantName` accessor when printing 'misconfigured ship' warnings
       - fix missing "outfits: " prefix

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -699,20 +699,27 @@ void Ship::FinishLoading(bool isNewInstance)
 	
 	canBeCarried = BAY_TYPES.count(attributes.Category()) > 0;
 	
-	// Issue warnings if this ship has negative outfit, cargo, weapon, or engine capacity.
+	// Issue warnings if this ship has is misconfigured, e.g. is missing required values
+	// or has negative outfit, cargo, weapon, or engine capacity.
 	string warning;
-	for(const string &attr : set<string>{"outfit space", "cargo space", "weapon capacity", "engine capacity"})
+	for(auto &&attr : set<string>{"outfit space", "cargo space", "weapon capacity", "engine capacity"})
 	{
 		double val = attributes.Get(attr);
 		if(val < 0)
 			warning += attr + ": " + Format::Number(val) + "\n";
 	}
+	if(attributes.Get("drag") <= 0.)
+	{
+		warning += "Defaulting " + string(attributes.Get("drag") ? "invalid" : "missing") + " \"drag\" attribute to 100.0\n";
+		attributes.Set("drag", 100.);
+	}
 	if(!warning.empty())
 	{
 		// This check is mostly useful for variants and stock ships, which have
 		// no names. Print the outfits to facilitate identifying this ship definition.
-		string message = (!name.empty() ? "Ship \"" + name + "\" " : "") + "(" + modelName + "):\n";
-		ostringstream outfitNames("outfits:\n");
+		string message = (!name.empty() ? "Ship \"" + name + "\" " : "") + "(" + VariantName() + "):\n";
+		ostringstream outfitNames;
+		outfitNames << "has outfits:\n";
 		for(const auto &it : outfits)
 			outfitNames << '\t' << it.second << " " + it.first->Name() << endl;
 		Files::LogError(message + warning + outfitNames.str());
@@ -3411,11 +3418,11 @@ bool Ship::CanFire(const Weapon *weapon) const
 	if(heat < -(weapon->FiringHeat() + weapon->RelativeFiringHeat() * MaximumHeat()))
 		return false;
 	// Repeat this for various effects which shouldn't drop below 0.
-	if(ionization < -(weapon->FiringIon()))
+	if(ionization < -weapon->FiringIon())
 		return false;
-	if(disruption < -(weapon->FiringDisruption()))
+	if(disruption < -weapon->FiringDisruption())
 		return false;
-	if(slowness < -(weapon->FiringSlowing()))
+	if(slowness < -weapon->FiringSlowing())
 		return false;
 	
 	return true;
