641e94c7224a321fb65345d989ee76fc93235401
Zitchas <32169904+Zitchas@users.noreply.github.com>
2021-06-04 07:40:35 -0600

    Changing the Force Effects
    This change removes the mitigating effect that was supposed to make forces affect big ships more and small ships less.
    
    Feedback appreciated.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -3176,7 +3176,7 @@ void Ship::ApplyForce(const Point &force, bool gravitational)
 	// Reduce acceleration of small ships and increase acceleration of large
 	// ones by having 20% of the force be based on a fixed mass of 500, about the
 	// mass of a human light warship:
-	acceleration += force * (.2 / 500. + .8 / currentMass);
+	acceleration += force / currentMass;
 }
 
 
