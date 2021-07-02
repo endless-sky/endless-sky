399e2370cde2ee684f7a2b715dab1c327e6dcd65
Peter van der Meer <peter.vd.meer@gmail.com>
2021-07-02 23:19:50 +0200

    feat(enhancement): Mission actions can now fine the player (#6093)

--- source/MissionAction.cpp
+++ source/MissionAction.cpp
@@ -230,6 +230,14 @@ void MissionAction::Load(const DataNode &node, const string &missionName)
 			if(child.Size() >= 3)
 				paymentMultiplier += child.Value(2);
 		}
+		else if(key == "fine" && hasValue)
+		{
+			int64_t loadedFine = child.Value(1);
+			if(loadedFine > 0)
+				fine += loadedFine;
+			else
+				child.PrintTrace("Skipping invalid \"fine\" with non-positive value:");
+		}
 		else if(key == "event" && hasValue)
 		{
 			int minDays = (child.Size() >= 3 ? child.Value(2) : 0);
@@ -320,6 +328,8 @@ void MissionAction::Save(DataWriter &out) const
 			out.Write("require", it.first->Name(), it.second);
 		if(payment)
 			out.Write("payment", payment);
+		if(fine)
+			out.Write("fine", fine);
 		for(const auto &it : events)
 		{
 			if(it.second.first == it.second.second)
@@ -515,6 +525,8 @@ void MissionAction::Do(PlayerInfo &player, UI *ui, const System *destination, co
 	
 	if(payment)
 		player.Accounts().AddCredits(payment);
+	if(fine)
+		player.Accounts().AddFine(fine);
 	
 	for(const auto &it : events)
 		player.AddEvent(*it.first, player.GetDate() + it.second.first);
@@ -558,11 +570,17 @@ MissionAction MissionAction::Instantiate(map<string, string> &subs, const System
 	result.giftOutfits = giftOutfits;
 	result.requiredOutfits = requiredOutfits;
 	result.payment = payment + (jumps + 1) * payload * paymentMultiplier;
+	result.fine = fine;
 	// Fill in the payment amount if this is the "complete" action.
 	string previousPayment = subs["<payment>"];
 	if(result.payment)
 		subs["<payment>"] = Format::Credits(abs(result.payment))
 			+ (result.payment == 1 ? " credit" : " credits");
+	
+	string previousFine = subs["<fine>"];
+	if(result.fine)
+		subs["<fine>"] = Format::Credits(result.fine)
+			+ (result.fine == 1 ? " credit" : " credits");
 	
 	if(!logText.empty())
 		result.logText = Format::Replace(logText, subs);
@@ -586,10 +604,12 @@ MissionAction MissionAction::Instantiate(map<string, string> &subs, const System
 	
 	result.conditions = conditions;
 	
-	// Restore the "<payment>" value from the "on complete" condition, for use
-	// in other parts of this mission.
+	// Restore the "<payment>" and "<fine>" values from the "on complete" condition, for
+	// use in other parts of this mission.
 	if(result.payment && trigger != "complete")
 		subs["<payment>"] = previousPayment;
+	if(result.fine && trigger != "complete")
+		subs["<fine>"] = previousFine;
 	
 	return result;
 }
