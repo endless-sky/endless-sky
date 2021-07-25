24b816605e8acdeddae1ac2cc585593118fc8553
Peter van der Meer <peter.vd.meer@gmail.com>
2021-07-25 22:35:36 +0200

    Add minimal test for account.cpp
    This test was part of https://github.com/endless-sky/endless-sky/pull/5244
    but it moved out of scope there.
    Submitting as separate PR because the test might still be useful.

---
+++ tests/src/test_account.cpp
@@ -0,0 +1,38 @@
+#include "es-test.hpp"
+
+// Include only the tested class's header.
+#include "../../source/Account.h"
+
+// ... and any system includes needed for the test file.
+#include <map>
+#include <string>
+
+
+
+namespace { // test namespace
+
+// #region mock data
+// #endregion mock data
+
+
+
+// #region unit tests
+SCENARIO( "Creating an Account" , "[Account][Creation]" ) {
+	GIVEN( "initial values" ) {
+		Account account = Account();
+		THEN( "storing and retrieving data should work" ) {
+			REQUIRE( account.Credits() == 0. );
+			account.AddCredits(100);
+			REQUIRE( account.Credits() == 100 );
+
+			REQUIRE( account.TotalDebt() == 0 );
+			account.AddFine(10000);
+			REQUIRE( account.TotalDebt() == 10000 );
+		}
+	}
+}
+// #endregion unit tests
+
+
+
+} // test namespace
