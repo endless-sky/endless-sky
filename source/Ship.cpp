eaf966527dd0bd03ae2bc00a33909816163a401d
MasterOfGrey <60949828+MasterOfGrey@users.noreply.github.com>
2021-08-07 07:15:07 +0930

    fix(mechanics): Add missing scaling factor to dragAcceleration computation (#5776)
    * Fix dragAcceleration formula
    DragAcceleration formula updated to correctly treat drag as an acceleration in the same units.
    
    The error size is small (~1.65%) but compounds to create significant problems when being used for predictive things such as stopping distances with reverse thrust.
    
    * Style fix from Amazinite

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -1806,7 +1806,7 @@ void Ship::Move(vector<Visual> &visuals, list<shared_ptr<Flotsam>> &flotsam)
 	if(acceleration)
 	{
 		acceleration *= slowMultiplier;
-		Point dragAcceleration = acceleration - velocity * (attributes.Get("drag") / mass);
+		Point dragAcceleration = acceleration - velocity * (attributes.Get("drag") / (mass / 60.));
 		// Make sure dragAcceleration has nonzero length, to avoid divide by zero.
 		if(dragAcceleration)
 		{
