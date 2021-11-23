249abe73e1c2494744b8092b78295c2e730bbda3
Amazinite <jsteck2000@gmail.com>
2021-11-22 19:55:42 -0500

    Also enforce maximum allowed values for some constants

--- source/GameRules.cpp
+++ source/GameRules.cpp
@@ -46,7 +46,23 @@ namespace {
 	// The default behavior (for those constant names not in this map) is a minimum
 	// value of 0.
 	const auto MINIMUM_ALLOWED = map<string, double>{
+		// The following constants are used in denominators and must be >0.
+		{"depreciation: max age", 1.},
+		{"flotsam: outfit: base cost", 1.},
+		{"hit force: base mass", 1.},
+		
+		// The following constants are used in Random::Int, which disallows 0.
 		{"person spawnrate", 1.},
+	};
+	
+	// A mapping of gamerule constant names to specifically-allowed minimum values.
+	// The default behavior (for those constant names not in this map) is an unbounded
+	// maximum.
+	const auto MAXIMUM_ALLOWED = map<string, double>{
+		// The following constants are percentage values that must be between 0 and 1.
+		{"depreciation: full", 1.},
+		{"depreciation: daily", 1.},
+		{"hit force: base scale", 1.},
 	};
 }
 
@@ -73,13 +89,18 @@ void GameRules::Load(const DataNode &node)
 		
 		const string &token = child.Token(1);
 		double value = (token == "true") ? 1. : ((token == "false") ? 0. : child.Value(1));
-		double minValue = 0.;
 		
 		auto it = MINIMUM_ALLOWED.find(key);
 		if(it != MINIMUM_ALLOWED.end())
-			minValue = it->second;
+			value = max(it->second, value);
+		else
+			value = max(0., value);
 		
-		rules[key] = max(minValue, value);
+		auto it = MAXIMUM_ALLOWED.find(key);
+		if(it != MAXIMUM_ALLOWED.end())
+			value = min(value, it->second);
+		
+		rules[key] = value;
 	}
 }
 
