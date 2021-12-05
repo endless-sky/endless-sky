endless-sky/endless-sky

commit 221cd58453dbf5fb621aa7f3e8cbc969526a622a

Process review comments
Removed redundant braces.
Use auto keyword more.
Made some variables more local.
Used shared_ptrs as pointers, not as references to pointers.

source/Engine.cpp
@@ -1615,10 +1615,13 @@ void Engine::MoveShip(const shared_ptr<Ship> &ship)
 	
 	// Boarding:
 	bool autoPlunder = !ship->IsYours();
-	shared_ptr<Ship> originalTarget = ship->GetTargetShip();
 	shared_ptr<Ship> victim = ship->Board(autoPlunder);
-	if(ship.get() == flagship && !ship->GetSystem() && originalTarget)
-		ChangePlayerFlagship(player.FlagshipPtr(), originalTarget);
+	if(ship.get() == flagship && !ship->GetSystem())
+	{
+		shared_ptr<Ship> originalTarget = ship->GetTargetShip();
+		if(originalTarget)
+			ChangePlayerFlagship(player.FlagshipPtr(), originalTarget);
+	}
 	if(victim)
 		eventQueue.emplace_back(ship, victim,
 			ship->GetGovernment()->IsEnemy(victim->GetGovernment()) ?
@@ -1641,7 +1644,7 @@ void Engine::MoveShip(const shared_ptr<Ship> &ship)
 
 // Changing the players flagship during flight requires an update of all active
 // data that references the players flagship.
-void Engine::ChangePlayerFlagship(const shared_ptr<Ship> oldFlagship, shared_ptr<Ship> &newFlagship)
+void Engine::ChangePlayerFlagship(const shared_ptr<Ship> oldFlagship, shared_ptr<Ship> newFlagship)
 {
 	// Remove active data in the old flagship.
 	if(oldFlagship)
@@ -1652,13 +1655,9 @@ void Engine::ChangePlayerFlagship(const shared_ptr<Ship> oldFlagship, shared_ptr
 	
 	// Update all ships that still have the old flagship as parent, for example
 	// NPCs that are following the player.
-	for(std::shared_ptr<Ship>& ship : ships)
-	{
+	for(auto& ship : ships)
 		if(ship && ship->GetParent() == oldFlagship)
-		{
 			ship->SetParent(newFlagship);
-		}
-	}
 }
 
 

source/Engine.h
@@ -102,7 +102,7 @@ private:
 	void CalculateStep();
 	
 	void MoveShip(const std::shared_ptr<Ship> &ship);
-	void ChangePlayerFlagship(const std::shared_ptr<Ship> oldFlagship, std::shared_ptr<Ship> &newFlagship);
+	void ChangePlayerFlagship(const std::shared_ptr<Ship> oldFlagship, std::shared_ptr<Ship> newFlagship);
 	
 	void SpawnFleets();
 	void SpawnPersons();

commit 65c365a89e94c651535e0c3bcc481f3dab6c1f08

Fix issue introduced in 221cd58453dbf5fb621aa7f3e8cbc969526a622a
The originalTarget was requested after it was already cleared,
resulting in the new flagship not getting found.
Updated the code to look for a parent on the flagship instead of
trying to retrieve the orginal target.
Also added an error-message if the lookup fails and a corrective
action that restores the system.

source/Engine.cpp
@@ -1618,9 +1618,15 @@ void Engine::MoveShip(const shared_ptr<Ship> &ship)
 	shared_ptr<Ship> victim = ship->Board(autoPlunder);
 	if(ship.get() == flagship && !ship->GetSystem())
 	{
-		shared_ptr<Ship> originalTarget = ship->GetTargetShip();
-		if(originalTarget)
-			ChangePlayerFlagship(player.Flagship(), *originalTarget);
+		// If the players flagship just docked, then it got the new flagship as parent.
+		shared_ptr<Ship> newFlag = ship->GetParent();
+		if(newFlag)
+			ChangePlayerFlagship(player.Flagship(), *newFlag);
+		else
+		{
+			Files::LogError("Engine::MoveShip: Undocked flagship without system!");
+			ship->SetSystem(player.GetSystem());
+		}
 	}
 	if(victim)
 		eventQueue.emplace_back(ship, victim,