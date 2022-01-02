0ee03842398658d4b765613582e3921489c0ce2a
tehhowch <tehhowch@users.noreply.github.com>
2022-01-02 17:10:44 -0600

    chore: use same control flow as before
    rather than two returns from the same conditional block

--- source/OutfitterPanel.cpp
+++ source/OutfitterPanel.cpp
@@ -362,11 +362,12 @@ void OutfitterPanel::Buy(bool alreadyOwned)
 		int mapSize = selectedOutfit->Get("map");
 		if(mapSize > 0)
 		{
-			if(player.HasMapped(mapSize))
-				return;
-			player.Map(mapSize);
-			int64_t price = player.StockDepreciation().Value(selectedOutfit, day);
-			player.Accounts().AddCredits(-price);
+			if(!player.HasMapped(mapSize))
+			{
+				player.Map(mapSize);
+				int64_t price = player.StockDepreciation().Value(selectedOutfit, day);
+				player.Accounts().AddCredits(-price);
+			}
 			return;
 		}
 
