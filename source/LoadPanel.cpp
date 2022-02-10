97043361b0ef26c6b10195763f06e4261598aa9e
quyykk <quyykk@protonmail.com>
2022-02-10 14:01:32 +0100

    feat(mechanics): Require confirmation when deleting a pilot (#6433)
    * Ask user to confirm the name of the pilot to delete.
    
    * Address review comments.
    
    * Address review comments.
    
    * Remove name of unused parameter.

--- source/LoadPanel.cpp
+++ source/LoadPanel.cpp
@@ -217,7 +217,9 @@ bool LoadPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &command, boo
 	{
 		GetUI()->Push(new Dialog(this, &LoadPanel::DeletePilot,
 			"Are you sure you want to delete the selected pilot, \"" + selectedPilot
-				+ "\", and all their saved games?\n\n(This will permanently delete the pilot data.)"));
+				+ "\", and all their saved games?\n\n(This will permanently delete the pilot data.)\n"
+				+ "Confirm the name of the pilot you want to delete.",
+				[this](const string &pilot) { return pilot == selectedPilot; }));
 	}
 	else if(key == 'a' && !player.IsDead() && player.IsLoaded())
 	{
@@ -507,7 +509,7 @@ void LoadPanel::LoadCallback()
 
 
 
-void LoadPanel::DeletePilot()
+void LoadPanel::DeletePilot(const string &)
 {
 	loadedInfo.Clear();
 	if(selectedPilot == player.Identifier())
