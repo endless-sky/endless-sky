b65d33c3c0e25960fdacd83083b92d74fd6506b8
quyykk <quyykk@protonmail.com>
2022-02-24 16:44:45 +0100

    Add a CI check to validate Code::Blocks project files.

---
+++ tests/check_codeblocks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script that checks if the Code::Blocks project is complete.
+# TODO: If the project is incomplete generate a patch file to make it complete.
+
+# Determine path of the current script, and go to the ES project root.
+HERE=$(cd `dirname $0` && pwd)
+cd ${HERE}/..
+
+ESTOP=$(pwd)
+CDPROJECT=${ESTOP}/EndlessSkyLib.cbp
+CDTPROJECT=${ESTOP}/EndlessSkyTests.cbp
+
+RESULT=0
+
+for FILE in $(find source -type f -not -name WinApp.rc -not -name main.cpp | sed s,^source/,, | sort)
+do
+  # Check if the file is already in the general Code::Blocks project.
+  if ! fgrep -q "${FILE}" ${CDPROJECT}; then
+    echo -e "${FILE}"
+    RESULT=1
+  fi
+done
+
+for FILE in $(find tests/src/ -type f -name "*.h" -o -name "*.cpp" | sed s,^tests/src/,, | sort)
+do
+  # Check if the file is already in the test Code::Blocks project.
+  if ! fgrep -q "${FILE}" ${CDTPROJECT}; then
+    echo -e "${FILE}"
+    RESULT=1
+  fi
+done
+
+exit ${RESULT}
