67d11c26311e1a499ffd4a11dd4f1f028182140d
quyykk <quyykk@protonmail.com>
2022-03-04 13:22:47 +0100

    Second attempt.

--- source/main.cpp
+++ source/main.cpp
@@ -125,7 +125,8 @@ int main(int argc, char *argv[])
 		// If we are not using the UI, or performing some automated task, we should load
 		// all data now. (Sprites and sounds can safely be deferred.)
 		if(isConsoleOnly || !testToRunName.empty())
-			dataLoading.get();
+			while(dataLoading.wait_for(chrono::seconds(0)) != future_status::ready)
+				std::this_thread::yield();
 
 		if(!testToRunName.empty() && !GameData::Tests().Has(testToRunName))
 		{
