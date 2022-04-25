ffdaf242378b6a54c72cee7b42289a5caa0bf476
Peter van der Meer <peter.vd.meer@gmail.com>
2022-04-25 22:43:56 +0200

    Filter /unit/tests/includes out of the check for Code::Blocks.

--- tests/check_codeblocks.sh
+++ tests/check_codeblocks.sh
@@ -26,7 +26,7 @@ do
   fi
 done
 
-for FILE in $(find tests/unit/ -type f -name "*.h" -o -name "*.cpp" | sed s,^tests/unit/,, | sort)
+for FILE in $(find tests/unit/ -type f -name "*.h" -o -name "*.cpp" | grep -v -e "^tests/unit/include" | sed s,^tests/unit/,, | sort)
 do
   # Check if the file is already in the test Code::Blocks project.
   if ! fgrep -q "${FILE}" "${CBTPROJECT}"; then
