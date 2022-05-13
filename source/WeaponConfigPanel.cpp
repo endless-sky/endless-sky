cb811c99e767220f8720485ce797c9767bfd4d65
warp-core <warp-core@users.noreply.github.com>
2022-05-13 02:05:24 +0100

    Some progress, maybe.

---
+++ source/WeaponConfigPanel.cpp
@@ -0,0 +1,199 @@
+/*WeaponConfigPanel.cpp
+Copyright (c) 2022 by warpcore
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#include "WeaponConfigPanel.h"
+
+WeaponConfigPanel::WeaponConfigPanel(PlayerInfo &player)
+	: WeaponConfigPanel(player, InfoPanelState(player))
+{
+}
+
+WeaponConfigPanel::WeaponConfigPanel(PlayerInfo &player, InfoPanelState panelState)
+	: player(player), panelState(panelState)
+{
+	shipIt = this->panelState.Ships().begin();
+	SetInterruptible(false);
+
+	// If a valid ship index was given, show that ship.
+	if(static_cast<unsigned>(panelState.SelectedIndex()) < player.Ships().size())
+		shipIt += panelState.SelectedIndex();
+	else if(player.Flagship())
+	{
+		// Find the player's flagship. It may not be first in the list, if the
+		// first item in the list cannot be a flagship.
+		while(shipIt != player.Ships().end() && shipIt->get() != player.Flagship())
+			++shipIt;
+	}
+
+	UpdateInfo();
+}
+
+
+
+void WeaponConfigPanel::Step()
+{
+    DoHelp("weapon config");
+}
+
+
+
+void WeaponConfigPanel::Draw()
+{
+	// Dim everything behind this panel.
+	DrawBackdrop();
+
+	// Fill in the information for how this interface should be drawn.
+	Information interfaceInfo;
+	interfaceInfo.SetCondition("weapon config panel");
+	/*if(panelState.CanEdit() && (shipIt != player.Ships().end())
+			&& (shipIt->get() != player.Flagship() || (*shipIt)->IsParked()))
+	{
+		interfaceInfo.SetCondition((*shipIt)->IsParked() ? "show unpark" : "show park");
+		interfaceInfo.SetCondition("show disown");
+	}
+	else if(!panelState.CanEdit())
+	{
+		interfaceInfo.SetCondition("show dump");
+		if(CanDump())
+			interfaceInfo.SetCondition("enable dump");
+	}*/
+	if(player.Ships().size() > 1)
+		interfaceInfo.SetCondition("five buttons");
+	else
+		interfaceInfo.SetCondition("three buttons");
+
+	// Draw the interface.
+	const Interface *weaponConfigPanelUi = GameData::Interfaces().Get("info panel");
+	weaponConfigPanelUi->Draw(interfaceInfo, this);
+
+	// Draw all the different information sections.
+	ClearZones();
+	if(shipIt == player.Ships().end())
+		return;
+	DrawWeapons(weaponConfigPanelUi->GetBox("weapons"));
+
+	// If the player hovers their mouse over a ship attribute, show its tooltip.
+	info.DrawTooltips();
+}
+
+
+
+bool WeaponConfigPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &command, bool /* isNewPress */)
+{
+	bool control = (mod & (KMOD_CTRL | KMOD_GUI));
+	bool shift = (mod & KMOD_SHIFT);
+	if(key == 'd' || key == SDLK_ESCAPE || (key == 'w' && control))
+		GetUI()->Pop(this);
+	else if(!player.Ships().empty() && ((key == 'p' && !shift) || key == SDLK_LEFT || key == SDLK_UP))
+	{
+		if(shipIt == panelState.Ships().begin())
+			shipIt = panelState.Ships().end();
+		--shipIt;
+		UpdateInfo();
+	}
+	else if(!panelState.Ships().empty() && (key == 'n' || key == SDLK_RIGHT || key == SDLK_DOWN))
+	{
+		++shipIt;
+		if(shipIt == panelState.Ships().end())
+			shipIt = panelState.Ships().begin();
+		UpdateInfo();
+	}
+	else if(key == 'i' || command.Has(Command::INFO) || (control && key == SDLK_TAB))
+	{
+		// Set scroll so the currently shown ship will be the first in page.
+		panelState.SetScroll(shipIt - panelState.Ships().begin());
+
+		GetUI()->Pop(this);
+		GetUI()->Push(new PlayerInfoPanel(player, std::move(panelState)));
+	}
+	else if(panelState.CanEdit() && (key == 'P' || (key == 'p' && shift)))
+	{
+		if(shipIt->get() != player.Flagship() || (*shipIt)->IsParked())
+			player.ParkShip(shipIt->get(), !(*shipIt)->IsParked());
+	}
+	else if(command.Has(Command::MAP) || key == 'm')
+		GetUI()->Push(new MissionPanel(player));
+	else if(key == 'l' && player.HasLogs())
+		GetUI()->Push(new LogbookPanel(player));
+	else
+		return false;
+
+	return true;
+}
+
+
+
+bool WeaponConfigPanel::Click(int x, int y, int /* clicks */)
+{
+	if(shipIt == panelState.Ships().end())
+		return true;
+
+	draggingIndex = -1;
+	if(panelState.CanEdit() && hoverIndex >= 0 && (**shipIt).GetSystem() == player.GetSystem() && !(**shipIt).IsDisabled())
+		draggingIndex = hoverIndex;
+
+	return true;
+}
+
+
+
+bool WeaponConfigPanel::Hover(int x, int y)
+{
+	Point point(x, y);
+	info.Hover(point);
+	return Hover(point);
+}
+
+
+
+bool WeaponConfigPanel::Drag(double dx, double dy)
+{
+	return Hover(hoverPoint + Point(dx, dy));
+}
+
+
+
+bool WeaponConfigPanel::Release(int /* x */, int /* y */)
+{
+	if(draggingIndex >= 0 && hoverIndex >= 0 && hoverIndex != draggingIndex)
+		(**shipIt).GetArmament().Swap(hoverIndex, draggingIndex);
+
+	draggingIndex = -1;
+	return true;
+}
+
+
+
+void WeaponConfigPanel::UpdateInfo()
+{
+	draggingIndex = -1;
+	hoverIndex = -1;
+	ClearZones();
+	if(shipIt == panelState.Ships().end())
+		return;
+
+	const Ship &ship = **shipIt;
+	if(player.Flagship() && ship.GetSystem() == player.GetSystem() && &ship != player.Flagship())
+		player.Flagship()->SetTargetShip(*shipIt);
+
+	outfits.clear();
+	for(const auto &it : ship.Outfits())
+		outfits[it.first->Category()].push_back(it.first);
+}
+
+
+
+void WeaponConfigPanel::ClearZones()
+{
+	zones.clear();
+}
+
---
+++ source/WeaponConfigPanel.h
@@ -0,0 +1,72 @@
+/*WeaponConfigPanel.h
+Copyright (c) 2022 by warpcore
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*/
+
+#ifndef WEAPONCONFIGPANEL_H_
+#define WEAPONCONFIGPANEL_H_
+
+#include "Panel.h"
+
+#include "ClickZone.h"
+#include "Point.h"
+#include "text/layout.hpp"
+
+class WeaponConfigPanel : public Panel {
+public:
+    explicit ShipInfoPanel(PlayerInfo &player);
+	explicit ShipInfoPanel(PlayerInfo &player, InfoPanelState state);
+
+	virtual void Step() override;
+	virtual void Draw() override;
+
+
+protected:
+	// Only override the ones you need; the default action is to return false.
+	virtual bool KeyDown(SDL_Keycode key, Uint16 mod, const Command &command, bool isNewPress);
+	virtual bool Click(int x, int y, int clicks);
+	virtual bool RClick(int x, int y);
+	virtual bool Hover(int x, int y);
+	virtual bool Drag(double dx, double dy);
+	virtual bool Release(int x, int y);
+	virtual bool Scroll(double dx, double dy);
+
+
+private:
+    // Handle a change to what ship is shown.
+	void UpdateInfo();
+	void ClearZones();
+
+	void DrawWeapons(const Rectangle &bounds);
+
+	// Helper functions.
+	void DrawLine(const Point &from, const Point &to, const Color &color) const;
+	bool Hover(const Point &point);
+
+
+private:
+	PlayerInfo &player;
+	// This is the currently selected ship.
+	std::vector<std::shared_ptr<Ship>>::const_iterator shipIt;
+
+	// Track all the clickable parts of the UI (other than the buttons).
+	std::vector<ClickZone<int>> zones;
+	// Keep track of which item the mouse is hovering over and which item is
+	// currently being dragged.
+	int hoverIndex = -1;
+	int draggingIndex = -1;
+
+	InfoPanelState panelState;
+
+	// Track the current mouse location.
+	Point hoverPoint;
+};
+
+#endif
