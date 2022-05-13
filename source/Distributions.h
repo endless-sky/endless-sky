400d9d2964c0ab6ef2f3b8abd4a67e261fe7b096
DeBlister <80174383+DeBlister@users.noreply.github.com>
2022-05-12 22:01:07 -0400

    Update copyright

--- source/Distributions.h
+++ source/Distributions.h
@@ -1,5 +1,5 @@
 /* Distributions.h
-Copyright (c) 2014 by Michael Zahniser
+Copyright (c) 2022 by DeBlister
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
