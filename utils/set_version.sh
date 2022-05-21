9ea69ba81e793f17bd5cf8672b5b67714c63dd63
quyykk <quyykk@protonmail.com>
2022-05-21 18:43:43 +0200

    Fix remaining style violations.

--- utils/set_version.sh
+++ utils/set_version.sh
@@ -12,19 +12,19 @@ isVersion=$?
 if [[ ${isVersion} -eq 0 ]]; then
     replacement="ver. $1"
 elif [[ $1 =~ ^[0-9A-Fa-f]+$ ]]; then
-	replacement="($1)"
-	perl -p -i -e "s/(\"Endless Sky .*)\"/\$1 ${replacement}\"/ig" source/main.cpp
-	perl -p -i -e "s/\".*?\" \"(ver\. .*)\" \"/\"$(date '+%d %b %Y')\" \"\$1 ${replacement}\" \"/ig" endless-sky.6
-	isVersion=2
+    replacement="($1)"
+    perl -p -i -e "s/(\"Endless Sky .*)\"/\$1 ${replacement}\"/ig" source/main.cpp
+    perl -p -i -e "s/\".*?\" \"(ver\. .*)\" \"/\"$(date '+%d %b %Y')\" \"\$1 ${replacement}\" \"/ig" endless-sky.6
+    isVersion=2
 else
     replacement=$1
 fi
 
 if [[ ${isVersion} -ne 2 ]]; then
-	# Update main.cpp
-	perl -p -i -e "s/(\"Endless Sky) .+?\"/\$1 ${replacement}\"/ig" source/main.cpp
-	# Update endless-sky.6
-	perl -p -i -e "s/\".*?\" \"ver\. .+?\"/\"$(date '+%d %b %Y')\" \"${replacement}\"/ig" endless-sky.6
+    # Update main.cpp
+    perl -p -i -e "s/(\"Endless Sky) .+?\"/\$1 ${replacement}\"/ig" source/main.cpp
+    # Update endless-sky.6
+    perl -p -i -e "s/\".*?\" \"ver\. .+?\"/\"$(date '+%d %b %Y')\" \"${replacement}\"/ig" endless-sky.6
 fi
 
 rm -rf source/main.cpp.bak
