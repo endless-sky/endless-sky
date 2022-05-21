ea0cdf9a30401dda988b0958cd171a4697f28afb
Sam Gleske <sam.mxracer@gmail.com>
2022-05-21 12:50:34 -0400

    Address feedback from tehhowch
    - Use `map::find` and boundary checking with `map::end`.
    - Thanks @quyykk for logic suggestions on how to do this in stream.

--- source/AI.cpp
+++ source/AI.cpp
@@ -1058,11 +1058,13 @@ void AI::AskForHelp(Ship &ship, bool &isStranded, const Ship *flagship)
 			if((helper->IsYours() && !ship.IsYours()) || helper.get() == flagship)
 				continue;
 			// Your escorts should not help each other if already under orders.
-			int helperOrders = 0;
-			if(orders.count(helper.get()))
-				helperOrders = orders.find(helper.get())->second.type;
-			if(helper->IsYours() && ship.IsYours() && orders.count(helper.get()) && helperOrders != Orders::MINING && helperOrders != Orders::HARVEST)
-				continue;
+			auto foundOrders = orders.find(helper.get());
+			if(foundOrders != orders.end())
+			{
+				int helperOrders = foundOrders->second.type;
+				if(helper->IsYours() && ship.IsYours() && helperOrders != Orders::MINING && helperOrders != Orders::HARVEST)
+					continue;
+			}
 
 			// Check if this ship is physically able to help.
 			if(!CanHelp(ship, *helper, isStranded))
