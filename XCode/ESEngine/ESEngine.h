9c37537f8e78021a2d5158c92098a72c334abc3b
DeBlister <80174383+DeBlister@users.noreply.github.com>
2022-05-22 17:56:25 -0400

    Point build targets to new engine target
    The game target and unit tests target now depend on the ESEngine framework.  In order to facilitate this, the project settings have been changed to place Xcode's Derived Data under build/Xcode.
    
    SDL is no longer packaged explicitly, but instead is packaged within ESEngine (which itself is packaged explicitly).
    
    The main build targets should take the appropriate ESEngine product depending on if they're building for Release or for Debug. (could not be tested due to warnings in ESEngine Debug becoming errors in ESEngine Release)
    
    Changed bundle version and bundle version string to point to Current Project Version and Marketing Version, respectively.  In order to facilitate this, the Current Project Version has been set to 0.9.14 and Marketing Version points to Current Project Version.

---
+++ XCode/ESEngine/ESEngine.h
@@ -0,0 +1,18 @@
+//
+//  ESEngineFramework.h
+//  ESEngineFramework
+//
+//  Created by Adam Asadoorian on 5/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ESEngineFramework.
+FOUNDATION_EXPORT double ESEngineFrameworkVersionNumber;
+
+//! Project version string for ESEngineFramework.
+FOUNDATION_EXPORT const unsigned char ESEngineFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESEngineFramework/PublicHeader.h>
+
+
