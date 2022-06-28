e6ccf6ef8d0d933275e371a21761c709d5bce0f0
warp-core <warp-core@users.noreply.github.com>
2022-06-28 09:28:08 +0100

    `"flagship:"` -> `"flagship model:"`

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2588,7 +2588,7 @@ void PlayerInfo::UpdateAutoConditions(bool isBoarding)
 	};
 	// Clear any existing ships: conditions. (Note: '!' = ' ' + 1.)
 	clearRange(conditions, "ships: ", "ships:!");
-	clearRange(conditions, "flagship: ", "flagship:!");
+	clearRange(conditions, "flagship model: ", "flagship model:!");
 	// Store special conditions for cargo and passenger space.
 	conditions["cargo space"] = 0;
 	conditions["passenger space"] = 0;
@@ -2600,7 +2600,7 @@ void PlayerInfo::UpdateAutoConditions(bool isBoarding)
 			++conditions["ships: " + ship->Attributes().Category()];
 		}
 	if(flagship)
-		++conditions["flagship: " + flagship->ModelName()];
+		++conditions["flagship model: " + flagship->ModelName()];
 	// If boarding a ship, missions should not consider the space available
 	// in the player's entire fleet. The only fleet parameter offered to a
 	// boarding mission is the fleet composition (e.g. 4 Heavy Warships).
