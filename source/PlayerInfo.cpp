63eec2630749ddfbcc60d263acd0b44e8d82888e
Nick <quyykk@protonmail.com>
2022-07-02 18:29:04 +0200

    Apply suggestions from code review
    Co-authored-by: Ben Hauch <tehhowch@users.noreply.github.com>

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -2551,13 +2551,13 @@ void PlayerInfo::ValidateLoad()
 
 	// Validate the missions that were loaded. Active-but-invalid missions are removed from
 	// the standard mission list, effectively pausing them until necessary data is restored.
-	auto mit = partition(missions.begin(), missions.end(), mem_fn(&Mission::IsValid));
+	auto mit = stable_partition(missions.begin(), missions.end(), mem_fn(&Mission::IsValid));
 	if(mit != missions.end())
 		inactiveMissions.splice(inactiveMissions.end(), missions, mit, missions.end());
 
 	// Invalid available jobs or missions are erased (since there is no guarantee
 	// the player will be on the correct planet when a plugin is re-added).
-	auto isInvalidMission = [](const Mission& m) noexcept -> bool { return !m.IsValid(); };
+	auto isInvalidMission = [](const Mission &m) noexcept -> bool { return !m.IsValid(); };
 	availableJobs.remove_if(isInvalidMission);
 	availableMissions.remove_if(isInvalidMission);
 }
