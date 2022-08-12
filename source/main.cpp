a4eeb3e515d3e5f5cf6cb3ae2e2b30d9c34da91c
warp-core <warp-core@users.noreply.github.com>
2022-08-12 19:38:57 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -106,7 +106,8 @@ int main(int argc, char *argv[])
 		else if(arg == "--tests")
 			printTests = true;
 		else if(arg == "-s" || arg == "--ships" || arg == "-w" || arg == "--weapons" ||
-				arg == "-o" || arg == "--outffits")
+				arg == "-o" || arg == "--outffits" || arg == "-e" || arg == "--engines" ||
+				arg == "--power")
 		{
 			printData = true;
 		}
