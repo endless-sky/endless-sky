f06a091d05eebc83f09fcfba2675e5bb81e852bb
Kari Pahula <kaol@iki.fi>
2022-08-23 02:52:12 +0300

    fix(AI): Use unconstrained personality for defense fleet carried ships

--- source/Personality.cpp
+++ source/Personality.cpp
@@ -370,6 +370,18 @@ Personality Personality::Defender()
 
 
 
+// Remove target and marked since the defender defeat check doesn't actually care
+// about carried ships.  Remove heroic and staying to make them stick with carriers
+// but use unconstrained to have targeting work outside of the fence.
+Personality Personality::DefenderFighter()
+{
+	Personality defender;
+	defender.flags = UNCONSTRAINED;
+	return defender;
+}
+
+
+
 void Personality::Parse(const DataNode &node, int index, bool remove)
 {
 	const string &token = node.Token(index);
