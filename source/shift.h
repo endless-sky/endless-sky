77ec96b70e27dfdbcdc3f62eaeb0c62c9235f0f4
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2022-09-04 23:27:38 +0200

    Oops

--- source/shift.h
+++ source/shift.h
@@ -11,12 +11,6 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
-
-You should have received a copy of the GNU General Public License along with
-this program.  If not, see <https://www.gnu.org/licenses/>.
-
-You should have received a copy of the GNU General Public License along with
-this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef SHIFT_H_
