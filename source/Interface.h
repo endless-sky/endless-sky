8df4e02c08677b71b4925264e3a1196c714391ce
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2022-09-04 12:42:27 +0200

    Fix copyright

--- source/Interface.h
+++ source/Interface.h
@@ -7,7 +7,7 @@ Foundation, either version 3 of the License, or (at your option) any later versi
 
 Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
 */
 
 #ifndef INTERFACE_H_
