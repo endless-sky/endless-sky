a68f2ed1cdb189867ab859bf9548515fe3a0b573
warp-core <warp-core@users.noreply.github.com>
2022-09-25 17:31:27 +0100

    There's always something

--- source/System.cpp
+++ source/System.cpp
@@ -121,7 +121,7 @@ void System::Load(const DataNode &node, Set<Planet> &planets)
 		// Check for conditions that require clearing this key's current value.
 		// "remove <key>" means to clear the key's previous contents.
 		// "remove <key> <value>" means to remove just that value from the key.
-		bool removeAll = (remove && !hasValue && key != "object");
+		bool removeAll = (remove && !hasValue && (key != "object" || !child.HasChildren()));
 		// If this is the first entry for the given key, and we are not in "add"
 		// or "remove" mode, its previous value should be cleared.
 		bool overwriteAll = (!add && !remove && shouldOverwrite.count(key));
