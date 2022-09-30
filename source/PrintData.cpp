16765587cf50690a0ea772455dcaa3c92709b9ff
warp-core <warp-core@users.noreply.github.com>
2022-09-30 18:08:15 +0100

    Update source/PrintData.cpp
    Co-authored-by: Hurleveur <94366726+Hurleveur@users.noreply.github.com>

--- source/PrintData.cpp
+++ source/PrintData.cpp
@@ -131,7 +131,7 @@ void PrintData::PrintBaseShipStats()
 {
 	cout << "model" << ',' << "category" << ',' << "chassis cost" << ',' << "loaded cost" << ',' << "shields" << ','
 		<< "hull" << ',' << "mass" << ',' << "drag" << ',' << "heat dissipation" << ','
-		<< "crew" << ',' << "bunks" << ',' << "cargo" << ',' << "fuel" << ','
+		<< "required crew" << ',' << "bunks" << ',' << "cargo" << ',' << "fuel" << ','
 		<< "outfit" << ',' << "weapon" << ',' << "engine" << ',' << "gun mounts" << ','
 		<< "turret mounts" << ',' << "fighter bays" << ',' << "drone bays" << '\n';
 	for(auto &it : GameData::Ships())
