4eb37c31d3be2ae6cae174a691d1e134b0ed86db
thvk-net <control@thvk.net>
2022-10-04 16:17:42 -0700

    Update

--- utils/build_custom_libmad.sh
+++ utils/build_custom_libmad.sh
@@ -2,7 +2,12 @@
 set -e
 
 # Build a customized backwards-compatible version of libmad for macOS.
-
-brew install --formula -s "${SRCROOT}/utils/mad-compat.rb"
-mkdir -p "${PROJECT_DIR}/build"
-cp -f `brew --prefix`/opt/mad-compat/lib/libmad.0.dylib "${PROJECT_DIR}/build"
+if ! type brew &> /dev/null; then
+	/opt/homebrew/bin/brew install --formula -s "${SRCROOT}/utils/mad-compat.rb"
+	mkdir -p "${PROJECT_DIR}/build"
+	cp -f /opt/homebrew/opt/mad-compat/lib/libmad.0.dylib "${PROJECT_DIR}/build"
+else
+	brew install --formula -s "${SRCROOT}/utils/mad-compat.rb"
+	mkdir -p "${PROJECT_DIR}/build"
+	cp -f `brew --prefix`/opt/mad-compat/lib/libmad.0.dylib "${PROJECT_DIR}/build"
+fi
