47f3258cae09f476f356296022f5eedd99fedf05
thvk-net <control@thvk.net>
2022-10-09 10:06:47 -0700

    Update GameAction.h

--- source/GameAction.h
+++ source/GameAction.h
@@ -79,7 +79,7 @@ private:
 	std::map<const GameEvent *, std::pair<int, int>> events;
 	std::vector<std::pair<const Ship *, std::string>> giftShips;
 	std::map<const Outfit *, int> giftOutfits;
-	
+
 	int64_t payment = 0;
 	int64_t paymentMultiplier = 0;
 	int64_t fine = 0;
