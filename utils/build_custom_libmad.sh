9336b61380ca555aa3f8d803d4ae6026d4e5f857
thvk-net <control@thvk.net>
2022-10-09 20:35:13 -0700

    Revert "fix 1"
    This reverts commit ef3efdb547fa572103a5579011704b8cde14a32c.

--- utils/build_custom_libmad.sh
+++ utils/build_custom_libmad.sh
@@ -3,6 +3,6 @@ set -e
 
 # Build a customized backwards-compatible version of libmad for macOS.
 
-/opt/homebrew/bin/brew install --formula -s "${SRCROOT}/utils/mad-compat.rb"
+brew install --formula -s "${SRCROOT}/utils/mad-compat.rb"
 mkdir -p "${PROJECT_DIR}/build"
-cp -f /opt/homebrew/opt/mad-compat/lib/libmad.0.dylib "${PROJECT_DIR}/build"
+cp -f `brew --prefix`/opt/mad-compat/lib/libmad.0.dylib "${PROJECT_DIR}/build"
