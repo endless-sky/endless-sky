a415c104a53058bca306e393ba30337d378eb7dc
quyykk <quyykk@protonmail.com>
2022-10-14 22:37:41 +0200

    Fix parsing for some compressed PNGs.

--- source/ImageBuffer.cpp
+++ source/ImageBuffer.cpp
@@ -247,19 +247,13 @@ namespace {
 		}
 
 		// Adjust settings to make sure the result will be a RGBA file.
-		int colorType = png_get_color_type(png, info);
 		int bitDepth = png_get_bit_depth(png, info);
 
-		png_set_strip_16(png);
 		png_set_packing(png);
-		if(colorType == PNG_COLOR_TYPE_PALETTE)
-			png_set_palette_to_rgb(png);
-		if(colorType == PNG_COLOR_TYPE_GRAY && bitDepth < 8)
-			png_set_expand_gray_1_2_4_to_8(png);
-		if(colorType == PNG_COLOR_TYPE_GRAY || colorType == PNG_COLOR_TYPE_GRAY_ALPHA)
-			png_set_gray_to_rgb(png);
-		if(colorType == PNG_COLOR_TYPE_RGB)
-			png_set_filler(png, 0xFFFF, PNG_FILLER_AFTER);
+		if(bitDepth == 16)
+			png_set_scale_16(png);
+		png_set_expand(png);
+		png_set_filler(png, 0xFFFF, PNG_FILLER_AFTER);
 		// Let libpng handle any interlaced image decoding.
 		png_set_interlace_handling(png);
 		png_read_update_info(png, info);
