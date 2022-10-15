7516504e4191c057d10f1f4cf0441f60167a65d5
thvk-net <control@thvk.net>
2022-10-15 10:30:41 -0700

    Update MapPanel.cpp

--- source/MapPanel.cpp
+++ source/MapPanel.cpp
@@ -218,12 +218,12 @@ void MapPanel::Draw()
 
 	// Draw the "visible range" circle around your current location.
 	Color dimColor(.1f, 0.f);
-	RingShader::Draw(Zoom() * (playerSystem.Position() + center), System::DEFAULT_NEIGHBOR_DISTANCE * Zoom() * 1.5, 2.0f, 1.0f,
+	RingShader::Draw(Zoom() * (playerSystem.Position() + center), System::DEFAULT_NEIGHBOR_DISTANCE * Zoom(), 2.0f, 1.0f,
 		dimColor, 25.0, 10.0);
 	// Draw the jump range circle around your current location if it is different than the
 	// visible range.
 	Color dimColorJump(.3f, 0.f);
-	if(true)//playerJumpDistance != System::DEFAULT_NEIGHBOR_DISTANCE)
+	if(playerJumpDistance != System::DEFAULT_NEIGHBOR_DISTANCE)
 		RingShader::Draw(Zoom() * (playerSystem.Position() + center),
 			(playerJumpDistance + .5) * Zoom(), (playerJumpDistance - .5) * Zoom(), dimColorJump);
 
