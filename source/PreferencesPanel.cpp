3c2f202b42700ecec24a6f2a6db1715020fb7e98
warp-core <warp-core@users.noreply.github.com>
2022-10-25 19:30:07 +0100

    Update source/PreferencesPanel.cpp
    Co-authored-by: Nick <quyykk@protonmail.com>

--- source/PreferencesPanel.cpp
+++ source/PreferencesPanel.cpp
@@ -449,7 +449,7 @@ void PreferencesPanel::DrawSettings()
 
 	// An empty string indicates that a category has ended.
 	// A '\t' character indicates that the first column on this page has ended,
-	// and the next line should be drawn at the start of hte next column.
+	// and the next line should be drawn at the start of the next column.
 	// A '\n' character indicates that this page is complete, no further lines should be drawn on this page.
 	// In all three cases, the first non-special string will be considered the category heading
 	// and will be drawn differently to normal setting entries.
