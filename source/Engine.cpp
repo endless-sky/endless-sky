2b19aea688bacaa98aad002151c78e7f3ab55f44
Koranir <101683475+Koranir@users.noreply.github.com>
2022-11-03 12:58:33 +1100

    Lowered outline intensity

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -200,7 +200,7 @@ namespace {
 
 	const double HEAT_THRESHHOLD = .9;
 	const double HEAT_EFFECT_MULTIPLIER = 0.15;
-	const float SHIELD_OUTLINE_MULT = 6.f;
+	const float SHIELD_OUTLINE_MULT = 5.f;
 }
 
 
