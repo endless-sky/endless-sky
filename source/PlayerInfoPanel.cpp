0dc0f386ff3baae857860f95b62a01eba465eaf6
Koranir <101683475+Koranir@users.noreply.github.com>
2022-11-09 07:35:16 +1100

    Fixed bug where button was not updating correctly

--- source/PlayerInfoPanel.cpp
+++ source/PlayerInfoPanel.cpp
@@ -219,20 +219,24 @@ void PlayerInfoPanel::Draw()
 	if(panelState.CanEdit() && panelState.Ships().size() > 1)
 	{
 		bool allParked = true;
+		bool allParkedSystem = true;
 		bool hasParked = false;
 		bool hasOtherShips = false;
 		const Ship *flagship = player.Flagship();
+		const System *flagshipSystem = flagship->GetSystem();
 		for(const auto &it : panelState.Ships())
 			if(!it->IsDisabled() && it.get() != flagship)
 			{
 				allParked &= it->IsParked();
 				hasParked |= it->IsParked();
 				hasOtherShips = true;
+				if(it->GetSystem() == flagshipSystem)
+					allParkedSystem &= it->IsParked();
 			}
 		if(hasOtherShips)
 		{
 			interfaceInfo.SetCondition(allParked ? "show unpark all" : "show park all");
-			interfaceInfo.SetCondition(hasParked ? "show unpark system" : "show park system");
+			interfaceInfo.SetCondition(allParkedSystem ? "show unpark system" : "show park system");
 		}
 
 		// If ships are selected, decide whether the park or unpark button
@@ -442,8 +446,9 @@ bool PlayerInfoPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &comman
 		// Toggle the parked status for all ships in system except the flagship.
 		bool allParked = true;
 		const Ship *flagship = player.Flagship();
+		const System *flagshipSystem = flagship->GetSystem();
 		for(const auto &it : panelState.Ships())
-			if(!it->IsDisabled() && it.get() != flagship && it->GetSystem() == flagship->GetSystem())
+			if(!it->IsDisabled() && it.get() != flagship && it->GetSystem() == flagshipSystem)
 				allParked &= it->IsParked();
 
 		for(const auto &it : panelState.Ships())
