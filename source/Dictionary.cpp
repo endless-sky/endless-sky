cc13d141b8e49137969916d3b20fe24bcf22434a
Peter van der Meer <peter.vd.meer@gmail.com>
2022-11-19 22:47:00 +0100

    Fix Style issues

--- source/Dictionary.cpp
+++ source/Dictionary.cpp
@@ -13,8 +13,9 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "Dictionary.h"
 #include "StringInterner.h"
+
+#include "Dictionary.h"
 
 #include <cstring>
 #include <mutex>
