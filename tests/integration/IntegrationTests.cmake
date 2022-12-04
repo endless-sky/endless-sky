c72468f448a55d4f0c26f7e81fbbfadb101407da
quyykk <quyykk@protonmail.com>
2022-12-04 16:28:49 +0100

    Add the ability to use the offscreen backend for integration tests.

--- tests/integration/IntegrationTests.cmake
+++ tests/integration/IntegrationTests.cmake
@@ -13,6 +13,13 @@ set(TEST_CONFIGS "${BINARY_PATH}/integration_configs")
 set(TEST_SCRIPT "file(REMOVE_RECURSE \"${TEST_CONFIGS}\")")
 set(TEST_SCRIPT "${TEST_SCRIPT}\nfile(MAKE_DIRECTORY \"${TEST_CONFIGS}\")\n")
 
+# Choose between using the offscreen backend and Xvfb.
+if(ES_USE_OFFSCREEN)
+	set(OFFSCREEN "set(ENV{SDL_VIDEODRIVER} offscreen)\n")
+else()
+	set(XFVB "xvfb-run --auto-servernum \"--server-args=+extension GLX +render -noreset\"")
+endif()
+
 # Add each test as CTest.
 foreach(test ${INTEGRATION_TESTS_LIST})
 	# Copy the template config
@@ -22,7 +29,7 @@ foreach(test ${INTEGRATION_TESTS_LIST})
 
 	set(ADD_TEST
 "add_test([==[${test}]==]
-	$ENV{ES_INTEGRATION_PREFIX} xvfb-run --auto-servernum \"--server-args=+extension GLX +render -noreset\" \"${ES}\" --config \"${TEST_CONFIG}\" --test \"${test}\")")
+	$ENV{ES_INTEGRATION_PREFIX} ${XFVB} \"${ES}\" --config \"${TEST_CONFIG}\" --test \"${test}\")")
 	set(SET_TEST_PROPS
 "set_tests_properties([==[${test}]==] PROPERTIES
 	WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\"
@@ -30,4 +37,4 @@ foreach(test ${INTEGRATION_TESTS_LIST})
 	set(TEST_SCRIPT ${TEST_SCRIPT}${COPY_CONFIG}\n${RENAME_CONFIG}\n${ADD_TEST}\n${SET_TEST_PROPS}\n)
 endforeach()
 
-file(WRITE "${BINARY_PATH}/IntegrationTests_tests.cmake" "${TEST_SCRIPT}")
+file(WRITE "${BINARY_PATH}/IntegrationTests_tests.cmake" "${OFFSCREEN}${TEST_SCRIPT}")
