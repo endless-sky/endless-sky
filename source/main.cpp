499f55e96572e9c33b9c5246c4d2c772d2d4c926
Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>
2022-12-04 16:05:23 -0800

    Update source/main.cpp
    Co-authored-by: Nick <quyykk@protonmail.com>

--- source/main.cpp
+++ source/main.cpp
@@ -169,7 +169,7 @@ int main(int argc, char *argv[])
 
 		Preferences::Load();
 
-		// Load global Conditions:
+		// Load global conditions:
 		DataFile globalConditions(Files::Config() + "global conditions.txt");
 		for(const DataNode &node : globalConditions)
 			if(node.Token(0) == "conditions")
