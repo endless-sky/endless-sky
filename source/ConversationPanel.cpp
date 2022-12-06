9aebd0348d041c4be989e48cb12d3bef5502f98b
Hurleveur <belliorum@gmail.com>
2022-12-06 17:24:14 +0100

    why is it so hard to query a pointer, compiler??

--- source/ConversationPanel.cpp
+++ source/ConversationPanel.cpp
@@ -426,7 +426,7 @@ void ConversationPanel::Exit()
 		bool overrideCapture = false;
 		if(!ship->IsCapturable())
 			for(const Mission &mission : player.Missions())
-				if(mission.OverridesCapture() && !mission.IsFailed() && mission.SourceShip() == ship)
+				if(mission.OverridesCapture() && !mission.IsFailed() && mission.SourceShip() == ship.get())
 				{
 					overrideCapture = true;
 					break;
