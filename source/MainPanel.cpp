915dda355c4c3d3b262054fb3a9853ae8a9e4ac5
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2022-12-06 11:06:37 +0100

    Update MainPanel.cpp

--- source/MainPanel.cpp
+++ source/MainPanel.cpp
@@ -527,8 +527,9 @@ void MainPanel::StepEvents(bool &isActive)
 		{
 			auto boardedShip = event.Target();
 			Mission *mission = player.BoardingMission(boardedShip);
+			bool overrideCapture = mission && mission->OverridesCapture();
 			if(mission && mission->HasSpace(*flagship))
-				mission->Do(Mission::OFFER, player, GetUI(), boardedShip);
+				overrideCapture |= mission->Do(Mission::OFFER, player, GetUI(), boardedShip);
 			else if(mission)
 				player.HandleBlockedMissions((event.Type() & ShipEvent::BOARD)
 						? Mission::BOARDING : Mission::ASSISTING, GetUI());
