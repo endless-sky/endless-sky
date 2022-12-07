52e8c938a0aa2183aa0def7ce5a6fcb31fb102fe
quyykk <quyykk@protonmail.com>
2022-12-07 22:45:16 +0100

    Allow running of the debug integration tests on every platform.

--- tests/integration/IntegrationTests.cmake
+++ tests/integration/IntegrationTests.cmake
@@ -27,14 +27,16 @@ foreach(test ${INTEGRATION_TESTS_LIST})
 	set(COPY_CONFIG "file(COPY \"${ES_CONFIG}\" DESTINATION \"${TEST_CONFIGS}\")")
 	set(RENAME_CONFIG "file(RENAME \"${TEST_CONFIGS}/config\" \"${TEST_CONFIG}\")")
 
-	# Launches the integration tests in release mode: In the background and as fast as possible.
-	set(ADD_TEST
-"add_test([==[${test}]==]
-	$ENV{ES_INTEGRATION_PREFIX} ${XFVB} \"${ES}\" --config \"${TEST_CONFIG}\" --test \"${test}\")")
-	set(SET_TEST_PROPS
-"set_tests_properties([==[${test}]==] PROPERTIES
-	WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\"
-	LABELS integration ${OFFSCREEN})")
+	if(UNIX AND NOT APPLE)
+		# Launches the integration tests in release mode: In the background and as fast as possible.
+		set(ADD_TEST
+	"add_test([==[${test}]==]
+		$ENV{ES_INTEGRATION_PREFIX} ${XFVB} \"${ES}\" --config \"${TEST_CONFIG}\" --test \"${test}\")")
+		set(SET_TEST_PROPS
+	"set_tests_properties([==[${test}]==] PROPERTIES
+		WORKING_DIRECTORY \"${CMAKE_CURRENT_SOURCE_DIR}\"
+		LABELS integration ${OFFSCREEN})")
+	endif()
 
 	# Launches the integration tests in debug mode, so that they can be followed.
 	set(ADD_TEST_DEBUG
