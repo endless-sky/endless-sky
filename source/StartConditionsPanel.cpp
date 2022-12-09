89dbcbb462fdbde62d93b6f8c82073f8a484ab8d
thvk-net <control@thvk.net>
2022-12-08 16:32:48 -0800

    this should mostly work, right?

--- source/StartConditionsPanel.cpp
+++ source/StartConditionsPanel.cpp
@@ -99,7 +99,7 @@ void StartConditionsPanel::Draw()
 	for(auto it = scenarios.begin(); it != scenarios.end();
 			++it, pos += Point(0., entryBox.Height()))
 	{
-		if(!it->Visible(player.Conditions()))
+		if(!it->Visible(GameData::GlobalConditions()))
 			continue;
 
 		// Any scenario wholly outside the bounds can be skipped.
@@ -115,7 +115,7 @@ void StartConditionsPanel::Draw()
 		if(it == startIt)
 			FillShader::Fill(zone.Center(), zone.Dimensions(), selectedBackground.Additive(opacity));
 
-		const auto name = DisplayText(it->Useable(player.Conditions()) ? it->GetDisplayName() : "???", Truncate::BACK);
+		const auto name = DisplayText(it->Useable(GameData::GlobalConditions()) ? it->GetDisplayName() : "???", Truncate::BACK);
 		font.Draw(name, pos + entryTextPadding, (isHighlighted ? bright : medium).Transparent(opacity));
 	}
 
@@ -312,7 +312,7 @@ void StartConditionsPanel::Select(StartConditionsList::const_iterator it)
 
 	// Update the displayed description text.
 	descriptionScroll = 0;
-	description.Wrap(startIt->Useable(player.Conditions()) ? startIt->GetDescription() : startIt->GetHint());
+	description.Wrap(startIt->Useable(GameData::GlobalConditions()) ? startIt->GetDescription() : startIt->GetHint());
 
 	// Scroll the selected scenario into view.
 	ScrollToSelected();
