0bd24bd6473d9a5af3d1597a8007619ab218bf78
quyykk <quyykk@protonmail.com>
2022-12-11 00:40:12 +0100

    Use old style copyright lines

--- utils/vcpkg_bootstrap.cmake
+++ utils/vcpkg_bootstrap.cmake
@@ -1,6 +1,16 @@
-# SPDX-FileCopyrightText: 2022 Osyotr
-# SPDX-License-Identifier: GPL-3.0
+# Copyright (c) 2022 by Osyotr
 # https://gist.github.com/Osyotr/e2d415312a10183bc7c614013494bf21
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
 
 include_guard(GLOBAL)
 
