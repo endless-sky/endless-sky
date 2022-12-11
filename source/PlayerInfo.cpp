7acc0edfa8cb2369b17d90ee18703ce177f70781
Andrew Rove (Rover) <rovermicrover@gmail.com>
2022-12-11 14:51:54 -0600

    Update PlayerInfo.cpp
    @Amazinite feedback around block message.

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -1830,7 +1830,7 @@ void PlayerInfo::HandleBlockedMissions(Mission::Location location, UI *ui)
 		return;
 
 	for(auto it = missionList.begin(); it != missionList.end(); ++it)
-		if(it->IsAtLocation(location) && it->CanOffer(*this) && !it->HasSpace(*this))
+		if(it->IsAtLocation(location) && it->CanOffer(*this) && !it->CanAccept(*this))
 		{
 			string message = it->BlockedMessage(*this);
 			if(!message.empty())
