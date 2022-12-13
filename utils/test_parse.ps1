964cd98c959bc21b407113a33c7d2026f83820b9
quyykk <quyykk@protonmail.com>
2022-12-13 18:00:00 +0100

    Test parse script improvements.

--- utils/test_parse.ps1
+++ utils/test_parse.ps1
@@ -1,28 +1,23 @@
 # PowerShell test script (for testing on Windows CI services)
 param(
   [Parameter(Mandatory=$true, HelpMessage="Path to the Endless Sky executable file")]
-  $EndlessSky #The input binary to execute
+  $EndlessSky, #The input binary to execute
+  [Parameter(HelpMessage="Path to the Endless Sky configuration")]
+  $Config
 )
 # Ensure the argument is a path to the binary (lest Start-Process complain).
 $EndlessSky = Resolve-Path -Path "$EndlessSky" -Relative;
 
-$av = !!$env:APPVEYOR;
-$testName = "Parse Game Data";
-if ($av)
-{
-  Add-AppveyorTest -Name $testName -Framework 'ES' -FileName $EndlessSky -Outcome Running;
-}
 # Remove any existing error files first.
-$FILEDIR = if ($IsWindows -or $Env:OS.ToLower().Contains('windows')) `
+$FILEDIR = if ($IsWindows) `
           { "$env:APPDATA\endless-sky" } `
           else { "$env:HOME/.local/share/endless-sky" };
+if ($Config) { $FILEDIR = $CONFIG; }
 $ERR_FILE = Join-Path -Path $FILEDIR -ChildPath "errors.txt";
 if (Test-Path -Path $ERR_FILE) { Remove-Item -Path $ERR_FILE; }
 
 # Parse the game data files
-$start = $(Get-Date);
-$p = Start-Process -FilePath "$EndlessSky" -ArgumentList '-p' -Wait -PassThru;
-$dur = New-TimeSpan -Start $start -End $(Get-Date);
+$p = Start-Process -FilePath "$EndlessSky" -ArgumentList '-p','--config',$FILEDIR -Wait -PassThru;
 
 # Assert there is no content in the "errors.txt" file.
 if ((Test-Path -Path "$ERR_FILE") -and ((Get-Content -Path "$ERR_FILE" -Raw).Length -gt 0))
@@ -30,27 +25,8 @@ if ((Test-Path -Path "$ERR_FILE") -and ((Get-Content -Path "$ERR_FILE" -Raw).Len
   $err_msg = "Assertion failed: content written to file $ERR_FILE";
   $content = Get-Content -Path "$ERR_FILE" -Raw;
   Write-Host $content;
-  if ($av)
-  {
-    $messages = @();
-    $content.Split("`n") | ForEach-Object `
-    {
-      if ($_.StartsWith(' ') -or $_.StartsWith('file'))
-        { $messages[-1] += "`n$_"; }
-      elseif ($_) { $messages += $_; }
-    }
-    Update-AppveyorTest -Name $testName -Framework 'ES' -FileName $EndlessSky `
-      -Outcome Failed -Duration $dur.TotalMilliseconds -ErrorMessage $err_msg `
-      -StdErr ($messages -Join "`n`n");
-  }
-
   Write-Error -Message $err_msg -Category ParserError;
   exit 1;
-}
-elseif ($av)
-{
-  Update-AppveyorTest -Name $testName -Framework 'ES' `
-    -FileName $EndlessSky -Outcome Passed -Duration $dur.TotalMilliseconds;
 }
 else { Write-Host "No data-parsing errors were encountered"; }
 exit $p.ExitCode;
