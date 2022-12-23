b632e3cc1f509f8562440cb50f023369b2bd3c97
mike-f1 <13293916+mike-f1@users.noreply.github.com>
2022-12-23 19:05:27 +0100

    fix(internal): Fixes some issues noticed by Cppchecks (#7917)

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -312,7 +312,7 @@ void Engine::Place()
 		{
 			const Personality &person = ship->GetPersonality();
 			bool hasOwnPlanet = ship->GetPlanet();
-			bool launchesWithPlayer = (ship->IsYours() || planet->CanLand(*ship))
+			bool launchesWithPlayer = (ship->IsYours() || (planet && planet->CanLand(*ship)))
 					&& !(person.IsStaying() || person.IsWaiting() || hasOwnPlanet);
 			const StellarObject *object = hasOwnPlanet ?
 					ship->GetSystem()->FindStellar(ship->GetPlanet()) : nullptr;
