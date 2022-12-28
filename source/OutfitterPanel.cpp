6de358ee7652be1dd44df369430e254e2d453c67
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2022-12-28 13:55:35 +0100

    Update OutfitterPanel.cpp

--- source/OutfitterPanel.cpp
+++ source/OutfitterPanel.cpp
@@ -109,7 +109,7 @@ void OutfitterPanel::Step()
 	ShopPanel::Step();
 	ShopPanel::CheckForMissions(Mission::OUTFITTER);
 	if(GetUI()->IsTop(this) && !checkedHelp)
-		if(DoHelp("outfitter")) || DoHelp("outfitter 2") || DoHelp("outfitter 3") || true)
+		if(DoHelp("outfitter") || DoHelp("outfitter 2") || DoHelp("outfitter 3") || true)
 			// A help messages has now been displayed, or all of them.
 			checkedHelp = true;
 }
