f8e2ced58887f580b784e82837c1dc0e2eaf61fc
Zitchas <32169904+Zitchas@users.noreply.github.com>
2023-01-06 16:27:59 -0700

    attempt at breaking the line?

--- source/Preferences.cpp
+++ source/Preferences.cpp
@@ -37,7 +37,8 @@ namespace {
 	const string EXPEND_AMMO = "Escorts expend ammo";
 	const string FRUGAL_ESCORTS = "Escorts use ammo frugally";
 
-	const vector<double> ZOOMS = {.05, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0};
+	const vector<double> ZOOMS = {.05, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 
+		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0};
 	int zoomIndex = 4;
 	constexpr double VOLUME_SCALE = .25;
 
