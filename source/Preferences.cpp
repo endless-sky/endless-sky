8c11807fe10f978412e836608df0aba0235f0524
Zitchas <32169904+Zitchas@users.noreply.github.com>
2023-01-07 13:53:41 -0700

    Removing the 3 and 4

--- source/Preferences.cpp
+++ source/Preferences.cpp
@@ -38,7 +38,7 @@ namespace {
 	const string FRUGAL_ESCORTS = "Escorts use ammo frugally";
 
 	const vector<double> ZOOMS = {.05, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0,
-		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0};
+		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0};
 	int zoomIndex = 4;
 	constexpr double VOLUME_SCALE = .25;
 
