ecab634fe4db5228dff7b99334f00384e8d80668
Zitchas <32169904+Zitchas@users.noreply.github.com>
2023-01-08 13:51:44 -0700

    Trimming the highest zoom a bit (removing the 5)

--- source/Preferences.cpp
+++ source/Preferences.cpp
@@ -40,7 +40,7 @@ namespace {
 	// This controls the range of zoom scales the player can switch to.
 	// larger values are close-up and small values are farther away
 	const vector<double> ZOOMS = {.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0,
-		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0, 5.0};
+		1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0, 4.0};
 	int zoomIndex = 4;
 	constexpr double VOLUME_SCALE = .25;
 
