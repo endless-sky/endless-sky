20f897702039ce52cce2f888c1b10327c7391fc4
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2023-01-10 09:22:44 +0100

    shouldnt be const

--- source/Government.h
+++ source/Government.h
@@ -154,7 +154,7 @@ private:
 	const Phrase *hostileHail = nullptr;
 	const Phrase *hostileDisabledHail = nullptr;
 	std::string language;
-	const std::vector<std::pair<const Fleet *, double>> raidFleets;
+	std::vector<std::pair<const Fleet *, double>> raidFleets;
 	double crewAttack = 1.;
 	double crewDefense = 2.;
 	bool provokedOnScan = false;
