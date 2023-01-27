27520fc584e1f900bf44a1d2d967ba4492c00430
Hurleveur <belliorum@gmail.com>
2023-01-27 09:02:16 +0100

    fix? idk I ested with normal JD and it works

--- source/ShipJumpNavigation.cpp
+++ source/ShipJumpNavigation.cpp
@@ -42,8 +42,6 @@ ShipJumpNavigation::ShipJumpNavigation(const Ship &ship)
 // Calibrate this ship's jump navigation information, caching its jump costs, range, and capabilities.
 void ShipJumpNavigation::Calibrate()
 {
-	if(!ship)
-		return;
 	mass = ship->Mass();
 
 	const Outfit &attributes = ship->Attributes();
@@ -128,8 +126,6 @@ double ShipJumpNavigation::JumpDriveFuel(double distance) const
 // If no jump method is possible, returns JumpType::None with a jump cost of 0.
 pair<JumpType, double> ShipJumpNavigation::GetCheapestJumpType(const System *destination) const
 {
-	if(!ship->GetSystem())
-		return make_pair(JumpType::NONE, 0.);
 	return GetCheapestJumpType(ship->GetSystem(), destination);
 }
 
