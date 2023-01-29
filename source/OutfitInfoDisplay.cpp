f7216ac8fb3a3d2818a161cae8b6024a334206bf
warp-core <warp-core@users.noreply.github.com>
2023-01-29 18:29:03 +0000

    Update source/OutfitInfoDisplay.cpp

--- source/OutfitInfoDisplay.cpp
+++ source/OutfitInfoDisplay.cpp
@@ -298,8 +298,7 @@ void OutfitInfoDisplay::UpdateRequirements(const Outfit &outfit, const PlayerInf
 	requirementsHeight += 10;
 
 	bool hasContent = false;
-	static const vector<string> BEFORE =
-		{ "outfit space", "weapon capacity", "engine capacity"};
+	static const vector<string> BEFORE = {"outfit space", "weapon capacity", "engine capacity"};
 	for(const auto &attr : BEFORE)
 	{
 		if(outfit.Get(attr) < 0)
