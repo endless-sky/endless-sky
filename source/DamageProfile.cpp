c9ac1c528a767c2ccb786d7fa4d5ca35dd92ce76
Quantumshark <thedup.adg@gmail.com>
2023-02-09 09:44:21 +0000

    split long bits across multiple lines

--- source/DamageProfile.cpp
+++ source/DamageProfile.cpp
@@ -126,10 +126,11 @@ void DamageProfile::PopulateDamage(DamageDealt &damage, const Ship &ship) const
 		double piercing = max(0., min(1., weapon.Piercing() / (1. + attributes.Get("piercing protection"))
 			- attributes.Get("piercing resistance")));
 		// Determine what portion of its maximum shields the ship is currently at.
-		double shieldPortion = shields/attributes.Get("shields")
-		double permeability = (attributes.Get("full shield permeability") * shieldPortion) + (attributes.Get("low shield permeability") * (1 - shieldPortion));
-		shieldFraction = (1. - min((piercing + permeability), 1.))
-			/ (1. + ship.DisruptionLevel() * .01);
+		double shieldPortion = shields/attributes.Get("shields");
+		double permeability = (attributes.Get("full shield permeability") * shieldPortion) +
+			(attributes.Get("low shield permeability") * (1 - shieldPortion));
+		shieldFraction = (1. - min((piercing + permeability), 1.)) /
+			(1. + ship.DisruptionLevel() * .01);
 
 		damage.shieldDamage = (weapon.ShieldDamage()
 			+ weapon.RelativeShieldDamage() * attributes.Get("shields"))
