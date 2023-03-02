b2f70aa9cae1d827e8028a9891091c22d1d9ae1d
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-03-02 19:34:11 +0100

    change the messages

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -2746,13 +2746,13 @@ int Ship::Scan(const PlayerInfo &player)
 		if(result & ShipEvent::SCAN_CARGO)
 			Messages::Add("The " + government->GetName() + " " + Noun() + " \""
 					+ Name() + (target->attributes.Get("inscrutable")
-					? "\" completed its scan of your cargo with no results."
+					? "\" completed its scan of your cargo with no useful results."
 					: "\" completed its scan of your cargo."),
 					Messages::Importance::High);
 		if(result & ShipEvent::SCAN_OUTFITS)
 			Messages::Add("The " + government->GetName() + " " + Noun() + " \""
 					+ Name() + (target->attributes.Get("inscrutable")
-					? "\" completed its scan of your outfits with no results."
+					? "\" completed its scan of your outfits with no useful results."
 					: "\" completed its scan of your outfits."),
 					Messages::Importance::High);
 	}
