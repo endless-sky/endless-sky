23fe408eac1fd14531909cb095fdff1ebea497a0
Dagg Stompler <daggs@gmx.com>
2023-03-08 20:41:02 +0200

    scanning: mark minable asteroids as known
    as minable asteroids are considered as outfits, when targeting a minable asteroids,
    make it as known outfit
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -2058,6 +2058,9 @@ void Engine::HandleMouseClicks()
 				flagship->SetTargetAsteroid(minable);
 				if(isRightClick)
 					ai.IssueAsteroidTarget(player, minable);
+
+				for(const auto &it : minable->Payload())
+					player.DiscoverOutfit(*it.first);
 			}
 		}
 	}
@@ -2304,8 +2307,12 @@ void Engine::DoCollection(Flotsam &flotsam)
 			player.Harvest(outfit);
 		}
 		else
-			message = name + to_string(amount) + " "
-				+ (amount == 1 ? outfit->DisplayName() : outfit->PluralName()) + ".";
+		{
+			bool outfitIsKnown = player.OutfitIsKnown(*outfit);
+			string outfitDisplayName = outfit->ShownName(outfitIsKnown, amount > 1);
+
+			message = name + to_string(amount) + " " + outfitDisplayName + ".";
+		}
 	}
 	else
 		commodity = flotsam.CommodityType();
