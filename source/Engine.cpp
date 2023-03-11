8a7072e3e7751474d489af0f250a5f7507ebdbd8
Dagg Stompler <daggs@gmx.com>
2023-03-11 13:18:43 +0200

    Engine: label unknown ship model as such on targeting
    when targeting a ship, it's type should be marked as unknown model
    if it isn't known to the player
    
    Signed-off-by: Dagg Stompler <daggs@gmx.com>

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -796,7 +796,8 @@ void Engine::Step(bool isActive)
 			targetUnit = target->Facing().Unit();
 		info.SetSprite("target sprite", target->GetSprite(), targetUnit, target->GetFrame(step));
 		info.SetString("target name", target->Name());
-		info.SetString("target type", target->ModelName());
+		std::string targetModel = (player.ShipModelIsKnown(*target.get())) ? target->ModelName() : "Unknown Ship Model";
+		info.SetString("target type", targetModel);
 		if(!target->GetGovernment())
 			info.SetString("target government", "No Government");
 		else
