698806414851b1903124948f16d08a10e2b55c22
Terin <4471575+Terin@users.noreply.github.com>
2023-03-19 22:41:10 -0500

    feat(enhancement): Support for min and max reputation limits on governments (#7137)

--- source/Politics.cpp
+++ source/Politics.cpp
@@ -136,9 +136,9 @@ void Politics::Offend(const Government *gov, int eventType, int count)
 			// influencing their reputation with the other.
 			double penalty = (count * weight) * other->PenaltyFor(eventType, gov);
 			if(eventType & ShipEvent::ATROCITY && weight > 0)
-				reputationWith[other] = min(0., reputationWith[other]);
+				Politics::SetReputation(other, min(0., reputationWith[other]));
 
-			reputationWith[other] -= penalty;
+			Politics::AddReputation(other, -penalty);
 		}
 	}
 }
@@ -334,13 +334,15 @@ double Politics::Reputation(const Government *gov) const
 
 void Politics::AddReputation(const Government *gov, double value)
 {
-	reputationWith[gov] += value;
+	SetReputation(gov, reputationWith[gov] + value);
 }
 
 
 
 void Politics::SetReputation(const Government *gov, double value)
 {
+	value = min(value, gov->ReputationMax());
+	value = max(value, gov->ReputationMin());
 	reputationWith[gov] = value;
 }
 
