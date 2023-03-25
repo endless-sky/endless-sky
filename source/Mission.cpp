af7b5c1c340765dc3c7b3bcabdd172599a03b2e0
UnorderedSigh <116329264+UnorderedSigh@users.noreply.github.com>
2023-03-25 18:32:40 -0400

    fix problems after merge

--- source/Mission.cpp
+++ source/Mission.cpp
@@ -1397,7 +1397,8 @@ Mission Mission::Instantiate(const PlayerInfo &player, const shared_ptr<Ship> &b
 		return result;
 	}
 	for(const NPC &npc : npcs)
-		result.npcs.push_back(npc.Instantiate(subs, sourceSystem, result.destination->GetSystem(), jumps, payload));
+		result.npcs.push_back(npc.Instantiate(player.Conditions(), subs, sourceSystem, result.destination->GetSystem(),
+			jumps, payload));
 
 	// Instantiate the actions. The "complete" action is always first so that
 	// the "<payment>" substitution can be filled in.
