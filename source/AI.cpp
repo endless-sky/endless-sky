0e0a37e084dc4ac4a869fea16aa1aa16a1d8a730
warp-core <warp-core@users.noreply.github.com>
2023-04-09 19:05:03 +0100

    Update AI.cpp

--- source/AI.cpp
+++ source/AI.cpp
@@ -3986,7 +3986,7 @@ void AI::MovePlayer(Ship &ship, const PlayerInfo &player, Command &activeCommand
 	{
 		if(activeCommands.Has(Command::FORWARD))
 			command |= Command::FORWARD;
-		if(activeCommands.Has(Command::RIGHT | Command::LEFT) && !mouseTurning)
+		if(activeCommands.Has(Command::RIGHT | Command::LEFT))
 			command.SetTurn(activeCommands.Has(Command::RIGHT) - activeCommands.Has(Command::LEFT));
 		if(activeCommands.Has(Command::BACK))
 		{
