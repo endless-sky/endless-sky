7182ee659e8ff94bfe01ce49d0ce6af5673f6b64
Zitchas <32169904+Zitchas@users.noreply.github.com>
2023-04-18 06:38:44 -0600

    Update Ship.h

--- source/Ship.h
+++ source/Ship.h
@@ -275,6 +275,7 @@ public:
 	// The direction that the ship is steering. If positive, the ship is steering right.
 	// If negative, the ship is steering left.
 	double SteeringDirection() const;
+	double LateralDirection() const;
 	// Get the points from which engine flares should be drawn.
 	const std::vector<EnginePoint> &EnginePoints() const;
 	const std::vector<EnginePoint> &ReverseEnginePoints() const;
