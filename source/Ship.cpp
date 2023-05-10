f4ed6f2300debd9d54bf6a65c470521132199bc8
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-05-10 16:54:45 +0200

    feat(enhancement): Only show important scan messages (#8615)
    * No longer show a message when the ship being scanned is the player's flagship or when the scan will result in a fine if successful.

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -2719,6 +2719,10 @@ int Ship::Scan(const PlayerInfo &player)
 	if(isYours || (target->isYours && activeScanning))
 		Audio::Play(Audio::Get("scan"), Position());
 
+	bool isImportant = false;
+	if(target->isYours)
+		isImportant = target.get() == player.Flagship() || government->FinesContents(target.get());
+
 	if(startedScanning && isYours)
 	{
 		if(!target->Name().empty())
@@ -2741,20 +2745,21 @@ int Ship::Scan(const PlayerInfo &player)
 				Messages::Importance::Highest);
 		}
 	}
-	else if(startedScanning && target->isYours)
+	else if(startedScanning && target->isYours && isImportant)
 		Messages::Add("The " + government->GetName() + " " + Noun() + " \""
-			+ Name() + "\" is attempting to scan you.", Messages::Importance::Low);
+				+ Name() + "\" is attempting to scan your ship \"" + target->Name() + "\".",
+				Messages::Importance::Low);
 
-	if(target->isYours && !isYours)
+	if(target->isYours && !isYours && isImportant)
 	{
 		if(result & ShipEvent::SCAN_CARGO)
 			Messages::Add("The " + government->GetName() + " " + Noun() + " \""
-					+ Name() + "\" completed its scan of your cargo.", Messages::Importance::High);
+					+ Name() + "\" completed its cargo scan of your ship \"" + target->Name() + "\".",
+					Messages::Importance::High);
 		if(result & ShipEvent::SCAN_OUTFITS)
 			Messages::Add("The " + government->GetName() + " " + Noun() + " \""
-					+ Name() + (target->attributes.Get("inscrutable") > 0.
-					? "\" completed its scan of your outfits with no useful results."
-					: "\" completed its scan of your outfits."),
+					+ Name() + "\" completed its outfit scan of your ship \"" + target->Name()
+					+ (target->Attributes().Get("inscrutable") > 0. ? "\" with no useful results." : "\"."),
 					Messages::Importance::High);
 	}
 
