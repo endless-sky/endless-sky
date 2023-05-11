46444ab27042eb377c8d14955c7a22b0fc58e779
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-05-11 21:11:12 +0200

    Don't allow license bypassing.

--- source/ShopPanel.h
+++ source/ShopPanel.h
@@ -110,7 +110,7 @@ protected:
 	virtual bool Release(int x, int y) override;
 	virtual bool Scroll(double dx, double dy) override;
 
-	int64_t LicenseCost(const Outfit *outfit) const;
+	int64_t LicenseCost(const Outfit *outfit, bool onlyOwned = false) const;
 
 
 protected:
