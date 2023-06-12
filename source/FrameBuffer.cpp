f03b12e63095a5e1f2a675eaac3eac3f7faf7f4b
thvk-net <control@thvk.net>
2023-06-11 19:45:10 -0700

    yeha, framebuffers

---
+++ source/FrameBuffer.cpp
@@ -0,0 +1,42 @@
+#include "FrameBuffer.h"
+
+
+
+int FrameBuffer::CreateFrameBuffer()
+{
+	GLuint frameBuffer;
+	glGenFramebuffers(1, &frameBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+	glDrawBuffer(GL_COLOR_ATTACHMENT0);
+	return frameBuffer;
+}
+
+
+
+int FrameBuffer::CreateTextureAttachment(int width, int height)
+{
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texture, 0);
+	return texture;
+}
+
+
+
+void FrameBuffer::BindFrameBuffer(int buffer, int width, int height)
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, buffer);
+	glViewport(0, 0, width, height);
+}
+
+
+
+void FrameBuffer::UnbindCurrentFrameBuffer()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
---
+++ source/FrameBuffer.h
@@ -0,0 +1,26 @@
+#include "opengl.h"
+
+
+
+// All functions for handling framebuffers can be found here.
+// The steps to create and use a buffer:
+//  1. int myBuffer = FrameBuffer::CreateFrameBuffer();
+//  2. int mytexture = FrameBuffer::CreateTextureAttachment(width, height);
+//   - creates a texture that can be given to the shader
+//   - I recommend using sizes smaller than the window size for post processing
+//  3. FrameBuffer::BindFrameBuffer(myBuffer, width, height);
+//   - From now on everything will be drawn to the texture, no changes in the drawing process necessary
+//  4. execute as many glDrawArrays or glDrawInstanced as you need
+//  5. UnbindCurrentFrameBuffer();
+//   - you are now back to drawing on the screen and have a texture that contains everything you just drew
+namespace FrameBuffer
+{
+	// Creates a framebuffer for color.
+	static int CreateFrameBuffer();
+	// Create a texture for a color framebuffer.
+	static int CreateTextureAttachment(int width, int height);
+	// Bind a framebuffer (haha unneccessary comment)
+	static void BindFrameBuffer(int buffer, int width, int height);
+	// Unbinds any active framebuffer, making the screen drawspace again
+	static void UnbindCurrentFrameBuffer();
+};
