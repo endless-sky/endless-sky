8a3fda0794bdf0b026f5de07e96380de2b258383
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-06-12 16:28:10 +0200

    Allow the main zoom factor to drop below 100.

--- source/Screen.cpp
+++ source/Screen.cpp
@@ -56,7 +56,7 @@ int Screen::Zoom()
 
 void Screen::SetZoom(int percent)
 {
-	USER_ZOOM = max(100, min(200, percent));
+	USER_ZOOM = max(50, min(200, percent));
 
 	// Make sure the zoom factor is not set too high for the full UI to fit.
 	static const int MIN_WIDTH = 1000; // Width of main menu
@@ -64,8 +64,8 @@ void Screen::SetZoom(int percent)
 	int minZoomX = 100 * RAW_WIDTH / MIN_WIDTH;
 	int minZoomY = 100 * RAW_HEIGHT / MIN_HEIGHT;
 	int minZoom = min(minZoomX, minZoomY);
-	// Never go below 100% zoom, no matter how small the window is
-	minZoom = max(minZoom, 100);
+	// Never go below 50% zoom, no matter how small the window is
+	minZoom = max(minZoom, 50);
 	// Use increments of 10, like the user setting
 	minZoom -= minZoom % 10;
 	EFFECTIVE_ZOOM = min(minZoom, UserZoom());
