0bef077ef4c6fd125b8fff18cac228af605ef55c
warp-core <warp-core@users.noreply.github.com>
2023-07-04 20:52:34 +0100

    Update source/DataWriter.cpp
    Co-authored-by: tibetiroka <68112292+tibetiroka@users.noreply.github.com>

--- source/DataWriter.cpp
+++ source/DataWriter.cpp
@@ -132,7 +132,7 @@ void DataWriter::WriteToken(const string &a)
 	// Figure out what kind of quotation marks need to be used for this string.
 	bool hasSpace = any_of(a.begin(), a.end(), [](char c) { return isspace(c); });
 	bool hasQuote = any_of(a.begin(), a.end(), [](char c) { return (c == '"'); });
-	// If  the token is an empty string, it needs to be wrapped in quotes as thought it had a space.
+	// If the token is an empty string, it needs to be wrapped in quotes as if it had a space.
 	hasSpace |= a.empty();
 	// Write the token, enclosed in quotes if necessary.
 	out << *before;
