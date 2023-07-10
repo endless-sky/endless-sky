ff281f58c01fc68b91d8185a62c31db912f6b37c
tibetiroka <tibetiroka@gmail.com>
2023-07-10 18:54:30 +0200

    remove comments

--- tests/unit/src/comparators/test_byGivenOrder.cpp
+++ tests/unit/src/comparators/test_byGivenOrder.cpp
@@ -66,8 +66,8 @@ SCENARIO( "Test basic ByGivenOrder functionality." , "[ByGivenOrder]" ) {
 			std::vector<int> toSort = { 2, 4, 6, 8, 5, 1, 3 };
 			const std::vector<int> expectedOrder = { 4, 2, 8, 6, 1, 3, 5 };
 
-			std::random_device rd;  // a seed source for the random number engine
-			std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+			std::random_device rd;
+			std::mt19937 gen(rd());
 			std::shuffle(toSort.begin(), toSort.end(), gen);
 			std::sort(toSort.begin(), toSort.end(), c);
 			CHECK( toSort == expectedOrder );
