0a492d0b68059ddd017ccf2bc9a9ce345611e9d1
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2023-08-05 09:51:24 +0200

    destroyed person -> person destroyed
    Co-Authored-By: Amazinite <17688683+Amazinite@users.noreply.github.com>

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -3672,10 +3672,10 @@ void PlayerInfo::RegisterDerivedConditions()
 	pluginProvider.SetHasFunction(pluginFun);
 	pluginProvider.SetGetFunction(pluginFun);
 
-	auto &&destroyedPersonProvider = conditions.GetProviderPrefixed("destroyed person: ");
+	auto &&destroyedPersonProvider = conditions.GetProviderPrefixed("person destroyed: ");
 	auto destroyedPersonFun = [](const string &name) -> bool
 	{
-		const Person *person = GameData::Persons().Get(name.substr(strlen("destroyed person: ")));
+		const Person *person = GameData::Persons().Get(name.substr(strlen("person destroyed: ")));
 		return person ? person->IsDestroyed() : false;
 	};
 	destroyedPersonProvider.SetGetFunction(destroyedPersonFun);
