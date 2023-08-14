4a62af091b8b4c773852c9bdc2f216304467e21b
Dave Flowers <flowers@hagsc.org>
2023-08-13 22:02:18 -0700

    added outfit optimizer class

---
+++ source/OutfitOptimizer.cpp
@@ -0,0 +1,201 @@
+/* OutfitOptimizer.cpp
+Copyright (c) 2023 by Dave Flowers
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "OutfitOptimizer.h"
+
+#include "Outfit.h"
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+OutfitOptimizer::OutfitOptimizer(const string &attribute, const double amount, const double space)
+	: attribute(attribute), totalAmount(amount), totalSpace(space)
+{
+}
+
+
+
+void OutfitOptimizer::AddOutfits(const set<const Outfit *> &outfitList)
+{
+	for(const auto &outfit : outfitList)
+		if(outfit->Get(attribute) > 0.)
+			outfits[outfit] = numeric_limits<int>::max();
+}
+
+
+
+void OutfitOptimizer::AddOutfits(const map<const Outfit *, int> &outfitList)
+{
+	for(const auto &it : outfitList)
+		if(it.second > 0 && it.first->Get(attribute) > 0. && outfits[it.first] < numeric_limits<int>::max())
+			outfits[it.first] += it.second;
+}
+
+
+
+map<const Outfit *, int> OutfitOptimizer::Optimize()
+{
+	if(InitializeOutfitList())
+		return results;
+
+	if(FindBest(0, totalAmount, totalSpace))
+		for(size_t i = 0; i < counts.size(); ++i)
+			results[outfitStats[i].outfit] = counts[i];
+	else
+		results.clear();
+
+	return results;
+}
+
+
+
+// Create the list of outfits to check (some may be winnowed out).
+// Will add in "free" outfits (ones with no space requirement).
+bool OutfitOptimizer::InitializeOutfitList()
+{
+	outfitStats.reserve(outfits.size());
+
+	for(const auto &it : outfits)
+	{
+		const Outfit *outfit = it.first;
+		const double amount = outfit->Get(attribute);
+		const double space = outfit->Get("outfit space");
+		const int count = it.second;
+
+		if(space > totalSpace)
+			continue;
+		else if(space > 0.)
+		{
+			// Remove (or don't add) outfits that are entirely superseded by others.
+			vector<OutfitStats>::iterator oit = outfitStats.begin();
+			while(oit != outfitStats.end())
+			{
+				if(space <= oit->space && (amount > oit->amount || (amount >= oit->amount && count > oit->count)))
+					oit = outfitStats.erase(oit);
+				else if(space >= oit->space && (amount < oit->amount || (amount <= oit->amount && count <= oit->count)))
+					break;
+				else
+					++oit;
+			}
+			if(oit == outfitStats.end())
+				outfitStats.emplace_back(outfit, amount, space, count);
+		}
+		else if(amount * count < totalAmount)
+		{
+			results[outfit] = count;
+			totalAmount -= amount * count;
+			totalSpace -= space * count;
+		}
+		else
+		{
+			results[outfit] = ceil(totalAmount / amount);
+			return true;
+		}
+	}
+
+	sort(outfitStats.begin(), outfitStats.end());
+
+	counts.assign(outfitStats.size(), 0);
+
+	return false;
+}
+
+
+
+bool OutfitOptimizer::FindBest(const size_t offset, const double targetAmount, const double spaceLimit)
+{
+	const OutfitStats &outfit(outfitStats[offset]);
+
+	// Check if remaining outfits are potentially efficient enough to work.
+	if(targetAmount > spaceLimit * outfit.efficiency)
+		return false;
+
+	// Start with enough of this outfit to fill the requirement, if possible.
+	int n = ceil(targetAmount / outfit.amount);
+	int bestCount = -1;
+	double bestSpace = 0.;
+
+	if(n * outfit.space > spaceLimit || n > outfit.count)
+	{
+		if(offset + 1 == outfitStats.size())
+			return false;
+		else
+			n = min(outfit.count, static_cast<int>(floor(spaceLimit / outfit.space))) + 1;
+	}
+	else if(offset + 1 == outfitStats.size())
+	{
+		counts[offset] = n;
+		return true;
+	}
+	else
+	{
+		bestCount = n;
+		bestSpace = n * outfit.space;
+	}
+
+	// Check for possibly better results with fewer of this outfit.
+	vector<int> bestCounts(counts.size() - offset - 1, 0);
+	for(--n; n > -1; --n)
+	{
+		double space(n * outfit.space);
+		if(FindBest(offset + 1, targetAmount - n * outfit.amount, spaceLimit - space))
+		{
+			// Find total space used by outfits from here on.
+			for(size_t i = offset + 1; i != counts.size(); ++i)
+				space += counts[i] * outfitStats[i].space;
+			if(bestCount == -1 || bestSpace > space)
+			{
+				bestCount = n;
+				bestSpace = space;
+				copy(counts.begin() + offset + 1, counts.end(), bestCounts.begin());
+			}
+		}
+	}
+
+	if(bestCount == -1)
+		return false;
+
+	counts[offset] = bestCount;
+	copy(bestCounts.begin(), bestCounts.end(), counts.begin() + offset + 1);
+	return true;
+}
+
+
+
+OutfitOptimizer::OutfitStats::OutfitStats(const Outfit *outfit, const double amount, const double space, const int count)
+	: outfit(outfit), amount(amount), space(space), efficiency(amount / space), count(count)
+{
+}
+
+
+
+bool OutfitOptimizer::OutfitStats::operator<(const OutfitStats &other)
+{
+	if(other.efficiency != efficiency)
+		return other.efficiency < efficiency;
+	else if(other.count != count)
+		return other.count < count;
+	else
+		return other.amount < amount;
+}
---
+++ source/OutfitOptimizer.h
@@ -0,0 +1,78 @@
+/* OutfitOptimizer.h
+Copyright (c) 2023 by Dave Flowers
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef OUTFIT_OPTIMIZER_H_
+#define OUTFIT_OPTIMIZER_H_
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+class Outfit;
+
+
+
+// This class supports the shipyard design center by taking the set
+// of all available outfits and a given attribute and returning the
+// smallest (in outfit space) set of outfits that will satisfy it.
+class OutfitOptimizer {
+public:
+	OutfitOptimizer(const std::string &attribute, double amount, double space);
+
+	// Add outfits from the outfitters.
+	void AddOutfits(const std::set<const Outfit *> &outfits);
+	// Add outfits from cargo or stock.
+	void AddOutfits(const std::map<const Outfit *, int> &outfits);
+
+	// Returns an empty set if no solution was found.
+	std::map<const Outfit *, int> Optimize();
+
+
+private:
+	class OutfitStats {
+	public:
+		OutfitStats(const Outfit *outfit, double amount, double space, int count);
+
+		bool operator<(const OutfitStats &other);
+
+		const Outfit *outfit;
+		double amount;
+		double space;
+		double efficiency;
+		int count;
+	};
+
+
+private:
+	bool InitializeOutfitList();
+	bool FindBest(size_t offset, double targetAmount, double spaceLimit);
+
+
+private:
+	std::string attribute;
+	double totalAmount;
+	double totalSpace;
+
+	std::map<const Outfit *, int> outfits;
+
+	std::map<const Outfit *, int> results;
+	std::vector<OutfitStats> outfitStats;
+	std::vector<int> counts;
+};
+
+
+
+#endif
