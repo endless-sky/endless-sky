e31c907cbedfef7b3f77fd9092fb8b79de075cbc
Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>
2023-08-17 16:07:37 +0200

    bruh

--- source/DownloadHelper.cpp
+++ source/DownloadHelper.cpp
@@ -145,7 +145,7 @@ namespace DownloadHelper {
 		hasHeadFolder = secondEntry.find(firstEntry) != std::string::npos;
 		if(!hasHeadFolder)
 #if defined(_WIN32)
-			_mkdir((destination + expectedName).c_str());
+			_wmkdir((destination + expectedName).c_str());
 #else
 			mkdir((destination + expectedName).c_str(), 0777);
 #endif
