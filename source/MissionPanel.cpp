c5d52996b0c51c892440e24c1eea085ee9f23d79
Dave Flowers <flowers@hagsc.org>
2023-08-17 08:57:25 -0700

    fixed style errors

--- source/MissionPanel.cpp
+++ source/MissionPanel.cpp
@@ -17,19 +17,13 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "MissionPanel.h"
 
-#include "text/alignment.hpp"
 #include "Command.h"
 #include "CoreStartData.h"
 #include "Dialog.h"
-#include "text/DisplayText.h"
 #include "FillShader.h"
-#include "text/Font.h"
-#include "text/FontSet.h"
-#include "text/Format.h"
 #include "GameData.h"
 #include "Information.h"
 #include "Interface.h"
-#include "text/layout.hpp"
 #include "LineShader.h"
 #include "Mission.h"
 #include "Planet.h"
@@ -43,9 +37,15 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "SpriteSet.h"
 #include "SpriteShader.h"
 #include "System.h"
-#include "Wormhole.h"
-#include "text/truncate.hpp"
 #include "UI.h"
+#include "Wormhole.h"
+#include "text/DisplayText.h"
+#include "text/Font.h"
+#include "text/FontSet.h"
+#include "text/Format.h"
+#include "text/alignment.hpp"
+#include "text/layout.hpp"
+#include "text/truncate.hpp"
 
 #include <algorithm>
 #include <cmath>
@@ -219,7 +219,7 @@ void MissionPanel::Draw()
 		previous = distance.Route(next);
 
 		bool isJump, isWormhole, isMappable;
-		if (!GetTravelInfo(previous, next, jumpRange, isJump, isWormhole, isMappable, nullptr))
+		if(!GetTravelInfo(previous, next, jumpRange, isJump, isWormhole, isMappable, nullptr))
 			break;
 		if(isWormhole && !isMappable)
 			continue;
