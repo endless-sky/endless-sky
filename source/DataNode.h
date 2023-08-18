90019914d98991ddae85490429778ea14cca23ff
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2023-08-18 23:12:11 +0200

    Update source/DataNode.h
    Co-authored-by: Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>

--- source/DataNode.h
+++ source/DataNode.h
@@ -48,7 +48,7 @@ public:
 	// DataFile loading guarantees index 0 always exists.
 	const std::string &Token(int index) const;
 	// Add a new token to the end of this node.
-	void AddToken(std::string token);
+	void AddToken(std::string token = "");
 	// Convert the token at the given index to a number. This returns 0 and prints an
 	// error if the index is out of range or the token cannot be interpreted as a number.
 	double Value(int index) const;
