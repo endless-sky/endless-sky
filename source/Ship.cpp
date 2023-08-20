ee5adb25b52f4aada52b6b47efc35e3f5bf211da
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-08-20 23:14:21 +0200

    feat(mechanic): Raid deterrence no longer looks at whether your launchers have ammunition (#8815)

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -3225,13 +3225,9 @@ void Ship::ExpendAmmo(const Weapon &weapon)
 		// A realistic fraction applicable to all cases cannot be computed, so assume 50%.
 		heat -= weapon.AmmoUsage() * .5 * ammo->Mass() * MAXIMUM_TEMPERATURE * Heat();
 		AddOutfit(ammo, -weapon.AmmoUsage());
-		// Only the player's ships make use of attraction and deterrence.
-		if(isYours && !OutfitCount(ammo) && ammo->AmmoUsage())
-		{
-			// Recalculate the AI to account for the loss of this weapon.
+		// Recalculate the AI to account for the loss of this weapon.
+		if(!OutfitCount(ammo) && ammo->AmmoUsage())
 			aiCache.Calibrate(*this);
-			deterrence = CalculateDeterrence();
-		}
 	}
 
 	energy -= weapon.FiringEnergy() + relativeEnergyChange;
@@ -4578,8 +4574,6 @@ double Ship::CalculateDeterrence() const
 		if(hardpoint.GetOutfit())
 		{
 			const Outfit *weapon = hardpoint.GetOutfit();
-			if(weapon->Ammo() && weapon->AmmoUsage() && !OutfitCount(weapon->Ammo()))
-				continue;
 			double strength = weapon->ShieldDamage() + weapon->HullDamage()
 				+ (weapon->RelativeShieldDamage() * attributes.Get("shields"))
 				+ (weapon->RelativeHullDamage() * attributes.Get("hull"));
