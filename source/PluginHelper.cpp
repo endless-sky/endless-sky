81ccd865b2b78f15419a404eeeb7aad318ab8a44
Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>
2023-08-26 11:41:21 +0200

    Update PluginHelper.cpp

--- source/PluginHelper.cpp
+++ source/PluginHelper.cpp
@@ -15,6 +15,10 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "PluginHelper.h"
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <archive.h>
 #include <archive_entry.h>
 #include <cstring>
