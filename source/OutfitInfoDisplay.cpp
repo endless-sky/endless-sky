7864379e87a9f97dbec5ff1d43fc5234e3086372
Quantumshark <thedup.adg@gmail.com>
2023-09-12 10:50:38 +0100

    use brackets

--- source/OutfitInfoDisplay.cpp
+++ source/OutfitInfoDisplay.cpp
@@ -567,9 +567,11 @@ void OutfitInfoDisplay::UpdateAttributes(const Outfit &outfit)
 				attributeLabels.emplace_back(VALUE_NAMES[i].first + PER_SECOND);
 				attributeValues.emplace_back(Format::Number(60. * values[i] / reload) + VALUE_NAMES[i].second);
 				if(fullDropoff != 1. && VALUE_NAMES[i].first.find("damage") != string::npos)
+				{
 					attributeLabels.emplace_back("At maximum range:");
 					attributeValues.emplace_back(Format::Number(60. * fullDropoff * values[i] / reload) + VALUE_NAMES[i].second);
 					attributesHeight += 20;
+				}
 				attributesHeight += 20;
 			}
 	}
@@ -646,9 +648,11 @@ void OutfitInfoDisplay::UpdateAttributes(const Outfit &outfit)
 				attributeLabels.emplace_back(VALUE_NAMES[i].first + PER_SHOT);
 				attributeValues.emplace_back(Format::Number(values[i]) + VALUE_NAMES[i].second);
 				if(fullDropoff != 1. && VALUE_NAMES[i].first.find("damage") != string::npos)
+				{
 					attributeLabels.emplace_back("At maximum range:");
 					attributeValues.emplace_back(Format::Number(fullDropoff * values[i]) + VALUE_NAMES[i].second);
 					attributesHeight += 20;
+				}
 				attributesHeight += 20;
 			}
 	}
