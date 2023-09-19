b416f461703bdbf60af346cd28703c3d019a4b00
Rian Shelley <9004013+thewierdnut@users.noreply.github.com>
2023-09-18 19:26:46 -0500

    Fix right y axis trigger vs scrolling behavior (again)

--- source/MainPanel.cpp
+++ source/MainPanel.cpp
@@ -364,6 +364,8 @@ bool MainPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &command, boo
 		Preferences::ZoomViewIn();
 	else if(key >= '0' && key <= '9' && !command)
 		engine.SelectGroup(key - '0', mod & KMOD_SHIFT, mod & (KMOD_CTRL | KMOD_GUI));
+	else if(!(command == Command()))
+		return true; // any other command will be handled in the engine
 	else
 		return false;
 
