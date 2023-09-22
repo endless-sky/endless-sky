1c8992d330b418e3bc4c87a52b4c10bb44e3b888
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2023-09-22 13:06:32 +0200

    check for sold outfits only

--- source/PlanetPanel.cpp
+++ source/PlanetPanel.cpp
@@ -320,7 +320,7 @@ void PlanetPanel::CheckWarningsAndTakeOff()
 	// Have you left any unique items at the outfitter?
 	map<const Outfit *, int> leftUniques;
 	for(const auto &it : player.GetStock())
-		if(it.first->Attributes().Get("unique"))
+		if(it.second > 0 && it.first->Attributes().Get("unique"))
 			leftUniques[it.first] = it.second;
 	// Count how many active ships we have that cannot make the jump (e.g. due to lack of fuel,
 	// drive, or carrier). All such ships will have been logged in the player's flightcheck.
