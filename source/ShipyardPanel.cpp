dee5099c51c12b1ca9f41b57b983737e715ca6d2
Ember369 <59035469+Ember369@users.noreply.github.com>
2023-10-29 01:39:56 +1100

    Simplify DoHelp() function call
    Since there's only one help message, no need for the extra variable checkedHelp
    
    Co-authored-by: TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>

--- source/ShipyardPanel.cpp
+++ source/ShipyardPanel.cpp
@@ -107,12 +107,8 @@ void ShipyardPanel::Step()
 {
 	ShopPanel::Step();
 	ShopPanel::CheckForMissions(Mission::SHIPYARD);
-	if(GetUI()->IsTop(this) && !checkedHelp)
-		// Use short-circuiting to only display one of them at a time.
-		// (The first valid condition encountered will make us skip the others.)
-		if(DoHelp("shipyard") || true)
-			// Either a help message was freshly displayed, or all of them have already been seen.
-			checkedHelp = true;
+	if(GetUI()->IsTop(this))
+		DoHelp("shipyard");
 }
 
 
