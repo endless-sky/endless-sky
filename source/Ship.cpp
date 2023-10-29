b423397d0947864826da294b4dc18a5452ffaa26
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2023-10-29 22:21:55 +0100

    Update source/Ship.cpp

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -2662,7 +2662,7 @@ int Ship::RequiredCrew() const
 
 int Ship::CrewValue() const
 {
-	int crewEquivalent = attributes.Get("crew equivalent")
+	int crewEquivalent = attributes.Get("crew equivalent");
 	if(attributes.get("crew equivalent overrides"))
 		return crewEquivalent ? crewEquivalent : max(Crew(), RequiredCrew());
 	return max(Crew(), RequiredCrew()) + crewEquivalent;
