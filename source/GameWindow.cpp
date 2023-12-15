d96960c7de912b594143905e0409c0d37238e5cc
quyykk <quyykk@protonmail.com>
2023-12-15 23:30:40 +0100

    Don't enable OpenGL support in SDL.

--- source/GameWindow.cpp
+++ source/GameWindow.cpp
@@ -125,12 +125,16 @@ bool GameWindow::Init(bool headless)
 	}
 
 	// Settings that must be declared before the window creation.
-	Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+	Uint32 flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
 
 	if(Preferences::ScreenModeSetting() == "fullscreen")
 		flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
 	else if(Preferences::Has("maximized"))
 		flags |= SDL_WINDOW_MAXIMIZED;
+
+	// Enable OpenGL for non-headless runs.
+	if(!headless)
+		flags |= SDL_WINDOW_OPENGL;
 
 	// The main window spawns visibly at this point.
 	mainWindow = SDL_CreateWindow("Endless Sky", SDL_WINDOWPOS_UNDEFINED,
