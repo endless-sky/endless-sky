47a1074e905a60ad044843ee67ed47b8c33c1dd4
Azure_ <42621251+Azure3141@users.noreply.github.com>
2024-01-21 11:31:03 -0800

    Fix

--- source/MapPanel.cpp
+++ source/MapPanel.cpp
@@ -1392,15 +1392,13 @@ void MapPanel::DrawSystems()
 		// Ensures every multiple-star system has a unique, deterministic rotation.
 		float starAngle = node.name.length() + node.position.Length();
 		float spin = 4 * acos(0.0) / node.mapIcon.size();
-		
-
-		Point starRotate(cos(starAngle), sin(starAngle));
 		Point starOffset = (node.mapIcon.size() == 1) ? Point(0, 0) : Point(4, 4);
 
 		// Draw the star sprites
 		for(string star : node.mapIcon)
 		{
 			starAngle = starAngle + spin;
+			Point starRotate(cos(starAngle), sin(starAngle));
 			const Body starBody = Body(SpriteSet::Get(star), pos + zoom * starOffset * starRotate, Point(0, 0), starAngle, sqrt(zoom) / 2);
 			batchDraw.Add(starBody);
 		}
