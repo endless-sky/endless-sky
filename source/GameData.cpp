ab91bdd5e7ee6b4a7bfcfd460074d2ed24bc0500
quyykk <quyykk@protonmail.com>
2024-03-22 12:13:05 +0100

    Fix various crashes when loading/unloading images.

--- source/GameData.cpp
+++ source/GameData.cpp
@@ -329,14 +329,14 @@ void GameData::Preload(TaskQueue &queue, const Sprite *sprite)
 	// This sprite is not currently preloaded. Check to see whether we already
 	// have the maximum number of sprites loaded, in which case the oldest one
 	// must be unloaded to make room for this one.
-	const string &name = sprite->Name();
 	pit = preloaded.begin();
 	while(pit != preloaded.end())
 	{
 		++pit->second;
 		if(pit->second >= 20)
 		{
-			SpriteSet::Modify(name)->Unload();
+			// Unloading needs to be queued on the main thread.
+			queue.Run({}, [name = pit->first->Name()] { SpriteSet::Modify(name)->Unload(); });
 			pit = preloaded.erase(pit);
 		}
 		else
