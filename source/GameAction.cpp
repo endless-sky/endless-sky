e67bd94c815a296bc6c9864da953684b648e7639
UnorderedSigh <116329264+UnorderedSigh@users.noreply.github.com>
2024-05-31 00:44:11 +0000

    reverse optimization change because MacOS does not like it

--- source/GameAction.cpp
+++ source/GameAction.cpp
@@ -461,7 +461,7 @@ GameAction GameAction::Instantiate(map<string, string> &subs, int jumps, int pay
 	}
 
 	for(auto &&it : giftShips)
-		result.giftShips.push_back(std::move(it.Instantiate(subs)));
+		result.giftShips.emplace_back(it.Instantiate(subs));
 	result.giftOutfits = giftOutfits;
 
 	result.music = music;
