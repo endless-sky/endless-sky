995610cd7c16743bbad4bbf2f13cbc8ce5541f57
Nicholas Shanks <nickshanks@gmail.com>
2024-06-09 00:09:26 +0100

    Warn if the cooling efficiency is below 50%

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -1325,6 +1325,10 @@ vector<string> Ship::FlightCheck() const
 				checks.emplace_back("insufficient energy to fire?");
 				break;
 			}
+		if(CoolingEfficiency() < .5)
+		{
+			checks.emplace_back("inefficient cooling?");
+		}
 	}
 
 	return checks;
