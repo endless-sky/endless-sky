30ba893f23be44ba450337313132dacfd19323fb
tibetiroka <tibetiroka@gmail.com>
2024-06-14 16:58:46 +0200

    Warp's suggestion

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -1115,11 +1115,10 @@ void Engine::Draw() const
 
 	for(const auto &outline : outlines)
 	{
-		if(outline.sprite)
-		{
-			Point size(outline.sprite->Width(), outline.sprite->Height());
-			OutlineShader::Draw(outline.sprite, outline.position, size, outline.color, outline.unit, outline.frame);
-		}
+		if(!outline.sprite)
+			continue;
+		Point size(outline.sprite->Width(), outline.sprite->Height());
+		OutlineShader::Draw(outline.sprite, outline.position, size, outline.color, outline.unit, outline.frame);
 	}
 
 	if(flash)
