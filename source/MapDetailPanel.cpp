718a13f6d552e7548fd2b6b97373034e2a85d880
Just-Existing <69060286+Just-Existing@users.noreply.github.com>
2024-06-19 21:18:06 -0600

    Update MapDetailPanel.cpp

--- source/MapDetailPanel.cpp
+++ source/MapDetailPanel.cpp
@@ -800,7 +800,7 @@ void MapDetailPanel::DrawInfo()
 			else
 			{
 				value -= localValue;
-				if(Preferences::GetPriceParens())
+				if(Preferences::Has(Preferences::PRICE_PARENS))
 					price += "(";
 				if(value > 0)
 					price += '+';
@@ -808,7 +808,7 @@ void MapDetailPanel::DrawInfo()
 					price += '-';
 				value = abs(value);
 				price += to_string(value);
-				if(Preferences::GetPriceParens())
+				if(Preferences::Has(Preferences::PRICE_PARENS))
 					price += ")";
 			}
 		}
