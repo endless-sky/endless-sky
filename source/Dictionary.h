9be49dce4ddf053d55e41bb38dcec9547599b9ae
tibetiroka <tibetiroka@gmail.com>
2024-06-23 14:48:13 +0200

    refactor

--- source/Dictionary.h
+++ source/Dictionary.h
@@ -16,31 +16,34 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #ifndef DICTIONARY_H_
 #define DICTIONARY_H_
 
+#include "FlatMap.h"
+#include "StringInterner.h"
+
+#include <cstring>
 #include <string>
 #include <utility>
 #include <vector>
 
 
 
+// Helper struct for using strcmp as a comparator object.
+struct StringComparator{
+	int operator()(const char *first, const char *second) const {
+		return strcmp(first, second);
+	}
+};
+
 // This class stores a mapping from character string keys to values, in a way
 // that prioritizes fast lookup time at the expense of longer construction time
 // compared to an STL map. That makes it suitable for ship attributes, which are
 // changed much less frequently than they are queried.
-class Dictionary : private std::vector<std::pair<const char *, double>> {
+class Dictionary : public FlatMap<const char *, double, StringComparator> {
 public:
-	// Access a key for modifying it:
 	double &operator[](const char *key);
 	double &operator[](const std::string &key);
 	// Get the value of a key, or 0 if it does not exist:
 	double Get(const char *key) const;
 	double Get(const std::string &key) const;
-
-	// Expose certain functions from the underlying vector:
-	using std::vector<std::pair<const char *, double>>::empty;
-	using std::vector<std::pair<const char *, double>>::begin;
-	using std::vector<std::pair<const char *, double>>::end;
 };
-
-
 
 #endif
---
+++ source/FlatMap.h
@@ -0,0 +1,109 @@
+/* FlatMap.h
+Copyright (c) 2024 by tibetiroka
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef FLAT_MAP_H_
+#define FLAT_MAP_H_
+
+#include <functional>
+#include <utility>
+#include <vector>
+
+
+
+// A generic flat map providing fast lookup and slower insertion.
+template <class Key, class Value, class ThreeWayComp = std::compare_three_way,
+		class Alloc = std::allocator<std::pair<Key, Value>>>
+class FlatMap : private std::vector<std::pair<Key, Value>, Alloc> {
+private:
+	// Use references or copies for passing keys, depending on what's more efficient.
+	typedef std::conditional_t<std::is_fundamental_v<Key> || std::is_pointer_v<Key> || std::is_enum_v<Key>,
+			Key, Key &> KeyRef;
+
+
+public:
+	// Access a key for modifying it:
+	inline Value &operator[](KeyRef key);
+	// Get the value of a key, or 0 if it does not exist:
+	inline Value Get(KeyRef key) const;
+
+	// Expose certain functions from the underlying vector:
+	using std::vector<std::pair<Key, Value>, Alloc>::empty;
+	using std::vector<std::pair<Key, Value>, Alloc>::size;
+	using std::vector<std::pair<Key, Value>, Alloc>::begin;
+	using std::vector<std::pair<Key, Value>, Alloc>::end;
+	using typename std::vector<std::pair<Key, Value>, Alloc>::iterator;
+	using typename std::vector<std::pair<Key, Value>, Alloc>::const_iterator;
+
+
+protected:
+	// Look up a value in the vector.
+	inline std::pair<size_t, bool> Search(KeyRef key) const;
+
+	using std::vector<std::pair<Key, Value>, Alloc>::at;
+	using std::vector<std::pair<Key, Value>, Alloc>::data;
+	using std::vector<std::pair<Key, Value>, Alloc>::insert;
+};
+
+
+
+// Access a key for modifying it:
+template <class Key, class Value, class ThreeWayComp, class Alloc>
+Value &FlatMap<Key, Value, ThreeWayComp, Alloc>::operator[](KeyRef key)
+{
+	std::pair<size_t, bool> pos = Search(key);
+	if(pos.second)
+		return data()[pos.first].second;
+
+	return insert(begin() + pos.first, std::make_pair(key, 0.))->second;
+}
+
+
+
+// Get the value of a key, or 0 if it does not exist:
+template <class Key, class Value, class ThreeWayComp, class Alloc>
+Value FlatMap<Key, Value, ThreeWayComp, Alloc>::Get(KeyRef key) const
+{
+	std::pair<size_t, bool> pos = Search(key);
+	return (pos.second ? data()[pos.first].second : 0.);
+}
+
+
+
+// Perform a binary search on a sorted vector. Return the key's location (or
+// proper insertion spot) in the first element of the pair, and "true" in
+// the second element if the key is already in the vector.
+template <class Key, class Value, class ThreeWayComp, class Alloc>
+std::pair<size_t, bool> FlatMap<Key, Value, ThreeWayComp, Alloc>::Search(KeyRef key) const
+{
+	// At each step of the search, we know the key is in [low, high).
+	size_t low = 0;
+	size_t high = size();
+
+	while(low != high)
+	{
+		size_t mid = (low + high) / 2;
+		int cmp = ThreeWayComp{}(key, at(mid).first);
+		if(!cmp)
+			return std::make_pair(mid, true);
+
+		if(cmp < 0)
+			high = mid;
+		else
+			low = mid + 1;
+	}
+	return std::make_pair(low, false);
+}
+
+#endif
