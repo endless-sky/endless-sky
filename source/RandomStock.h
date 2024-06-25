a8612c2d239402d8bb7cd2e52291500bed17ba74
Zarkonnen <david.stark@zarkonnen.com>
2024-06-25 10:32:57 +0200

    Made copyright clearer

--- source/RandomStock.h
+++ source/RandomStock.h
@@ -1,5 +1,5 @@
 /* RandomStock.h
-Copyright (c) 2024 by David Stark
+Copyright (c) 2024 by David Stark (Zarkonnen)
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
