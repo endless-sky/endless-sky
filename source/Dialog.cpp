c3f3f81dbe20f522165aba8862fd59748b7f1cb9
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2024-07-16 18:11:34 +0200

    Apply suggestions from code review
    Co-authored-by: Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>

--- source/Dialog.cpp
+++ source/Dialog.cpp
@@ -83,53 +83,36 @@ namespace {
 		{SDLK_KP_VERTICALBAR, '|'}
 	};
 
-	// The width of the margin on the left side of the dialog. This area is part of the sprite,
+	// The width of the margin on the right/left sides of the dialog. This area is part of the sprite,
 	// but shouldn't have any text or other graphics rendered over it. (It's mostly transparent.)
 	const int leftMargin = 20;
-	// The width of the margin on the right side of the dialog. This area is part of the sprite,
-	// but shouldn't have any text or other graphics rendered over it. (It's mostly transparent.)
 	const int rightMargin = 20;
-	// The overall margin width of the dialog, in pixels.
 	const int horizontalMargin = leftMargin + rightMargin;
-	// The margin on the left side of the button sprite. The bottom segment also includes a button
+	// The margin on the right/left sides of the button sprite. The bottom segment also includes a button
 	// that uses the same value.
 	const int buttonLeftMargin = 10;
-	// The margin on the left side of the button sprite. The bottom segment also includes a button
-	// that uses the same value.
 	const int buttonRightMargin = 10;
-	// The overall margin width of the button, in pixels.
 	const int buttonHorizontalMargin = buttonLeftMargin + buttonRightMargin;
-	// The margin on the left side of the button sprite. The bottom segment also includes a button
+	// The margin on the top/bottom sides of the button sprite. The bottom segment also includes a button
 	// that uses the same value.
 	const int buttonTopMargin = 10;
-	// The margin on the left side of the button sprite. The bottom segment also includes a button
-	// that uses the same value.
 	const int buttonBottomMargin = 10;
-	// The overall margin height of the button, in pixels.
 	const int buttonVerticalMargin = buttonTopMargin + buttonBottomMargin;
-	// The width of the padding used on the left side of each segment, in pixels.
+	// The width of the padding used on the left/right sides of each segment, in pixels.
 	const int leftPadding = 10;
-	// The width of the padding used on the right side of each segment, in pixels.
 	const int rightPadding = 10;
-	// The overall padding width of the dialog, in pixels.
 	const int horizontalPadding = rightPadding + leftPadding;
-	// The height of the padding used by the top segment, in pixels.
+	// The height of the padding used by the top/bottom segment, in pixels.
 	const int topPadding = 10;
-	// The height of the padding used by the bottom segment, in pixels.
 	const int bottomPadding = 10;
-	// The overall padding height of the dialog, in pixels.
 	const int verticalPadding = topPadding + bottomPadding;
-	// The width of the padding at the beginning of an input field.
+	// The width of the padding at the beginning/end of an input field.
 	const int inputLeftPadding = 5;
-	// The width of the padding at the end of an input field.
 	const int inputRightPadding = 5;
-	// The overall padding width of an input field, in pixels.
 	const int inputHorizontalPadding = inputLeftPadding + inputRightPadding;
-	// The height of the padding at the top of an input field.
+	// The height of the padding at the top/bottom of an input field.
 	const int inputTopPadding = 2;
-	// The height of the padding at the bottom of an input field.
 	const int inputBottomPadding = 2;
-	// The overall padding height of an input field, in pixels.
 	const int inputVerticalPadding = inputTopPadding + inputBottomPadding;
 	// The height of an input field in pixels.
 	const int inputHeight = 20;
@@ -178,7 +161,7 @@ void Dialog::Draw()
 	const Sprite *cancel = SpriteSet::Get("ui/dialog cancel");
 
 	// Get the position of the top of this dialog, and of the input.
-	Point pos(0., (top->Height() + extensionCount * middle->Height() + bottom->Height()) * -.5f);
+	Point pos(0., (top->Height() + extensionCount * middle->Height() + bottom->Height()) * -.5);
 	Point inputPos = Point(0., -(cancel->Height() + inputHeight)) - pos;
 
 	// Draw the top section of the dialog box.
@@ -205,7 +188,7 @@ void Dialog::Draw()
 	const Color &dim = *GameData::Colors().Get("medium");
 	const Color &back = *GameData::Colors().Get("faint");
 	const Color &inactive = *GameData::Colors().Get("inactive");
-	string okText = isMission ? "Accept" : "OK";
+	const string okText = isMission ? "Accept" : "OK";
 	okPos = pos + Point((top->Width() - rightMargin - cancel->Width()) * .5, 0.);
 	Point labelPos(
 		okPos.X() - .5 * font.Width(okText),
