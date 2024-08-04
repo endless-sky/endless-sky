256a4433833674a918a978bfa43e2cd66e504aa0
thvk-net <control@thvk.net>
2024-08-04 21:34:24 +0200

    Update GameData.h

--- source/GameData.h
+++ source/GameData.h
@@ -107,7 +107,7 @@ public:
 	static void Change(const DataNode &node);
 	// Update the neighbor lists and other information for all the systems.
 	// This must be done any time that a change creates or moves a system.
-	static void UpdateSystems(const PlayerInfo *player);
+	static void UpdateSystems();
 	static void UpdateSystem(const std::string &name, const Ship *flagship);
 
 	static void AddJumpRange(double neighborDistance);
