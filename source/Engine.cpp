8bc0835ccbb29dbc1efea2954b03656f1ad4a888
Saugia <saugianima@gmail.com>
2024-08-04 15:48:42 -0400

    Update Engine.cpp

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -1853,7 +1853,7 @@ void Engine::SpawnPersons()
 	if(!sum)
 		return;
 
-	// Although an attempt to spawn a person is made every 20 minutes on average,
+	// Although an attempt to spawn a person is specified by a gamerule,
 	// that attempt can still fail due to an added weight for no person to spawn.
 	sum = Random::Int(sum + GameData::GetGamerules().NoPersonSpawnWeight());
 	for(const auto &it : GameData::Persons())
