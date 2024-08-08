3ab8d075c4c53ed976e6ce90942906ed4d3864ce
tibetiroka <tibetiroka@gmail.com>
2024-08-08 21:14:49 +0200

    constants

--- source/text/Format.cpp
+++ source/text/Format.cpp
@@ -27,7 +27,9 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 using namespace std;
 
 namespace {
-	const int64_t K = 1000;
+	// The greatest number displayed without switching to scientific notation
+	constexpr int64_t SCIENTIFIC_THRESHOLD = 1000000000000000ll;
+	constexpr int64_t K = 1000;
 	static const vector<pair<const char *, int64_t>> WORD_NUMBERS = {
 		{ "quintillion", K * K * K * K * K * K },
 		{ "quadrillion", K * K * K * K * K },
@@ -277,7 +279,7 @@ string Format::Credits(int64_t value)
 	int64_t absolute = abs(value);
 
 	// If the value is above one quadrillion, show it in scientific notation.
-	if(absolute > 1000000000000000ll)
+	if(absolute > SCIENTIFIC_THRESHOLD)
 	{
 		ostringstream out;
 		out.precision(3);
@@ -384,7 +386,7 @@ string Format::Number(double value)
 		return "???";
 	else if(std::isinf(value))
 		return value > 0. ? "infinity" : "-infinity";
-	else if(fabs(value) > 1e15)
+	else if(fabs(value) > SCIENTIFIC_THRESHOLD)
 	{
 		// Use scientific notation for excessively large numbers.
 		ostringstream out;
