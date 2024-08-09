f383a91f53dbb31358bbaeafd28daee50bf16628
Peter van der Meer <peter.vd.meer@gmail.com>
2024-08-09 17:31:39 +0200

    Switch to pragma-once instead of ifndef headers
    Co-authored-by: tibetiroka <68112292+tibetiroka@users.noreply.github.com>

--- source/ConditionAssignments.h
+++ source/ConditionAssignments.h
@@ -13,8 +13,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-#ifndef CONDITION_ASSIGNMENTS_H_
-#define CONDITION_ASSIGNMENTS_H_
+#pragma once
 
 #include "ConditionSet.h"
 
@@ -65,5 +64,3 @@ private:
 
 	ConditionSet setToEvaluate;
 };
-
-#endif
