65c5969b119fafc0aeea6a97ef6a33ef1990ca3c
tibetiroka <tibetiroka@gmail.com>
2024-08-10 09:22:33 +0200

    Unit tests

--- tests/unit/src/text/test_format.cpp
+++ tests/unit/src/text/test_format.cpp
@@ -267,6 +267,10 @@ TEST_CASE( "Format::Number", "[Format][Number]") {
 		CHECK( Format::Number(107.09) == "107.09" );
 		CHECK( Format::Number(0.0123) == "0.01" );
 	}
+	SECTION( "Large numbers" ) {
+		CHECK( Format::Number(1e15) == "1000000000000000" );
+		CHECK( Format::Number(1e15 + 1) == "1e+15" );
+	}
 }
 
 TEST_CASE( "Format::Credits", "[Format][Credits]") {
