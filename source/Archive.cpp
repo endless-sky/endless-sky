8bc4754ad94a330569582bb31eebe69639d61656
Rising Leaf <85687254+RisingLeaf@users.noreply.github.com>
2024-08-28 15:42:15 +0200

    use smart pointers

--- source/Archive.cpp
+++ source/Archive.cpp
@@ -21,38 +21,38 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include <archive_entry.h>
 
 using namespace std;
+using ReadingHandle = unique_ptr<archive, int(*)(archive *)>;
+using EntryHandle = unique_ptr<archive_entry, void(*)(archive_entry *)>;
 
 namespace {
-	bool InitArchive(const string &path, archive *&reading, archive_entry *&entry, string &firstEntry)
+	bool InitArchive(const string &path, ReadingHandle &reading, EntryHandle &entry, string &firstEntry)
 	{
-		reading = archive_read_new();
-		archive_read_support_filter_zip(reading);
-		archive_read_support_format_zip(reading);
-		int status = archive_read_open_filename(reading, path.c_str(), 10240);
+		archive_read_support_filter_all(reading.get());
+		archive_read_support_format_all(reading.get());
+		int status = archive_read_open_filename(reading.get(), path.c_str(), 10240);
 		if(status != ARCHIVE_OK)
 			return false;
 		size_t start = path.rfind("/");
 		string cleanedArchiveName = path.substr(start, path.size() - start - 4) + "/";
 
 		// Find out if the archive contains a head folder.
-		archive_read_next_header(reading, &entry);
-		firstEntry = archive_entry_pathname(entry);
+		archive_read_next_header2(reading.get(), entry.get());
+		firstEntry = archive_entry_pathname(entry.get());
 		bool hasHeadFolder = firstEntry == cleanedArchiveName;
 		if(hasHeadFolder)
 			firstEntry = firstEntry.substr(0, firstEntry.find("/") + 1);
 		else
 			firstEntry = "";
-		archive_read_data_skip(reading);
+		archive_read_data_skip(reading.get());
 
 		// If the archive has no head folder rewind to the start.
 		if(!hasHeadFolder)
 		{
-			archive_read_free(reading);
-
-			reading = archive_read_new();
-			archive_read_support_filter_all(reading);
-			archive_read_support_format_all(reading);
-			int ret = archive_read_open_filename(reading, path.c_str(), 10240);
+			reading.reset();
+			reading = ReadingHandle(archive_read_new(), archive_read_free);
+			archive_read_support_filter_all(reading.get());
+			archive_read_support_format_all(reading.get());
+			int ret = archive_read_open_filename(reading.get(), path.c_str(), 10240);
 			if(ret != ARCHIVE_OK)
 				return false;
 		}
@@ -65,15 +65,14 @@ namespace {
 
 Archive::ArchiveResourceHandle::~ArchiveResourceHandle()
 {
-	free(data);
-	data = nullptr;
+	Clear();
 }
 
 
 
 void Archive::ArchiveResourceHandle::Allocate(size_t newSize)
 {
-	data = reinterpret_cast<unsigned char *>(malloc(newSize));
+	data = reinterpret_cast<unsigned char *>( new bool[newSize] );
 	size = newSize;
 }
 
@@ -88,8 +87,7 @@ void Archive::ArchiveResourceHandle::CreateFileFromData()
 
 void Archive::ArchiveResourceHandle::Clear()
 {
-	if(data)
-		free(data);
+	delete[] data;
 	size = 0;
 }
 
@@ -127,8 +125,8 @@ pair<string, vector<string>> Archive::GetRecursiveFileList(const string &archive
 {
 	pair<string, vector<string>> result;
 
-	struct archive *reading = nullptr;
-	archive_entry *entry = nullptr;
+	ReadingHandle reading(archive_read_new(), archive_read_free);
+	EntryHandle entry(nullptr, archive_entry_free);
 	string firstEntry;
 
 	if(!InitArchive(archivePath, reading, entry, firstEntry))
@@ -138,14 +136,13 @@ pair<string, vector<string>> Archive::GetRecursiveFileList(const string &archive
 
 	result.first = archivePath + "/" + directoryPath;
 
-	while(archive_read_next_header(reading, &entry) == ARCHIVE_OK)
+	while(archive_read_next_header2(reading.get(), entry.get()) == ARCHIVE_OK)
 	{
-		const string name = archive_entry_pathname(entry);
+		const string name = archive_entry_pathname(entry.get());
 		if(!name.starts_with(directoryPath))
 			continue;
 		result.second.emplace_back(archivePath + "/" + name);
 	}
-	archive_read_free(reading);
 
 	return result;
 }
@@ -154,8 +151,8 @@ pair<string, vector<string>> Archive::GetRecursiveFileList(const string &archive
 
 string Archive::GetRootPath(const string &archivePath)
 {
-	struct archive *reading = nullptr;
-	archive_entry *entry = nullptr;
+	ReadingHandle reading(archive_read_new(), archive_read_free);
+	EntryHandle entry(nullptr, archive_entry_free);
 	string firstEntry = "";
 
 	InitArchive(archivePath, reading, entry, firstEntry);
@@ -175,16 +172,16 @@ bool Archive::FileExists(const string &archiveFilePath)
 	string archivePath = archiveFilePath.substr(0, start + 4);
 	string filePath = archiveFilePath.substr(start + 5, archiveFilePath.size());
 
-	struct archive *reading = nullptr;
-	archive_entry *entry = nullptr;
+	ReadingHandle reading(archive_read_new(), archive_read_free);
+	EntryHandle entry(nullptr, archive_entry_free);
 	string firstEntry = "";
 
 	if(!InitArchive(archivePath, reading, entry, firstEntry))
 		return false;
 
-	while(archive_read_next_header(reading, &entry) == ARCHIVE_OK)
+	while(archive_read_next_header2(reading.get(), entry.get()) == ARCHIVE_OK)
 	{
-		string name = archive_entry_pathname(entry);
+		string name = archive_entry_pathname(entry.get());
 		if(name == filePath)
 			return true;
 	}
@@ -204,28 +201,25 @@ void Archive::GetArchiveFile(const std::string &archiveFilePath, Archive::Archiv
 	string archivePath = archiveFilePath.substr(0, start + 4);
 	string filePath = archiveFilePath.substr(start + 5, archiveFilePath.size());
 
-	struct archive *reading = nullptr;
-	archive_entry *entry = nullptr;
+	ReadingHandle reading(archive_read_new(), archive_read_free);
+	EntryHandle entry(nullptr, archive_entry_free);
 	string firstEntry;
 
 	if(!InitArchive(archivePath, reading, entry, firstEntry))
 		return;
 
-	while(archive_read_next_header(reading, &entry) == ARCHIVE_OK)
+	while(archive_read_next_header2(reading.get(), entry.get()) == ARCHIVE_OK)
 	{
-		string name = archive_entry_pathname(entry);
+		string name = archive_entry_pathname(entry.get());
 		if(name == filePath)
 		{
-			size_t size = archive_entry_size(entry);
+			size_t size = archive_entry_size(entry.get());
 
 			handle.Allocate(size);
-			archive_read_data(reading, handle.GetData(), size);
+			archive_read_data(reading.get(), handle.GetData(), size);
 			handle.CreateFileFromData();
 
 			return;
 		}
 	}
-	archive_read_free(reading);
-
-	return;
 }
