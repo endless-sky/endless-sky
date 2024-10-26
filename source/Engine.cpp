ce18549c922038e5210c21022a5ed5f30e32a2bf
Zitchas <32169904+Zitchas@users.noreply.github.com>
2024-10-26 06:31:45 -0400

    Fixes "no mobility scanning but is escort so overlaps with crew" bug

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -894,7 +894,8 @@ void Engine::Step(bool isActive)
 			{
 				info.SetString("target crew", to_string(target->Crew()));
 				if(targetRange <= (strategicScanRange + accelerationScanRange)
-					|| targetRange <= (strategicScanRange + velocityScanRange))
+					|| targetRange <= (strategicScanRange + velocityScanRange)
+					|| target->IsYours())
 				{
 					info.SetCondition("mobility crew display");
 				}
