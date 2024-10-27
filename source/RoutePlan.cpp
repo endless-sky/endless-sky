d5093542b8f679d6f9f9ae39e24d6ac5af5103d3
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2024-10-27 06:52:42 +0100

    Update source/RoutePlan.cpp

--- source/RoutePlan.cpp
+++ source/RoutePlan.cpp
@@ -1,5 +1,5 @@
 /* RoutePlan.cpp
-Copyright (c) 2015 by alextd
+Copyright (c) 2022 by alextd
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
