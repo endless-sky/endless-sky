82b7636da180e8acd4ad5c4bcc12a070dc982929
Anarchist2 <60202690+Anarchist2@users.noreply.github.com>
2024-10-29 00:22:44 +1000

    Make all non-leading ships in a fleet wait until the lead is full until fleeting

--- source/AI.cpp
+++ source/AI.cpp
@@ -325,7 +325,7 @@ namespace {
 		if(ship.GetAICache().NeedsAmmo())
 			return true;
 
-		if(personality.IsGetaway() && ship.Cargo().Free() == 0 && !ship.CanBeCarried())
+		if(personality.IsGetaway() && ship.Cargo().Free() == 0 && !ship.GetParent())
 			return true;
 
 		return false;
