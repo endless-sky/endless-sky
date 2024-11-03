4241a79540fc3b6c7dd3ec940ade6a1616c53411
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2024-11-03 17:15:03 +0800

    Update source/Preferences.cpp
    Co-authored-by: TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>

--- source/Preferences.cpp
+++ source/Preferences.cpp
@@ -172,7 +172,6 @@ void Preferences::Load()
 	settings["Ship outlines in HUD"] = true;
 	settings["Extra fleet status messages"] = true;
 	settings["Target asteroid based on"] = true;
-	settings["Play ding on destination"] = false;
 
 	DataFile prefs(Files::Config() + "preferences.txt");
 	for(const DataNode &node : prefs)
