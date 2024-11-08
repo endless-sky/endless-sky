375690863d33989e875d19fabfa32b25c165a09b
Anarchist2 <60202690+Anarchist2@users.noreply.github.com>
2024-11-09 02:51:02 +1000

    Update source/MissionPanel.cpp
    Co-authored-by: TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>

--- source/MissionPanel.cpp
+++ source/MissionPanel.cpp
@@ -219,10 +219,7 @@ void MissionPanel::Step()
 			// Only highlight and pan to the destination if the mission is visible.
 			// Otherwise, pan to the player's current system.
 			specialSystem = mission->IsVisible() ? mission->Destination()->GetSystem() : nullptr;
-			if(specialSystem)
-				CenterOnSystem(specialSystem);
-			else
-				CenterOnSystem(player.GetSystem());
+			CenterOnSystem(specialSystem ? specialSystem : player.GetSystem());
 
 			mission->Do(Mission::OFFER, player, GetUI());
 		}
