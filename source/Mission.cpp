c8b4bf261b59a7e8490e741b18c4be94cc254929
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2024-12-01 08:11:09 +0100

    Style oversight

--- source/Mission.cpp
+++ source/Mission.cpp
@@ -1678,5 +1678,5 @@ bool Mission::ParseContraband(const DataNode &node)
 
 const bool Mission::SortHelper(const Mission &a, const Mission &b)
 {
-	return (a.order == b.order) ? a.name < b.name: a.order < b.order;
+	return (a.order == b.order) ? a.name < b.name : a.order < b.order;
 }
