660d11d90b59c335e1eb70e3a0d38c620c3128ba
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2024-12-01 13:18:59 +0100

    Update source/Mission.cpp
    Co-authored-by: Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>

--- source/Mission.cpp
+++ source/Mission.cpp
@@ -1673,5 +1673,5 @@ bool Mission::ParseContraband(const DataNode &node)
 
 const bool Mission::SortHelper(const Mission &a, const Mission &b)
 {
-	return (a.order == b.order) ? a.name < b.name : a.order < b.order;
+	return a.order == b.order ? a.name < b.name : a.order < b.order;
 }
