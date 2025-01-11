7a9ed3133974c154cfe273fbe24f40976a4c2ebe
tibetiroka <tibetiroka@gmail.com>
2025-01-11 22:36:29 +0100

    github???

--- source/MapPanel.h
+++ source/MapPanel.h
@@ -72,7 +72,7 @@ public:
 public:
 	explicit MapPanel(PlayerInfo &player, int commodity = SHOW_REPUTATION,
 		const System *special = nullptr, bool fromMission = false);
-  virtual ~MapPanel() override;
+	virtual ~MapPanel() override;
 
 	virtual void Step() override;
 	virtual void Draw() override;
