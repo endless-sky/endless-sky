80be881b2a4fe84d00169f2ede0b862c20e04aca
Daniel <101683475+Koranir@users.noreply.github.com>
2025-01-13 13:13:41 +1100

    Update MapPanel.cpp

--- source/MapPanel.cpp
+++ source/MapPanel.cpp
@@ -804,7 +804,11 @@ void MapPanel::Select(const System *system)
 {
 	if(!system)
 		return;
+
 	selectedSystem = system;
+	// Update the cache to apply any visual changes needed after the selected system was changed.
+	UpdateCache();
+
 	vector<const System *> &plan = player.TravelPlan();
 	Ship *flagship = player.Flagship();
 	if(!flagship || (!plan.empty() && system == plan.front()))
@@ -1128,7 +1132,7 @@ void MapPanel::UpdateCache()
 		const bool canViewSystem = player.CanView(system);
 		nodes.emplace_back(system.Position(), color,
 			player.KnowsName(system) ? system.DisplayName() : "",
-			(&system == &playerSystem) ? closeNameColor : farNameColor,
+			(&system == &playerSystem || &system == selectedSystem) ? closeNameColor : farNameColor,
 			canViewSystem ? system.GetGovernment() : nullptr,
 			canViewSystem ? system.GetMapIcons() : unmappedSystem);
 	}
