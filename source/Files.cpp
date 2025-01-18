fe2a998001aad6fe0f04d2866f197ad3b763be19
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2025-01-18 20:42:49 +0100

    Update source/Files.cpp

--- source/Files.cpp
+++ source/Files.cpp
@@ -296,7 +296,7 @@ time_t Files::Timestamp(const filesystem::path &filePath)
 
 void Files::Copy(const filesystem::path &from, const filesystem::path &to)
 {
-#ifdef WIN32
+#ifdef _WIN32
 	// Due to a mingw bug, the overwrite_existing flag is not respected on windows.
 	// TODO remove once it is fixed.
 	if(Exists(to))
