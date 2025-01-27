2dd2afe8da203b57b70d5972687e199c161c1bb2
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2025-01-27 09:50:04 +0100

    Update source/System.h
    Co-authored-by: Loymdayddaud <145969603+TheGiraffe3@users.noreply.github.com>

--- source/System.h
+++ source/System.h
@@ -17,6 +17,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "Hazard.h"
 #include "Point.h"
+#include "Raiders.h"
 #include "RandomEvent.h"
 #include "Set.h"
 #include "StellarObject.h"
