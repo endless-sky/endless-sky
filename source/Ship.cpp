b1e5b308224ed715046c9e3ae4621799a63b739a
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2025-02-22 23:26:11 +0100

    don't need this check anymore

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -4940,8 +4940,7 @@ void Ship::DoEngineVisuals(vector<Visual> &visuals, bool isUsingAfterburner)
 			Point effectVelocity = velocity - 6. * afterburnerAngle.Unit();
 			for(auto &&it : Attributes().AfterburnerEffects())
 				for(int i = 0; i < it.second; ++i)
-					visuals.emplace_back(*it.first, pos, effectVelocity, afterburnerAngle, Point{},
-						it.first->RespectsEngineZoom() ? point.zoom : 1.);
+					visuals.emplace_back(*it.first, pos, effectVelocity, afterburnerAngle, Point{}, point.zoom);
 		}
 	}
 }
