91fda4f3d65718b48947ec95a4b76b8873490ffd
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2025-02-23 17:10:03 +0100

    Clipboard refactor and enhancements.

--- source/ConversationPanel.cpp
+++ source/ConversationPanel.cpp
@@ -18,6 +18,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "text/alignment.hpp"
 #include "audio/Audio.h"
 #include "BoardingPanel.h"
+#include "text/Clipboard.h"
 #include "Color.h"
 #include "Command.h"
 #include "Conversation.h"
@@ -256,26 +257,31 @@ bool ConversationPanel::KeyDown(SDL_Keycode key, Uint16 mod, const Command &comm
 	}
 	if(choices.empty())
 	{
+		// Allow editing the text. The tab key toggles to the other entry field,
+		// as does the return key if the other field is still empty.
+		// Don't allow characters that can't be used in a file name.
+		static constexpr string FORBIDDEN = "/\\?*:|\"<>~";
+		// Prevent the name from being so large that it cannot be saved.
+		// Most path components can be at most 255 bytes.
+		size_t MAX_NAME_LENGTH = 250;
+#if defined _WIN32
+		MAX_NAME_LENGTH -= PATH_LENGTH;
+#endif
+
 		// Right now we're asking the player to enter their name.
 		string &name = (choice ? lastName : firstName);
 		string &otherName = (choice ? firstName : lastName);
-		// Allow editing the text. The tab key toggles to the other entry field,
-		// as does the return key if the other field is still empty.
-		if(key >= ' ' && key <= '~')
+		if(Clipboard::KeyDown(name, key, mod, MAX_NAME_LENGTH, FORBIDDEN))
+		{
+			// Input handled by Clipboard.
+		}
+		else if(key >= ' ' && key <= '~')
 		{
 			// Apply the shift or caps lock key.
 			char c = ((mod & KMOD_SHIFT) ? SHIFT[key] : key);
 			// Caps lock should shift letters, but not any other keys.
 			if((mod & KMOD_CAPS) && c >= 'a' && c <= 'z')
 				c += 'A' - 'a';
-			// Don't allow characters that can't be used in a file name.
-			static const string FORBIDDEN = "/\\?*:|\"<>~";
-			// Prevent the name from being so large that it cannot be saved.
-			// Most path components can be at most 255 bytes.
-			size_t MAX_NAME_LENGTH = 250;
-#if defined _WIN32
-			MAX_NAME_LENGTH -= PATH_LENGTH;
-#endif
 			if(FORBIDDEN.find(c) == string::npos && (name.size() + otherName.size()) < MAX_NAME_LENGTH)
 				name += c;
 			else
