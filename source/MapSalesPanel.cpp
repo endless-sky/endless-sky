ab3f8fabc6f3d0645c4913b90fd71a7aba96ea6c
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2025-04-05 19:56:05 +0200

    Update source/MapSalesPanel.cpp
    Co-authored-by: TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>

--- source/MapSalesPanel.cpp
+++ source/MapSalesPanel.cpp
@@ -15,7 +15,6 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "MapSalesPanel.h"
 
-#include "audio/Audio.h"
 #include "CategoryTypes.h"
 #include "Command.h"
 #include "Dialog.h"
