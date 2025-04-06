0a910824da4e209f5f91ea520be70b7e881d6353
tibetiroka <tibetiroka@gmail.com>
2025-04-06 12:26:47 +0200

    Style

--- source/Point.cpp
+++ source/Point.cpp
@@ -334,7 +334,7 @@ Point Point::Unit() const
 	double b = LengthSquared();
 	if(!b)
 		return Point(1., 0.);
-	return *this / sqrt(length);
+	return *this / sqrt(b);
 #else
 	double b = x * x + y * y;
 	if(!b)
