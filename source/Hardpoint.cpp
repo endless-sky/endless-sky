df2e29a2cdee2a59337958bc9733117ae9b23c84
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2025-04-30 11:00:06 +0200

    support inverting the range to make handling edge cases easier

--- source/Hardpoint.cpp
+++ source/Hardpoint.cpp
@@ -203,8 +203,12 @@ bool Hardpoint::IsBlind() const
 	return any_of(baseAttributes.blindspots.begin(), baseAttributes.blindspots.end(),
 		[this](pair<Angle, Angle> blindspot)
 		{
-			return angle.Degrees() >= (blindspot.first + baseAngle).Degrees()
-				&& angle.Degrees() <= (blindspot.second + baseAngle).Degrees();
+			if(blindspot.first.Degrees() < blindspot.second.Degrees())
+				return angle.Degrees() >= (blindspot.first + baseAngle).Degrees()
+					&& angle.Degrees() <= (blindspot.second + baseAngle).Degrees();
+			else
+				return angle.Degrees() >= (blindspot.first + baseAngle).Degrees()
+					|| angle.Degrees() <= (blindspot.second + baseAngle).Degrees();
 		});
 }
 
