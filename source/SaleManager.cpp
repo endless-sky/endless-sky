65b2ec04f1db98e6db39cafc3adf5593c827f57d
Amazinite <17688683+Amazinite@users.noreply.github.com>
2025-05-05 01:33:35 -0400

    Create a SaleManager class that handles combining ShopModifier and Depreciation to determine the final cost of an item

---
+++ source/SaleManager.cpp
@@ -0,0 +1,217 @@
+/* SaleManager.cpp
+Copyright (c) 2025 by Amazinite
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "SaleManager.h"
+
+#include "Date.h"
+#include "Depreciation.h"
+#include "GameData.h"
+#include "Outfit.h"
+#include "PlayerInfo.h"
+#include "Ship.h"
+#include "Shop.h"
+#include "ShopPricing.h"
+#include "StockItem.h"
+
+using namespace std;
+
+
+
+SaleManager::SaleManager(const PlayerInfo &player, const Stock<Outfit> *outfitter, const Stock<Ship> *shipyard)
+	: player(player), outfitter(outfitter), shipyard(shipyard), day(player.GetDate().DaysSinceEpoch()),
+	fleetDepreciation(player.FleetDepreciation()), stockDepreciation(player.StockDepreciation())
+{
+}
+
+
+
+int64_t SaleManager::BuyValue(const Outfit *outfit, int count) const
+{
+	if(!outfit || count <= 0)
+		return 0;
+
+	int64_t baseCost = outfit->Cost();
+	double fraction = stockDepreciation.ValueFraction(outfit, day, count);
+
+	if(outfitter)
+	{
+		// If this item is for sale from the outfitter, use the outfitter's price modifier.
+		const StockItem<Outfit> *stock = outfitter->Get(outfit);
+		if(stock)
+		{
+			const ShopPricing &modifier = stock->BuyModifier();
+			return modifier.Value(baseCost, fraction, count);
+		}
+	}
+
+	// Otherwise, just return the outfit cost with normal appreciation applied.
+	return baseCost * fraction;
+}
+
+
+
+int64_t SaleManager::SellValue(const Outfit *outfit, int count) const
+{
+	if(!outfit || count <= 0)
+		return 0;
+
+	int64_t baseCost = outfit->Cost();
+
+	if(outfitter)
+	{
+		// If this item is for sale from the outfitter, use the outfitter's price modifier.
+		const StockItem<Outfit> *stock = outfitter->Get(outfit);
+		if(stock)
+		{
+			// If an item was just purchased, we want to sell it at the same value it was
+			// purchased at. Determine how many of this item being sold are brand new,
+			// and for those new items, use the buying price. For all remaining items,
+			// use the modified selling price.
+			int newCount = fleetDepreciation.NumberNew(outfit, count);
+			count -= newCount;
+			int64_t value = 0;
+			if(newCount)
+				value += BuyValue(outfit, newCount);
+			if(count)
+			{
+				double fraction = fleetDepreciation.ValueFraction(outfit, day, count);
+				const ShopPricing &modifier = stock->SellModifier();
+				value += modifier.Value(baseCost, fraction, count);
+			}
+			return value;
+		}
+	}
+
+	// Otherwise, just return the base cost with normal depreciation applied.
+	double fraction = fleetDepreciation.ValueFraction(outfit, day, count);
+	return baseCost * fraction;
+}
+
+
+
+int64_t SaleManager::BuyValue(const Ship *ship, int count, bool chassisOnly) const
+{
+	if(!ship || count <= 0)
+		return 0;
+
+	ship = GameData::Ships().Get(ship->TrueModelName());
+	int64_t baseCost = ship->ChassisCost();
+	double fraction = stockDepreciation.ValueFraction(ship, day, count);
+
+	int64_t value = 0;
+	const StockItem<Ship> *stock = nullptr;
+	if(shipyard)
+	{
+		// If this item is for sale from the shipyard, use the shipyard's price modifier.
+		// Note: The shipyard should always have a StockItem for the given ship, at least
+		// unless we make it so that selling a ship stores its chassis in the shipyard.
+		stock = shipyard->Get(ship);
+		if(stock)
+		{
+			const ShopPricing &modifier = stock->BuyModifier();
+			value += modifier.Value(baseCost, fraction, count);
+		}
+	}
+	if(!shipyard && !stock)
+	{
+		// Otherwise, just use the ship cost with normal appreciation applied.
+		value += baseCost * fraction;
+	}
+
+	if(!chassisOnly)
+		for(const auto &it : ship->Outfits())
+			value += BuyValue(it.first, it.second);
+	return value;
+}
+
+
+
+int64_t SaleManager::SellValue(const Ship *ship, int count) const
+{
+	if(!ship || count <= 0)
+		return 0;
+
+	ship = GameData::Ships().Get(ship->TrueModelName());
+	int64_t baseCost = ship->ChassisCost();
+
+	if(shipyard)
+	{
+		// If this item is for sale from the shipyard, use the shipyard's price modifier.
+		const StockItem<Ship> *stock = shipyard->Get(ship);
+		if(stock)
+		{
+			// If an item was just purchased, we want to sell it at the same value it was
+			// purchased at. Determine how many of this item being sold are brand new,
+			// and for those new items, use the buying price. For all remaining items,
+			// use the modified selling price.
+			int newCount = fleetDepreciation.NumberNew(ship, count);
+			count -= newCount;
+			int64_t value = 0;
+			if(newCount)
+				value += BuyValue(ship, newCount);
+			if(count)
+			{
+				double fraction = fleetDepreciation.ValueFraction(ship, day, count);
+				const ShopPricing &modifier = stock->SellModifier();
+				value += modifier.Value(baseCost, fraction, count);
+			}
+			return value;
+		}
+	}
+
+	// Otherwise, just return the base cost with normal depreciation applied.
+	double fraction = fleetDepreciation.ValueFraction(ship, day, count);
+	return baseCost * fraction;
+}
+
+
+
+int64_t SaleManager::SellValue(const Ship &ship) const
+{
+	int64_t value = SellValue(&ship);
+	for(const auto &it : ship.Outfits())
+		value += SellValue(it.first, it.second);
+	return value;
+}
+
+
+
+int64_t SaleManager::SellValue(const vector<shared_ptr<Ship>> &fleet, bool chassisOnly) const
+{
+	if(fleet.empty())
+		return 0;
+
+	// Determine how many of each ship and outfit is being sold.
+	map<const Ship *, int> shipCount;
+	map<const Outfit *, int> outfitCount;
+
+	for(const shared_ptr<Ship> &ship : fleet)
+	{
+		const Ship *base = GameData::Ships().Get(ship->TrueModelName());
+		++shipCount[base];
+
+		if(!chassisOnly)
+			for(const auto &it : ship->Outfits())
+				outfitCount[it.first] += it.second;
+	}
+
+	// Add up the value across all ship chassis and outfits being sold.
+	int64_t value = 0;
+	for(const auto &it : shipCount)
+		value += SellValue(it.first, it.second);
+	for(const auto &it : outfitCount)
+		value += SellValue(it.first, it.second);
+	return value;
+}
---
+++ source/SaleManager.h
@@ -0,0 +1,65 @@
+/* SaleManager.h
+Copyright (c) 2025 by Amazinite
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "Stock.h"
+
+#include <memory>
+#include <vector>
+
+class Depreciation;
+class Outfit;
+class PlayerInfo;
+class Ship;
+
+
+
+// A class for managing the buying and selling of items in a shop.
+class SaleManager {
+public:
+	SaleManager(const PlayerInfo &player, const Stock<Outfit> *outfitter = nullptr,
+		const Stock<Ship> *shipyard = nullptr);
+
+	// The buy value of an outfit in the outfitter.
+	int64_t BuyValue(const Outfit *outfit, int count = 1) const;
+	// The sell value of an outfit in the outfitter. May equal the buy value if the
+	// shop has no modifiers, or the outfit was just purchased and is being sold back
+	// to the shop at its full price.
+	int64_t SellValue(const Outfit *outfit, int count = 1) const;
+
+	// The buy value of a ship in the shipyard.
+	int64_t BuyValue(const Ship *ship, int count = 1, bool chassisOnly = false) const;
+	// The sell value of a ship in the shipyard. May equal the buy value if the
+	// shop has no modifiers, or the ship was just purchased and is being sold back
+	// to the shop at its full price. This is only the value of the chassis.
+	int64_t SellValue(const Ship *ship, int count = 1) const;
+
+	// The sell value of one of the player's ships, chassis and outfits included.
+	int64_t SellValue(const Ship &ship) const;
+	// The sell value from a collection of the player's ships.
+	int64_t SellValue(const std::vector<std::shared_ptr<Ship>> &fleet, bool chassisOnly = false) const;
+
+
+private:
+	const PlayerInfo &player;
+	const Stock<Outfit> *outfitter = nullptr;
+	const Stock<Ship> *shipyard = nullptr;
+
+	// The current number of days since the epoch, for determining depreciation.
+	int day;
+	const Depreciation &fleetDepreciation;
+	const Depreciation &stockDepreciation;
+};
