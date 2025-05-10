0bdf7ed7383d0a7f0062ad85334e04cd1cd79c0a
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2025-05-10 18:41:16 +0200

    use Angle::IsInRange
    Co-authored-by: Peter van der Meer <peter.vd.meer@gmail.com>

--- source/Hardpoint.cpp
+++ source/Hardpoint.cpp
@@ -203,12 +203,7 @@ bool Hardpoint::IsBlind() const
 	return any_of(baseAttributes.blindspots.begin(), baseAttributes.blindspots.end(),
 		[this](pair<Angle, Angle> blindspot)
 		{
-			if(blindspot.first.Degrees() <= blindspot.second.Degrees())
-				return angle.Degrees() >= (blindspot.first + baseAngle).Degrees()
-					&& angle.Degrees() <= (blindspot.second + baseAngle).Degrees();
-			else
-				return angle.Degrees() >= (blindspot.first + baseAngle).Degrees()
-					|| angle.Degrees() <= (blindspot.second + baseAngle).Degrees();
+			return angle.IsInRange(blindspot.first + baseAngle, blindspot.second + baseAngle);
 		});
 }
 
