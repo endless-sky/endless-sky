28bd676ed229fbbf403c4e6a694ecc90db8fd6c0
Amazinite <jsteck2000@gmail.com>
2025-05-27 02:27:52 -0400

    Remove constants from Engine that were moved to Camera

--- source/Engine.cpp
+++ source/Engine.cpp
@@ -244,9 +244,6 @@ namespace {
 
 	const double RADAR_SCALE = .025;
 	const double MAX_FUEL_DISPLAY = 3000.;
-
-	const double CAMERA_VELOCITY_TRACKING = 0.1;
-	const double CAMERA_POSITION_CENTERING = 0.01;
 }
 
 
