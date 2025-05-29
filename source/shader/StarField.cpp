993caefe3e715b13722f33d59b54a7250bebf1ea
Wladimir Palant <374261+palant@users.noreply.github.com>
2025-05-29 21:49:31 +0200

    fix(mechanics): Consider sprite size correctly when drawing haze (#5711)

--- source/shader/StarField.cpp
+++ source/shader/StarField.cpp
@@ -257,14 +257,11 @@ void StarField::Draw(const Point &blur, const System *system) const
 	else
 		transparency = 0.;
 
-	// Set zoom to a higher level than stars to avoid premature culling.
-	zoom = min(.25, zoom / 1.4);
-
 	// Any object within this range must be drawn. Some haze sprites may repeat
 	// more than once if the view covers a very large area.
 	Point size = Point(1., 1.) * haze[0].front().Radius();
-	Point topLeft = pos + (Screen::TopLeft() - size) / zoom;
-	Point bottomRight = pos + (Screen::BottomRight() + size) / zoom;
+	Point topLeft = pos + Screen::TopLeft() / zoom - size;
+	Point bottomRight = pos + Screen::BottomRight() / zoom + size;
 	if(transparency > 0.)
 		AddHaze(drawList, haze[1], topLeft, bottomRight, 1 - transparency);
 	AddHaze(drawList, haze[0], topLeft, bottomRight, transparency);
