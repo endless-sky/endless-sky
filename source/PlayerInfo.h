ea5b3ae13a3a13cff8bd335dbcbc3195223accab
Djordje Petrović <djordje.pe.petrovic@gmail.com>
2025-06-22 16:42:10 +0200

    fix: make PlayerInfo::SetFlagship public

--- source/PlayerInfo.h
+++ source/PlayerInfo.h
@@ -162,6 +162,8 @@ public:
 	const Ship *Flagship() const;
 	Ship *Flagship();
 	const std::shared_ptr<Ship> &FlagshipPtr();
+	// Set the flagship (on departure or during flight).
+	void SetFlagship(Ship &other);
 	// Get the full list of ships the player owns.
 	const std::vector<std::shared_ptr<Ship>> &Ships() const;
 	// Inspect the flightworthiness of the player's active fleet as a whole to
@@ -387,9 +389,6 @@ private:
 
 	// Check for and apply any punitive actions from planetary security.
 	void Fine(UI *ui);
-
-	// Set the flagship (on departure or during flight).
-	void SetFlagship(Ship &other);
 
 	void HandleFlagshipParking(Ship *oldFirstShip, Ship *newFirstShip);
 
