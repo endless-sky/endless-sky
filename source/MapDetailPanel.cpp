2b4e8e95ee2fb87596fcaaa18de6b1da3a5a3a4f
xobes <ajzobro@gmail.com>
2025-07-06 09:54:28 -0500

    used PointerShader instead of colored text

--- source/MapDetailPanel.cpp
+++ source/MapDetailPanel.cpp
@@ -817,23 +817,46 @@ void MapDetailPanel::DrawInfo()
 	// Adapt the coordinates for the text (the sprite is drawn from a center coordinate).
 	uiPoint.X() -= (tradeSprite->Width() / 2. - textMargin);
 	uiPoint.Y() -= (tradeSprite->Height() / 2. - textMargin);
+
+	// Don't "compare" prices if the current system is uninhabited and thus has no prices to compare to.
+	bool noCompare = (!player.GetSystem() || !player.GetSystem()->IsInhabited(player.Flagship()));
+	int value = 0;
+	double lowCompare = 0;
+	double halfCompare = 0;
+
+	// When comparing prices, determine min/max deltas in order to represent commodity delta prices for displayed
+	// commodities as a gradient.
+	if(!noCompare && canView && selectedSystem->IsInhabited(player.Flagship()))
+	{
+		double highCompare = 0;
+		for(const Trade::Commodity &commodity : GameData::Commodities())
+		{
+			value = selectedSystem->Trade(commodity.name);
+			int localValue = (player.GetSystem() ? player.GetSystem()->Trade(commodity.name) : 0);
+			if(value && localValue)
+			{
+				value -= localValue;
+				lowCompare = value < lowCompare ? value : lowCompare;
+				highCompare = value > highCompare ? value : highCompare;
+			}
+		}
+		halfCompare = (0.5 * (highCompare - lowCompare));
+	}
+
 	for(const Trade::Commodity &commodity : GameData::Commodities())
 	{
 		bool isSelected = false;
 		if(static_cast<unsigned>(this->commodity) < GameData::Commodities().size())
 			isSelected = (&commodity == &GameData::Commodities()[this->commodity]);
-		const Color &color = isSelected ? medium : dim;
+		Color color = isSelected ? medium : dim;
 
 		font.Draw(commodity.name, uiPoint, color);
 
 		string price;
 		if(canView && selectedSystem->IsInhabited(player.Flagship()))
 		{
-			int value = selectedSystem->Trade(commodity.name);
+			value = selectedSystem->Trade(commodity.name);
 			int localValue = (player.GetSystem() ? player.GetSystem()->Trade(commodity.name) : 0);
-			// Don't "compare" prices if the current system is uninhabited and
-			// thus has no prices to compare to.
-			bool noCompare = (!player.GetSystem() || !player.GetSystem()->IsInhabited(player.Flagship()));
 			if(!value)
 				price = "----";
 			else if(noCompare || player.GetSystem() == selectedSystem || !localValue)
@@ -856,7 +879,20 @@ void MapDetailPanel::DrawInfo()
 		const auto alignRight = Layout(140, Alignment::RIGHT, Truncate::BACK);
 		font.Draw({price, alignRight}, uiPoint, color);
 
+		if(!noCompare && player.GetSystem() != selectedSystem && canView &&
+			selectedSystem->IsInhabited(player.Flagship()))
+		{
+			// Avoid divide by zero.
+			halfCompare = halfCompare < 1 ? 1 : halfCompare;
+			double v = (static_cast<double>(value) - (lowCompare + halfCompare)) / halfCompare;
+			color = MapColor(v);
+			// Draw up/down/equals arrows based on price delta (value)
+			PointerShader::Draw(uiPoint + Point(95., 7. + (-7 * v)), Point(0., 1), 20.f,
+				static_cast<float>(-14. * v), 0.f, color);
+		}
+
 		if(isSelected)
+			// const Point &center, const Point &angle, float width, float height, float offset, const Color &color
 			PointerShader::Draw(uiPoint + Point(0., 7.), Point(1., 0.), 10.f, 10.f, 0.f, color);
 
 		uiPoint.Y() += 20.;
