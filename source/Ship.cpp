0d7792e4dc6cd3a020dc0fd677704f3ec5da0270
Anarchist2 <60202690+Anarchist2@users.noreply.github.com>
2025-08-05 11:05:16 +1000

    check if energy usage is positive (requires energy)

--- source/Ship.cpp
+++ source/Ship.cpp
@@ -2263,13 +2263,13 @@ bool Ship::IsIonized() const
 		return false;
 
 	// A ship can only be fully ionized if its engines or weapons require energy.
-	bool movementEnergy = attributes.Get("thrusting energy")
-		|| attributes.Get("reverse thrusting energy")
-		|| attributes.Get("turning energy");
+	bool movementEnergy = attributes.Get("thrusting energy") > 0
+		|| attributes.Get("reverse thrusting energy") > 0
+		|| attributes.Get("turning energy") > 0;
 
 	bool firingEnergy = false;
 	for(const auto &it : outfits)
-		if(it.first->IsWeapon() && it.first->FiringEnergy())
+		if(it.first->IsWeapon() && it.first->FiringEnergy() > 0)
 			firingEnergy = true;
 
 	return movementEnergy || firingEnergy ? ionization > energy : false;
