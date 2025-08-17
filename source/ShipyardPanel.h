d4220da956ad08e698722f1f3435451591ad9b5e
xobes <ajzobro@gmail.com>
2025-08-16 22:07:54 -0500

    Cleaned up several commits into new clean branch; implements additional button to sell ship hull and store outfits. Builds on top of the outfitter button rework in PR #10743 and button click zones in PR #11447

--- source/ShipyardPanel.h
+++ source/ShipyardPanel.h
@@ -43,25 +43,23 @@ protected:
 	virtual int TileSize() const override;
 	virtual bool HasItem(const std::string &name) const override;
 	virtual void DrawItem(const std::string &name, const Point &point) override;
-	virtual int DividerOffset() const override;
-	virtual int DetailWidth() const override;
+	virtual double ButtonPanelHeight() const override;
 	virtual double DrawDetails(const Point &center) override;
-	virtual BuyResult CanBuy(bool onlyOwned = false) const override;
-	virtual void Buy(bool onlyOwned = false) override;
-	virtual bool CanSell(bool toStorage = false) const override;
-	virtual void Sell(bool toStorage = false) override;
-	virtual bool CanSellMultiple() const override;
+
+	virtual void DrawButtons() override;
 	virtual int FindItem(const std::string &text) const override;
+	virtual TransactionResult HandleShortcuts(char key) override;
 
 
 private:
+	TransactionResult CanDoBuyButton() const;
+	void DoBuyButton();
+	void Sell(bool storeOutfits);
 	void BuyShip(const std::string &name);
 	void SellShipAndOutfits();
 	void SellShipChassis();
 	void SellShip(bool toStorage);
 
-
-private:
 	int modifier;
 
 	Sale<Ship> shipyard;
