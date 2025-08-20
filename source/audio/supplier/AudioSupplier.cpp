202dd7ba6498f76a70e48d838dd2fe805bb5bfbe
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2025-08-20 12:06:42 +0200

    Update source/audio/supplier/AudioSupplier.cpp
    Co-authored-by: TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>

--- source/audio/supplier/AudioSupplier.cpp
+++ source/audio/supplier/AudioSupplier.cpp
@@ -33,6 +33,8 @@ void AudioSupplier::DestroyBuffer(ALuint buffer)
 	alDeleteBuffers(1, &buffer);
 }
 
+
+
 AudioSupplier::AudioSupplier(bool is3x, bool isLooping)
 	: is3x(is3x), nextPlaybackIs3x(is3x), isLooping(isLooping)
 {
