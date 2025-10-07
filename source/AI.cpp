410c017baca78175de0290fbd7b1ce16f6dce2e3
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2025-10-07 05:16:49 +0200

    fix(mechanics): Take maximum velocity into account when approaching a moving target (#11788)

--- source/AI.cpp
+++ source/AI.cpp
@@ -2496,7 +2496,10 @@ bool AI::MoveTo(const Ship &ship, Command &command, const Point &targetPosition,
 	bool facingAgainstTarget = angle.Unit().Dot(-wantedAcceleration.Unit()) > .9;
 	bool hasCruiseSpeed = (cruiseSpeed > 0.);
 
-	if(reverseTime < forwardTime)
+	bool canMatchVelocityForward = ship.MaxVelocity() * ship.MaxVelocity() >= targetVelocity.LengthSquared();
+	bool canMatchVelocityReverse = ship.MaxReverseVelocity() * ship.MaxReverseVelocity() >= targetVelocity.LengthSquared();
+	if((canMatchVelocityReverse && (reverseTime < forwardTime || !canMatchVelocityForward))
+			|| (!canMatchVelocityReverse && !canMatchVelocityForward && reverseTime < forwardTime))
 		command.SetTurn(TurnToward(ship, -wantedAcceleration));
 	else
 		command.SetTurn(TurnToward(ship, wantedAcceleration));
