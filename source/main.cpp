5feb232d56135c89cdb9e768add986b6321dde1b
TomGoodIdea <108272452+TomGoodIdea@users.noreply.github.com>
2025-10-21 17:01:58 +0200

    Also handle zoom changes

--- source/main.cpp
+++ source/main.cpp
@@ -337,7 +337,8 @@ void GameLoop(PlayerInfo &player, TaskQueue &queue, const Conversation &conversa
 			else if(event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
 			{
 				// The window has been resized. Adjust the raw screen size
-				// and the OpenGL viewport to match.
+				// and the OpenGL viewport to match. The order (window first, then UI panels)
+				// matters as window changes can adjust the zoom factor.
 				GameWindow::AdjustViewport();
 				menuPanels.AdjustViewport();
 				gamePanels.AdjustViewport();
