5e8cfdb591418e268bcf56e89dd98e2a5ad9df93
xobes <ajzobro@gmail.com>
2025-11-14 20:05:16 -0600

    commit with debug overlays

--- source/MapDetailPanel.cpp
+++ source/MapDetailPanel.cpp
@@ -26,7 +26,6 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "shader/FillShader.h"
 #include "text/Font.h"
 #include "text/FontSet.h"
-#include "text/Format.h"
 #include "GameData.h"
 #include "Government.h"
 #include "Interface.h"
@@ -36,9 +35,9 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "Planet.h"
 #include "PlayerInfo.h"
 #include "shader/PointerShader.h"
-#include "Politics.h"
 #include "Preferences.h"
 #include "Radar.h"
+#include "Rectangle.h"
 #include "shader/RingShader.h"
 #include "Screen.h"
 #include "Ship.h"
@@ -142,6 +141,8 @@ void MapDetailPanel::Step()
 void MapDetailPanel::Draw()
 {
 	MapPanel::Draw();
+
+	clickZones.clear();
 
 	DrawInfo();
 	DrawOrbits();
@@ -416,34 +417,16 @@ bool MapDetailPanel::Click(int x, int y, MouseButton button, int clicks)
 	const double arrowOffset = mapInterface->GetValue("arrow x offset");
 	const double planetCardHeight = MapPlanetCard::Height();
 
-	if(x < Screen::Left() + 170)
-	{
-		// The player clicked in the left-hand interface. This could be the system
-		// name, the system government, a planet box, the commodity listing, or nothing.
-		if(y >= tradeY && y < tradeY + 200)
+	// Check all the click zones.
+	Point clickPoint(x, y);
+	for(const ClickZone<int> zone : clickZones)
+		if(zone.Contains(clickPoint))
 		{
-			// The player clicked on a tradable commodity. Color the map by its price.
-			isStars = false;
-			SetCommodity((y - tradeY) / 20);
+			SetCommodity(zone.Value());
 			return true;
 		}
-		// Clicking the system name activates the view of the player's reputation with various governments.
-		// But the bit to the left will show danger of pirate/raid fleets instead.
-		else if(y < governmentY && y > governmentY - 30)
-		{
-			isStars = false;
-			SetCommodity(x < Screen::Left() + mapInterface->GetValue("text margin") ?
-				SHOW_DANGER : SHOW_REPUTATION);
-		}
 
-		// Clicking the government name activates the view of system / planet ownership.
-		else if(y >= governmentY && y < governmentY + 25)
-		{
-			isStars = false;
-			SetCommodity(SHOW_GOVERNMENT);
-		}
-
-	}
+	// Check the planet cards.
 	if(y <= Screen::Top() + planetPanelHeight + 30 && x <= Screen::Left() + planetCardWidth + arrowOffset + 10)
 	{
 		for(auto &card : planetCards)
@@ -765,15 +748,21 @@ void MapDetailPanel::DrawInfo()
 	// This needs to fill from the start of the screen.
 	FillShader::Fill(Rectangle::FromCorner(Screen::TopLeft(), size), back);
 
-	const double startingX = mapInterface->GetValue("starting X");
+	////////////////////////////////////////
+	const double planetCardWidth = planetCardInterface->GetValue("width");
+	const double arrowOffset = mapInterface->GetValue("arrow x offset");
+	FillShader::Fill(Rectangle::FromCorner(Screen::TopLeft(),
+		Point(planetCardWidth + arrowOffset + 10, planetPanelHeight + 30)),
+		Color(0.15,0,.15,.1));
+	////////////////////////////////////////
+
+		const double startingX = mapInterface->GetValue("starting X");
 	Point uiPoint(Screen::Left() + startingX, Screen::Top());
 
 	// Draw the basic information for visitable planets in this system.
-	if(canView && !planetCards.empty())
-	{
+	if (canView && !planetCards.empty()) {
 		uiPoint.Y() -= scroll.AnimatedValue();
-		for(auto &card : planetCards)
-		{
+		for (auto &card: planetCards) {
 			// Fit another planet, if we can, also give scrolling freedom to reach the planets at the end.
 			// This updates the location of the card so it needs to be called before AvailableSpace().
 			card.DrawIfFits(uiPoint);
@@ -843,6 +832,25 @@ void MapDetailPanel::DrawInfo()
 	uiPoint.X() -= (tradeSprite->Width() / 2. - textMargin);
 	uiPoint.Y() -= (tradeSprite->Height() / 2. - textMargin);
 
+	// Add the danger icon click zone.
+	FillShader::Fill(Rectangle::FromCorner(Point(Screen::Left(), governmentY - 30),
+		Point(mapInterface->GetValue("text margin"), 30)), Color(.1,0,0,0.01));
+	clickZones.emplace_back(Rectangle::FromCorner(Point(Screen::Left(), governmentY - 30),
+		Point(mapInterface->GetValue("text margin"), 30)), 1 * SHOW_DANGER);
+
+	// Add the reputation click zone.
+	FillShader::Fill(Rectangle::FromCorner(
+		Point(Screen::Left() + mapInterface->GetValue("text margin"), governmentY - 30),
+		Point(160 - mapInterface->GetValue("text margin"), 30)), Color(0,.1,.1,0.01));
+	clickZones.emplace_back(Rectangle::FromCorner(
+		Point(Screen::Left() + mapInterface->GetValue("text margin"), governmentY - 30),
+		Point(160 - mapInterface->GetValue("text margin"), 30)), 1 * SHOW_REPUTATION);
+
+	// Add the government click zone.
+	FillShader::Fill(Rectangle::FromCorner(Point(Screen::Left(), governmentY), Point(160, 25)), Color(.1,0,.1,0.01));
+	clickZones.emplace_back(Rectangle::FromCorner( Point(Screen::Left(), governmentY),
+		Point(160, 25)), 1 * SHOW_GOVERNMENT);
+
 	// Don't "compare" prices if the current system is uninhabited and thus has no prices to compare to.
 	bool noCompare = !player.GetSystem() || !player.GetSystem()->IsInhabited(player.Flagship());
 	int value = 0;
@@ -869,12 +877,19 @@ void MapDetailPanel::DrawInfo()
 		}
 	}
 
+	int i = 0;
 	for(const Trade::Commodity &commodity : GameData::Commodities())
 	{
 		bool isSelected = false;
 		if(static_cast<unsigned>(this->commodity) < GameData::Commodities().size())
 			isSelected = (&commodity == &GameData::Commodities()[this->commodity]);
 		const Color &color = isSelected ? medium : dim;
+
+		// The player clicked on a tradable commodity. Color the map by its price.
+		// Add the click zone for this commodity.
+		FillShader::Fill(Rectangle::FromCorner(Point(Screen::Left(), uiPoint.Y()), Point(170, 20)),
+			Color(1 - (.05 * i), .05 * i, 0, 0.01));
+		clickZones.emplace_back(Rectangle::FromCorner(Point(Screen::Left(), uiPoint.Y()), Point(170, 20)), i);
 
 		font.Draw(commodity.name, uiPoint, color);
 
@@ -935,6 +950,7 @@ void MapDetailPanel::DrawInfo()
 			PointerShader::Draw(uiPoint + Point(0., 7.), Point(1., 0.), 10.f, 10.f, 0.f, color);
 
 		uiPoint.Y() += 20.;
+		++i;
 	}
 
 	if(selectedPlanet && !selectedPlanet->Description().IsEmptyFor()
