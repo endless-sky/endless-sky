f84f96058f5a0c2f231dcb8d52ff40e61a4fb692
Amazinite <jsteck2000@gmail.com>
2025-11-15 12:41:02 -0500

    refactor(style): Remove uses of the std:: prefix from various cpp files (#11926)

--- source/shader/FogShader.cpp
+++ source/shader/FogShader.cpp
@@ -67,7 +67,7 @@ void FogShader::Init()
 	// Compile the shader and store indices to its variables.
 	shader = GameData::Shaders().Get("fog");
 	if(!shader->Object())
-		throw std::runtime_error("Could not find fog shader!");
+		throw runtime_error("Could not find fog shader!");
 	cornerI = shader->Uniform("corner");
 	dimensionsI = shader->Uniform("dimensions");
 
