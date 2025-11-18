bd00918125a6470283f044118bc2bb7a0dc58489
Timothy <tcollett+github@topazgryphon.org>
2025-11-17 20:18:33 -0500

    feat(enhancement): Missions can now have "timer" nodes that trigger an action after a certain number of frames have elapsed (#9089)

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -4354,6 +4354,14 @@ void PlayerInfo::StepMissions(UI *ui)
 
 
 
+void PlayerInfo::StepMissionTimers(UI *ui)
+{
+	for(Mission &mission : missions)
+		mission.StepTimers(*this, ui);
+}
+
+
+
 void PlayerInfo::Autosave() const
 {
 	if(!CanBeSaved() || filePath.length() < 4)
