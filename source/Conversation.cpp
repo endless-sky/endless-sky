75888d12a86b5c8aef6cab5604579ede865e69ee
Mere Decade <meredecade@protonmail.com>
2025-11-18 16:47:01 -0500

    Adds GoTo support for conversation nodes

--- source/Conversation.cpp
+++ source/Conversation.cpp
@@ -182,6 +182,13 @@ void Conversation::Load(const DataNode &node, const ConditionsStore *playerCondi
 				}
 			}
 		}
+		else if(key == "goto") {
+			// A goto is the same as an empty condition (per issue #11946.)
+			nodes.emplace_back();
+			nodes.back().canMergeOnto = false;
+			nodes.back().elements.emplace_back("", nodes.size());
+			Goto(child.Token(1), nodes.size() - 1);
+		}
 		else if(key == "action" || key == "apply")
 		{
 			if(key == "apply")
