7566b409cec22d37dea68e16a3d88e46d9ed7324
Amazinite <17688683+Amazinite@users.noreply.github.com>
2026-01-06 17:47:43 -0500

    fix(mechanics): Fix a crash caused by missions that take ships from the player while in the shop

--- source/ShopPanel.cpp
+++ source/ShopPanel.cpp
@@ -103,6 +103,34 @@ ShopPanel::ShopPanel(PlayerInfo &player, bool isOutfitter)
 
 void ShopPanel::Step()
 {
+	// Verify that the player's selection is still valid.
+	// A mission action may have taken a ship away from the player,
+	// therefore invalidating its pointer.
+	set<Ship *> shipPtrs;
+	for(const shared_ptr<Ship> &ship : player.Ships())
+		shipPtrs.insert(ship.get());
+	set<Ship *> stillValid;
+	ranges::set_intersection(shipPtrs, playerShips, inserter(stillValid, stillValid.begin()));
+	playerShips = stillValid;
+	if(playerShip && !playerShips.contains(playerShip))
+	{
+		if(!playerShips.empty())
+			playerShip = *playerShips.begin();
+		else
+		{
+			for(auto it = shipPtrs.begin() ; it != shipPtrs.end() ; ++it)
+			{
+				Ship *ship = *it;
+				if(CanShowInSidebar(*ship, player.GetPlanet()))
+				{
+					playerShip = ship;
+					playerShips.insert(playerShip);
+					break;
+				}
+			}
+		}
+	}
+
 	if(!checkedHelp && GetUI().IsTop(this) && player.Ships().size() > 1)
 	{
 		if(DoHelp("multiple ships"))
