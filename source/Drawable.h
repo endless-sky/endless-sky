d405a0252e225147ac74d3ab38fd9831ab3d3c96
Nicholas Shanks <nickshanks@gmail.com>
2026-01-24 20:35:45 +0000

    Split Body in half, with all non-positional properties moved to a parent class.

---
+++ source/Drawable.h
@@ -0,0 +1,130 @@
+/* Drawable.h
+Copyright (c) 2026 by Endless Sky contributors
+
+Endless Sky is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+Endless Sky is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "Angle.h"
+#include "Point.h"
+#include "Swizzle.h"
+
+#include <string>
+
+class DataNode;
+class DataWriter;
+class Government;
+class Mask;
+class Sprite;
+
+
+
+// Class representing any object in the game that usually also has a sprite.
+class Drawable {
+public:
+	// Constructors.
+	Drawable() = default;
+	Drawable(const Sprite *sprite, double zoom = 1., Point scale = Point(1., 1.), double alpha = 1.);
+	Drawable(const Drawable &sprite, double zoom = 1., Point scale = Point(1., 1.), double alpha = 1.);
+
+	// Check that this Drawable has a sprite and that the sprite has at least one frame.
+	bool HasSprite() const;
+	// Access the underlying Sprite object.
+	const Sprite *GetSprite() const;
+	// Get the dimensions of the sprite.
+	double Width() const;
+	double Height() const;
+	// Get the farthest a part of this sprite can be from its center.
+	double Radius() const;
+	// Which color swizzle should be applied to the sprite?
+	const Swizzle *GetSwizzle() const;
+	bool InheritsParentSwizzle() const;
+	// Get the sprite frame and mask for the given time step.
+	float GetFrame(int step = -1) const;
+	const Mask &GetMask(int step = -1) const;
+
+	// Positional attributes.
+	double Zoom() const;
+	Point Scale() const;
+
+	// Check if this object is marked for removal from the game.
+	bool ShouldBeRemoved() const;
+
+	// Sprite serialization.
+	void LoadSprite(const DataNode &node);
+	void SaveSprite(DataWriter &out, const std::string &tag = "sprite") const;
+	// Set the sprite.
+	void SetSprite(const Sprite *sprite);
+	// Set the color swizzle.
+	void SetSwizzle(const Swizzle *swizzle);
+
+
+protected:
+	// Adjust the frame rate.
+	void SetFrameRate(float framesPerSecond);
+	void AddFrameRate(float framesPerSecond);
+	void PauseAnimation();
+	// Mark this object to be removed from the game.
+	void MarkForRemoval();
+	// Mark that this object should not be removed (e.g. a launched fighter).
+	void UnmarkForRemoval();
+
+
+protected:
+	// Basic positional attributes.
+	Point center;
+	// A zoom of 1 means the sprite should be drawn at half size. For objects
+	// whose sprites should be full size, use zoom = 2.
+	float zoom = 1.f;
+	Point scale = Point(1., 1.);
+
+	double alpha = 1.;
+	// The maximum distance at which the body is visible, and at which it becomes invisible again.
+	double distanceVisible = 0.;
+	double distanceInvisible = 0.;
+
+	// Government, for use in collision checks.
+	const Government *government = nullptr;
+
+
+private:
+	// Set what animation step we're on. This affects future calls to GetMask()
+	// and GetFrame().
+	void SetStep(int step) const;
+
+
+private:
+	// Animation parameters.
+	const Sprite *sprite = nullptr;
+	// Allow objects based on this one to adjust their frame rate and swizzle.
+	const Swizzle *swizzle = Swizzle::None();
+	bool inheritsParentSwizzle = false;
+
+	float frameRate = 2.f / 60.f;
+	int delay = 0;
+	// The chosen frame will be (step * frameRate) + frameOffset.
+	mutable float frameOffset = 0.f;
+	mutable bool startAtZero = false;
+	mutable bool randomize = false;
+	bool repeat = true;
+	bool rewind = false;
+	int pause = 0;
+
+	// Record when this object is marked for removal from the game.
+	bool shouldBeRemoved = false;
+
+	// Cache the frame calculation so it doesn't have to be repeated if given
+	// the same step over and over again.
+	mutable int currentStep = -1;
+	mutable float frame = 0.f;
+};
