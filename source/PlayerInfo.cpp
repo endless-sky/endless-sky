49634a4491036bed4c399824a585301f21c57e4a
TomGoodIdea <tomgoodidea@proton.me>
2026-01-29 01:04:40 +0100

    Allow sending escorts to the system center from the map

--- source/PlayerInfo.cpp
+++ source/PlayerInfo.cpp
@@ -3465,7 +3465,7 @@ void PlayerInfo::SetEscortDestination(const System *system, Point pos)
 // Determine if a system and nonzero position were specified.
 bool PlayerInfo::HasEscortDestination() const
 {
-	return interstellarEscortDestination.first && interstellarEscortDestination.second;
+	return interstellarEscortDestination.first;
 }
 
 
