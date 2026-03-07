ef709aafaafa2bee4ca568c1baafdfd7b84ced0f
NellyDevo <Noelle.Devonshire@yahoo.com>
2026-03-07 16:23:44 -0500

    update copyrights

--- source/PresetsPanel.cpp
+++ source/PresetsPanel.cpp
@@ -1,5 +1,5 @@
 /* PresetsPanel.cpp
-Copyright (c) 2026 by Endless Sky contributors
+Copyright (c) 2026 by Noelle Devonshire
 
 Endless Sky is free software: you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
