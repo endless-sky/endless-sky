efabb09406038168a8e9e48caa02560db5d47b67
Amazinite <17688683+Amazinite@users.noreply.github.com>
2026-04-06 17:09:52 -0400

    Don't spam the player with scan dialogs

--- source/MainPanel.cpp
+++ source/MainPanel.cpp
@@ -131,6 +131,19 @@ void MainPanel::Step()
 	// and updating the isActive flag).
 	StepEvents(isActive);
 
+	// Ships that were recently scanned shouldn't show a new scan dialog for 10 seconds.
+	auto StepRecentScans = [](map<shared_ptr<Ship>, int> &recentScans) -> void {
+		for(auto it = recentScans.begin() ; it != recentScans.end() ; )
+		{
+			if(++(it->second) > 600)
+				it = recentScans.erase(it);
+			else
+				++it;
+		}
+	};
+	StepRecentScans(recentCargoScans);
+	StepRecentScans(recentOutfitScans);
+
 	if(isActive)
 		engine.Go();
 	else
@@ -672,12 +685,22 @@ void MainPanel::StepEvents(bool &isActive)
 		}
 
 		// Handle scan events of or by the player.
-		if(event.Type() & (ShipEvent::SCAN_CARGO | ShipEvent::SCAN_OUTFITS))
+		bool cargoScan = event.Type() & ShipEvent::SCAN_CARGO;
+		bool outfitScan = event.Type() & ShipEvent::SCAN_OUTFITS;
+		if(cargoScan || outfitScan)
 		{
 			if(actor->IsPlayer())
 			{
-				ShowScanDialog(event);
-				isActive = false;
+				const shared_ptr<Ship> &target = event.Target();
+				if((cargoScan && !recentCargoScans.contains(target)) || (outfitScan && !recentOutfitScans.contains(target)))
+				{
+					if(cargoScan)
+						recentCargoScans[target] = 0;
+					if(outfitScan)
+						recentOutfitScans[target] = 0;
+					ShowScanDialog(event);
+					isActive = false;
+				}
 			}
 			else if(event.TargetGovernment() && event.TargetGovernment()->IsPlayer())
 			{
